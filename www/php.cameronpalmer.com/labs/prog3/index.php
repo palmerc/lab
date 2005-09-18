a49d32f66b6621a0220c05316da83df10117bf23
palmerc <palmerc@9c8e43f7-b2fe-0310-8927-e999a62a3b53>
2005-09-18 20:49:40 +0000

    making tim inspired adjustments part three Now ignores non-numeric input

--- www/php.cameronpalmer.com/labs/prog3/index.php
+++ www/php.cameronpalmer.com/labs/prog3/index.php
@@ -8,7 +8,9 @@
 	$table_size = $default_size;
 	
 if ($_SERVER['REQUEST_METHOD'] == "POST"){
+	// Get the last value entered so that we can stay on the same value when illegal input is given
 	$last_size = $_REQUEST['last_size'];
+	// Check to see which button was hit and grab the value
 	if (isset($_POST['text_submit_button'])){
 		$table_size = $_REQUEST['text_field'];
 	} elseif (isset($_POST['select_submit_button'])){
@@ -16,17 +18,27 @@ if ($_SERVER['REQUEST_METHOD'] == "POST"){
 	} elseif (isset($_POST['radio_submit_button'])){
 		$table_size = $_REQUEST['radio_button'];
 	}
+	// Validate the result. Is it in the range of acceptable values?
 	if ($table_size > $max_size or $table_size < $min_size) {
-		echo "<p class=\"error\">Value out of range, must be between {$min_size} and {$max_size}</p>\n";
+		if (is_int($table_size)) {
+			echo "<p class=\"error\">Value out of range, must be between {$min_size} and {$max_size}</p>\n";
+		} else {
+			echo "<p class=\"error\">Non-integer, must be an integer between {$min_size} and {$max_size}</p>\n";
+		}
 		$table_size = $last_size;
 	} else {
 		echo "<p class=\"nonerror\">Value should be between {$min_size} and {$max_size}</p>\n";
 		$last_size = $table_size;
 	}
+	// If it isn't a POST populate $last_size with the default 
+} else {
+	echo "<p class=\"nonerror\">Value should be between {$min_size} and {$max_size}</p>\n";
+	$last_size = $default_size;
 }
 
 ?>
 
+<!-- BEGIN Generate the form -->
 <div class="form">
 	<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
 		<input type="hidden" name="last_size" value="<?php echo $last_size; ?>" />
@@ -69,7 +81,10 @@ if ($_SERVER['REQUEST_METHOD'] == "POST"){
 		</div>
 	</form>
 </div>
+<!-- END Generate the form -->
+<!-- BEGIN Generate the multiplication table -->
 		<?php table_generate($table_size); ?>
+<!-- END Generate the multiplication table -->
 
 <?php
 function table_generate($table_size){
@@ -78,14 +93,14 @@ function table_generate($table_size){
 		<!-- The table head -->
 		<tr><th>&nbsp;</th>';
     for ($i=1; $i <= $table_size; $i++) {
-	  	echo "<th>{$i}</th>";
+	  	echo "<th scope=\"col\" class=\"col\">{$i}</th>";
     }
 	  echo"</tr>
 		<!-- Generate the table -->
 		";
 		for ($i=1; $i <= $table_size; $i++) {
 			echo"
-			<tr><th>{$i}</th>";
+			<tr><th scope=\"row\">{$i}</th>";
 			for ($j=1; $j <= $table_size; $j++) {
 				echo "<td>" . $i * $j. "</td>";
 			}
