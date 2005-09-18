457689bb07a8616bd6bf833c453d1d981fb8c1bd
palmerc <palmerc@9c8e43f7-b2fe-0310-8927-e999a62a3b53>
2005-09-18 18:53:39 +0000

    Getting close

--- www/php.cameronpalmer.com/labs/prog3/index.php
+++ www/php.cameronpalmer.com/labs/prog3/index.php
@@ -8,39 +8,63 @@
 	$table_size = $default_size;
 	
 if ($_SERVER['REQUEST_METHOD'] == "POST"){
-	if ($_REQUEST['text_field']) $table_size = $_REQUEST['text_field'];
-	if ($_REQUEST['table_select']) $table_size = $_REQUEST['table_select'];
-	if ($_REQUEST['radio_button']) $table_size = $_REQUEST['radio_button'];
-	if ($table_size > $max_size and $table_size < $min_size) {
-		echo "<p>Value out of range, must be between {$min_size} and {$max_size}</p>\n";
-		$table_size = 12;
+	$last_size = $_REQUEST['last_size'];
+	if (isset($_POST['text_submit_button'])){
+		$table_size = $_REQUEST['text_field'];
+	} elseif (isset($_POST['select_submit_button'])){
+		$table_size = $_REQUEST['table_select'];
+	} elseif (isset($_POST['radio_submit_button'])){
+		$table_size = $_REQUEST['radio_button'];
+	}
+	if ($table_size > $max_size or $table_size < $min_size) {
+		echo "<p class=\"error\">Value out of range, must be between {$min_size} and {$max_size}</p>\n";
+		$table_size = $last_size;
+	} else {
+		$last_size = $table_size;
 	}
 }
 
 ?>
 
-<div>
-	<form method="post" action="?">
-		<input name="text_field" type="text" size="3" maxlength="2" value="<?php echo "{$table_size}"; ?>" />
-		<input type="submit" name="text_submit_button" value="Submit" />
-		<select name="table_select" />
+<div class="form">
+	<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+		<input type="hidden" name="last_size" value="<?php echo $last_size; ?>" />
+		<div id="text_input">			
+			<input name="text_field" type="text" size="3" maxlength="2" value="<?php echo $table_size; ?>" />
+			<input type="submit" name="text_submit_button" value="Submit" />
+		</div>
+		<div id="select_input">
+			<select name="table_select">
+				<?php
+				for ($i = $min_size; $i <= $max_size; $i++){
+					echo "<option value=\"{$i}\" "; 
+					if ($_SERVER['REQUEST_METHOD'] != "POST" and $i == $max_size){
+						echo 'selected="selected" ';
+					}
+					if ($_SERVER['REQUEST_METHOD'] == "POST" and $i == $table_size){
+						echo 'selected="selected" ';
+					}
+					echo ">{$i}</option>\n";
+				}
+				?>
+			</select>
+			<input type="submit" name="select_submit_button" value="Submit" />
+		</div>
+		<div id="radio_input">
 			<?php
 			for ($i = $min_size; $i <= $max_size; $i++){
-				echo "<option>{$i}</option>\n";
+				echo "<label for=\"Radio{$i}\">{$i}</label><input id=\"Radio{$i}\" type=\"radio\" name=\"radio_button\" value=\"{$i}\" ";
+					if ($_SERVER['REQUEST_METHOD'] != "POST" and $i == $max_size){
+						echo 'checked="checked" ';
+					}
+					if ($_SERVER['REQUEST_METHOD'] == "POST" and $i == $table_size){
+						echo 'checked="checked" ';
+					}
+				echo "/>\n";
 			}
 			?>
-		</select>
-		<input type="submit" name="select_submit_button" value="Submit" /> 	
-		<?php
-		for ($i = $min_size; $i <= $max_size; $i++){
-			echo "{$i}<input type=\"radio\" name=\"radio_button\" value=\"{$i}\" ";
-				if ($_SERVER['REQUEST_METHOD'] != "POST" and $i == $max_size){
-					echo 'checked="checked" ';
-				}
-			echo "/>\n";
-		}
-		?>
-		<input type="submit" name="radio_submit_button" value="Submit" /> 	
+			<input type="submit" name="radio_submit_button" value="Submit" />
+		</div>
 	</form>
 </div>
 
