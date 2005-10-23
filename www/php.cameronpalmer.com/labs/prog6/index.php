1dfebea5c7044b9e869cf6dfc809086910f77dcb
palmerc <palmerc@9c8e43f7-b2fe-0310-8927-e999a62a3b53>
2005-10-23 21:33:35 +0000

    Adding assignment dat files.

--- www/php.cameronpalmer.com/labs/prog6/index.php
+++ www/php.cameronpalmer.com/labs/prog6/index.php
@@ -3,4 +3,12 @@
 	$section = "Assignment: ";
 	require("../../php-template.php");
 
+  echo"
+  			Quotation filename: <input type=\"text\" value=\"\" />
+  			Always build index? <input type=\"checkbox\" />
+  			Limitation: <select></select>
+  			Max Size: <input type=\"text\" value=\"\" />
+  			Preformat output: <input type=\"checkbox\" />
+	  		<input type=\"submit\" value-\"Display 5 random quotes\" />
+	  	";
 ?>
