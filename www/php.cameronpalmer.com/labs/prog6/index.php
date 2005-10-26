4a8508fd96d7ad32e2fcfa1c38ab26e4748edab7
palmerc <palmerc@9c8e43f7-b2fe-0310-8927-e999a62a3b53>
2005-10-26 02:33:07 +0000

    Added some new fortune files and rot13 support for offensive ones

--- www/php.cameronpalmer.com/labs/prog6/index.php
+++ www/php.cameronpalmer.com/labs/prog6/index.php
@@ -2,18 +2,20 @@
 	$title = "CSCE 2410 - PHP Program 6";
 	$section = "Assignment: Quotation Display System";
 	require("../../php-template.php");
+	$fortunesdir = "fortunesdir/";
 
   echo"
-	  	<form method=\"post\" action=\"{$_SERVER['PHP_SELF']}\">
-	  		<div class=\"leftside\">
-	  			<p>
-	  				Quotation filename: <input type=\"text\" name=\"filename\" size=\"40\" value=\"";
-	  				
+  <form method=\"post\" action=\"{$_SERVER['PHP_SELF']}\">
+  	<div class=\"leftside\">
+  		
+  		<p>
+  			Quotation filename: <input type=\"text\" name=\"filename\" size=\"40\" value=\"";
+  				
 	if ($_POST['filename']) echo $_POST['filename'];
 	echo "\" />
-	  			</p>
-	  			<p>
-		  			Always build index? <input type=\"checkbox\" name=\"buildindex\"";
+  		</p>
+  		<p>
+  			Always build index? <input type=\"checkbox\" name=\"buildindex\"";
 	if (isset($_POST['buildindex'])) echo " checked=\"checked\"";
 	echo' />
 		  			Limitation: <select name="limitation">
@@ -37,7 +39,12 @@
 		  			Max Size: <input type=\"text\" name=\"maxsize\" size=\"4\" value=\"{$_POST['maxsize']}\" />
 		  			Preformat output: <input type=\"checkbox\" name=\"preformat\"";
 	if (isset($_POST['preformat'])) echo " checked=\"checked\"";
-	echo " />
+	echo" />";
+	echo"
+			  		Offensive ROT13 output: <input type=\"checkbox\" name=\"rot13\"";
+	if (isset($_POST['rot13'])) echo " checked=\"checked\"";
+
+	echo" />
 		  		</p>
 		  		<p>
 			  		<input type=\"submit\" name=\"submit\" value=\"Display 5 random quotes\" />
@@ -55,21 +62,24 @@
 		if (isset($_POST['filename'])) {
 			//print_r($_POST);
 			$passed_value = trim($_POST['filename']);
-			if (preg_match('/\.dat$/', $passed_value)) { 
-				$datfile = $passed_value;
-			} else {
-				$datfile = $passed_value . ".dat";
-			}
-	
+			$datfile = $fortunesdir . $passed_value;
+
 			$indexfile = preg_replace('/\.dat$/', '.index', $datfile);
 		}
+
+		if (isset($_POST['rot13'])) {
+				$rot13 = true;
+			} else {
+				$rot13 = false;
+			}
 		
 		//var_dump($datfile);
 		//var_dump($indexfile);
 	
 		if (file_exists($datfile)) {
 			$handle = @fopen($datfile, "rb");
-			if (isset($_POST['buildindex']) or !file_exists($indexfile)) {
+			if ((isset($_POST['buildindex']) and file_exists($datfile)) or 
+					(file_exists($datfile) and !file_exists($indexfile))) {
 				createIndexFile($datfile, $indexfile);
 			}
 		
@@ -78,14 +88,17 @@
 			if ($_POST['limitation'] == "Max Lines" and $_POST['maxsize'] > 0) {
 				$maxsize = $_POST['maxsize'];
 				$quote_array = selectQuotes(0, $maxsize);
+				if ($rot13) { $quote_array = rot13Quotes($quote_array); }
 				displayQuotes($quote_array);
 			} else if ($_POST['limitation'] == "Max Characters" and $_POST['maxsize'] > 0) {
 				// Added one for the new line character
 				$maxsize = $_POST['maxsize'] + 1;
+				if ($rot13) { $quote_array = rot13Quotes($quote_array); }
 				$quote_array = selectQuotes($maxsize, 0);
 				displayQuotes($quote_array);
 			} else {
 				$quote_array = selectQuotes(0, 0);
+				if ($rot13) { $quote_array = rot13Quotes($quote_array); }
 				displayQuotes($quote_array);
 			}
 			fclose($handle);
@@ -99,18 +112,28 @@
 				";
 	}
 
+	$dh = opendir($fortunesdir);
+	while ($filename = readdir($dh)) {
+		$file_array[] = $filename;
+	}
 	echo"
 			<div class=\"leftside\">
 				<hr />
-		  	Original quotation files:
+		  	Quotation files:
 	  		<ul>
-		  		<li><a href=\"cats.dat\">cats.dat</a></li>
-		  		<li><a href=\"quotes.dat\">quotes.dat</a></li>
-		  		<li><a href=\"colors.dat\">colors.dat</a></li>
-		  	</ul>
+	  	";
+	  	
+	foreach ($file_array as $file) {
+		if (preg_match('/\.dat$/', $file)) {
+			echo"
+					<li><a href=\"". $fortunesdir . $file . "\">{$file}</a></li>
+		  		";
+		}
+	}
+	echo"
+			  </ul>
 	  	</div>
 	  	";
-
 
 function createIndexFile($datfile, $indexfile) {
 	global $handle;
@@ -225,6 +248,16 @@ function displayQuotes($quote_array) {
 	echo"
 		</table>
 			";
+}
+
+function rot13Quotes($quote_array) {
+	//echo "NO";
+	for ($i=0; $i < count($quote_array); $i++) {
+		$quote_array[$i] = str_rot13($quote_array[$i]);
+	}
+
+	//print_r($quote_array);
+	return $quote_array;
 }
 
 ?>
