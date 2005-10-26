e099503a6fe7021f8cab4412c8452fb44734846a
palmerc <palmerc@9c8e43f7-b2fe-0310-8927-e999a62a3b53>
2005-10-26 06:08:50 +0000

    Moved netpbm location variables to a seperate file

---
+++ www/php.cameronpalmer.com/labs/prog7/netpbm.php
@@ -0,0 +1,10 @@
+<?php
+	$netpbmdir = "/usr/local/netpbm/bin/";
+	$jpegtopnm = "{$netpbmdir}jpegtopnm";
+	$pngtopnm = "{$netpbmdir}pngtopnm";
+	$giftopnm = "{$netpbmdir}giftopnm";
+	$pamscale = "{$netpbmdir}pamscale";
+	$pnmtojpeg = "{$netpbmdir}pnmtojpeg";
+	$pnmtopng = "{$netpbmdir}pnmtopng";
+	$pnmtogif = "{$netpbmdir}pnmtogif";
+?>
