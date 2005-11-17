b53eda5dbc3b6e3e0fc0fb2474e2bd485f14c46d
(no author) <(no author)@9c8e43f7-b2fe-0310-8927-e999a62a3b53>
2005-11-17 03:49:09 +0000

    the latest mostly working 8

--- www/php.cameronpalmer.com/labs/prog8/index.php
+++ www/php.cameronpalmer.com/labs/prog8/index.php
@@ -28,13 +28,44 @@
 		$urlfile = $_POST['urlfile'];
 		$page_array = file($urlfile);
 		$page_string = implode('', $page_array);
+		// I didn't want a bunch of unsightly spaces. Besides a web browser collapses them.
 		$page_string = preg_replace('/\s\s+/', ' ', $page_string);
 //		$page_string = preg_replace('/\n/', '', $page_string);
 	
-		echo '<pre>';
-		echo strlen($page_string) . ": ";
-		echo htmlspecialchars($page_string) . "\n";				
-		echo '</pre>';		
+//		echo '<pre>';
+//		echo strlen($page_string) . ": ";
+//		$page_string = nl2br($page_string);
+//		echo htmlspecialchars($page_string) . "\n";				
+//		echo '</pre>';
+//		echo '<hr />';
+
+		preg_match_all('#(<\w+[^>]*/*>)|(</[\w]+[^>]*>)|(\w+[^<]*)#', $page_string, $matches);
+		
+//		echo '<pre>';
+//		print_r($matches);
+
+		// So basically I went to a lot of trouble here for no apparent reason. Well I just wanted to operate on a
+		// stack. So there. Nanny nanny boo boo.
+		$stack = array();
+		foreach ($matches[0] as $token) {
+			array_push($stack, $token);
+//			echo htmlspecialchars($token) . "\n";
+		}
+//		echo '</pre>';
+
+		while ($item = array_pop($stack)) {
+			if (preg_match('/<meta .*content="?(.+)"?/', $item, $content)) {
+				echo "Content field: {$content[1]}<br />";
+			}
+			if ($item == "</title>") {
+					$title = array_pop($stack);
+					// Pop off the opening title tag
+					array_pop($stack);
+			}
+
+			if ($item == "</table>") {
+			}
+		}
+		echo "Title is: {$title}.<br />\n";
 	}
-	
 ?>
