50e4c296ea8bfef2afdf1f4af9506883eaa1d17d
(no author) <(no author)@9c8e43f7-b2fe-0310-8927-e999a62a3b53>
2006-01-05 20:06:42 +0000

    beginnings of a web page for the pdf data

---
+++ www/unt.cameronpalmer.com/index.php
@@ -0,0 +1,19 @@
+<?php
+	$title = "UNT Schedule Search Project";
+	$section = "Welcome";
+	require("unt-template.php");
+?>
+	<div id="frontpage">
+		<div class="leftside">
+			<h2>Project Details</h2>
+			<p>The goal of this project is to automatically download some of the
+            UNT schedule information and process it into a useful collection of
+            files for further automated report and study.</p>
+		</div>
+
+
+		<h3>Raw Schedule Data</h3>
+			<ul>
+				<li><a href=\"data/pdf">PDF Format</a></li>
+			</ul>
+  </div>
