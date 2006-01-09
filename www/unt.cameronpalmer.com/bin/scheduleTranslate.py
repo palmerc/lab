8a5ffdbfe4c854e347dcb3a0de44835dc13cf327
(no author) <(no author)@9c8e43f7-b2fe-0310-8927-e999a62a3b53>
2006-01-09 07:32:49 +0000

    bunch of changes

--- www/unt.cameronpalmer.com/bin/scheduleTranslate.py
+++ www/unt.cameronpalmer.com/bin/scheduleTranslate.py
@@ -105,7 +105,7 @@ def txttocsv(txtfile, csvfile=None):
     if csvfile and os.path.isfile(txtfile):
         input = open(txtfile, 'rb')
         output = open(outfile, 'w')
-        outputtxt = parsetxt(input.read())
+        outputtxt = parsetxt(input.reads())
         output.write(outputtxt)
         input.close()
         output.close()
@@ -122,7 +122,7 @@ def txttocsv(txtfile, csvfile=None):
             csvfile = datadir + semesdir + '/' + csvfile
             input = open(origfile, 'rb')
             output = open(csvfile, 'w')
-            outputtxt = parsetxt(input.read())
+            outputtxt = parsetxt(input.reads())
             output.write(outputtxt)
             input.close()
             output.close()
