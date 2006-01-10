e38193abfefa8780be170eff770fcf315951e1e5
(no author) <(no author)@9c8e43f7-b2fe-0310-8927-e999a62a3b53>
2006-01-10 07:10:00 +0000

    latest and greatest

--- www/unt.cameronpalmer.com/bin/scheduleTranslate-backup.py
+++ www/unt.cameronpalmer.com/bin/scheduleTranslate-backup.py
@@ -190,3 +190,94 @@ if __name__ == '__main__':
     textfile = '../data/txt/1061/accounting_1061.txt'
     #textfile = '/var/data/www/unt.cameronpalmer.com/data/txt/'
     csvfile = txttocsv(textfile)
+
+
+        """
+    CSV Description:
+    TERM,DEPT,COURSENUM,COURSETITLE,SECTION,REGNUM,CREDITTYPE,CREDITHOURS,DAYS,STARTTIME,ENDTIME,CLASSROOM,PROF,NOTES
+    """
+    docstart = re.compile(r'^\s*Updated:\s+(?P<updatedate>\d{1,2}/\d{1,2}/\d{4})' \
+                          r'\s+(?P<updatetime>\d{1,2}:\d{1,2}:\d{1,2}AM|PM)' \
+                          r'\s+Term:(?P<term>\d{4})' \
+                          r'\s+(?P<session>Spring|Summer|Fall)\s+(?P<year>\d+)$')
+    
+    course = re.compile(r'^\s*(?P<coursedept>[A-Z]{3,4})\s(?P<coursenumber>\d{4})\s+(?P<coursetitle>.+)$')
+    section = re.compile(r'^\s(?P<section>\d{3})\s+\((?P<regcode>\d+)\)' \
+                            r'\s+(?P<type>CRE|LAB|REC)\s+(?P<credits>[V\d.]+)\s')
+    times = re.compile(r'\s(?P<starttime>\d{2}:\d{2}\s(?:am|pm))' \
+                            r'-(?P<endtime>\d{2}:\d{2}\s(?:am|pm))\s')
+    classroom = re.compile(r'\s(?P<classroom>INET|(?:[A-Z]+\s[0-9]+))\s')
+    instructor = re.compile(r'INET|[0-9 ]*(?P<instructor>[A-Za-z- ]+)$')
+    
+    regsection = re.compile(r'^\s*(?P<section>\d{3})\s+\((?P<regcode>\d+)\)' \
+                            r'\s+(?P<type>CRE|LAB|REC)\s+(?P<credits>[V\d.]+)' \
+                            r'\s+(?P<days>[MTWRFSU]+)\s+(?P<starttime>\d{2}:\d{2}\s+(?:am|pm))' \
+                            r'-(?P<endtime>\d{2}:\d{2}\s+(?:am|pm))\s+(?P<classroom>[A-Z]+\s[0-9]*)' \
+                            r'\s*(?P<instructor>.*)$')
+    inetsection = re.compile(r'^\s*(?P<section>\d{3})\s+\((?P<regcode>\d+)\)' \
+                            r'\s+(?P<type>CRE|LAB|REC)\s+(?P<credits>[V\d.]+)' \
+                            r'\s+(?P<classroom>INET)\s*(?P<instructor>.*)$')
+    specsection = re.compile(r'^\s*(?P<section>\d{3})\s+\((?P<regcode>\d+)\)' \
+                            r'\s+(?P<type>CRE|LAB|REC)\s+(?P<credits>[V\d.]+)' \
+                            r'\s*(?P<classroom>[A-Z]*\s[0-9]*)\s*(?P<instructor>[A-Z]*.*)$')
+    stack = []
+    dept = None
+    #i = 0
+    outputtxt = 'TERM,DEPT,COURSENUM,COURSETITLE,SECTION,REGNUM,CREDITTYPE,CREDITHOURS,DAYS,STARTTIME,ENDTIME,CLASSROOM,PROF,NOTES\n'
+    for line in inputtxt:
+        line = line.strip()
+        docmatch = docstart.match(line)
+        coursematch = course.match(line)
+        regsectionmatch = regsection.match(line)
+        inetsectionmatch = inetsection.match(line)
+        specsectionmatch = specsection.match(line)
+        if docmatch:
+            term = docmatch.group('term')
+            continue
+            #print 'DOCMATCH: %s,%s,%s,%s %s' % docmatch.groups()
+        elif coursematch:
+            dept = coursematch.group('coursedept')
+            number = coursematch.group('coursenumber')
+            title = coursematch.group('coursetitle')
+            continue
+            #print 'COURSE: %s,%s,%s' % coursematch.groups()
+
+        if not len(stack) and dept:
+            stack.extend([term,dept,number,title])
+
+        if regsectionmatch:
+            #i = i+1
+            stack.extend(regsectionmatch.groups())
+            stack.append('')
+            outputtxt = outputtxt + string.join(stack, ',') + '\n'
+            stack = []
+        elif inetsectionmatch:
+            #i = i+1
+            stack.append(inetsectionmatch.group('section'))
+            stack.append(inetsectionmatch.group('regcode'))
+            stack.append(inetsectionmatch.group('type'))
+            stack.append(inetsectionmatch.group('credits'))
+            stack.extend(['','',''])
+            stack.append(inetsectionmatch.group('classroom'))
+            stack.append(inetsectionmatch.group('instructor'))
+            stack.append('')
+            outputtxt = outputtxt + string.join(stack, ',') + '\n'
+            stack = []
+        elif specsectionmatch:
+            #i = i+1
+            stack.append(specsectionmatch.group('section'))
+            stack.append(specsectionmatch.group('regcode'))
+            stack.append(specsectionmatch.group('type'))
+            stack.append(specsectionmatch.group('credits'))
+            stack.extend(['','',''])
+            stack.append(specsectionmatch.group('classroom'))
+            stack.append(specsectionmatch.group('instructor'))
+            stack.append('')
+            outputtxt = outputtxt + string.join(stack, ',') + '\n'
+            stack = []
+        else:
+            stack = []
+            #print 'NOMATCH: %s' % line
+    
+    #print i 
+    return outputtxt
--- www/unt.cameronpalmer.com/bin/scheduleTranslate.py
+++ www/unt.cameronpalmer.com/bin/scheduleTranslate.py
@@ -7,134 +7,116 @@ __license__ = "GPL"
 
 import os, glob, sys, re, string
 
+#dirprefix = '../data/'
+dirprefix = '/var/data/www/unt.cameronpalmer.com/data/'
+
 def parsetxt(inputtxt):
-    """
-    CSV Description:
-    TERM,DEPT,COURSENUM,COURSETITLE,SECTION,REGNUM,CREDITTYPE,CREDITHOURS,DAYS,STARTTIME,ENDTIME,CLASSROOM,PROF,NOTES
-    """
+   """
+   CSV Description:
+   TERM,DEPT,COURSENUM,COURSETITLE,SECTION,REGNUM,CREDITTYPE,CREDITHOURS,DAYS,STARTTIME,ENDTIME,CLASSROOM,PROF,NOTES
+   """
 
-    docstart = re.compile(r'^\s*Updated:\s+(?P<updatedate>\d{1,2}/\d{1,2}/\d{4})' \
-                          r'\s+(?P<updatetime>\d{1,2}:\d{1,2}:\d{1,2}AM|PM)' \
-                          r'\s+Term:(?P<term>\d{4})' \
-                          r'\s+(?P<session>Spring|Summer|Fall)\s+(?P<year>\d+)$')
-    
-    course = re.compile(r'^\s*(?P<coursedept>[A-Z]{3,4})\s(?P<coursenumber>\d{4})\s+(?P<coursetitle>.+)$')
-    section = re.compile(r'^\s(?P<section>\d{3})\s+\((?P<regcode>\d+)\)' \
-                            r'\s+(?P<type>CRE|LAB|REC)\s+(?P<credits>[V\d.]+)\s')
-    times = re.compile(r'\s(?P<starttime>\d{2}:\d{2}\s(?:am|pm))' \
-                            r'-(?P<endtime>\d{2}:\d{2}\s(?:am|pm))\s')
-    classroom = re.compile(r'\s(?P<classroom>INET|(?:[A-Z]+\s[0-9]+))\s')
-    instructor = re.compile(r'INET|[0-9 ]*(?P<instructor>[A-Za-z- ]+)$')
-    
-    regsection = re.compile(r'^\s*(?P<section>\d{3})\s+\((?P<regcode>\d+)\)' \
-                            r'\s+(?P<type>CRE|LAB|REC)\s+(?P<credits>[V\d.]+)' \
-                            r'\s+(?P<days>[MTWRFSU]+)\s+(?P<starttime>\d{2}:\d{2}\s+(?:am|pm))' \
-                            r'-(?P<endtime>\d{2}:\d{2}\s+(?:am|pm))\s+(?P<classroom>[A-Z]+\s[0-9]*)' \
-                            r'\s*(?P<instructor>.*)$')
-    inetsection = re.compile(r'^\s*(?P<section>\d{3})\s+\((?P<regcode>\d+)\)' \
-                            r'\s+(?P<type>CRE|LAB|REC)\s+(?P<credits>[V\d.]+)' \
-                            r'\s+(?P<classroom>INET)\s*(?P<instructor>.*)$')
-    specsection = re.compile(r'^\s*(?P<section>\d{3})\s+\((?P<regcode>\d+)\)' \
-                            r'\s+(?P<type>CRE|LAB|REC)\s+(?P<credits>[V\d.]+)' \
-                            r'\s*(?P<classroom>[A-Z]*\s*[0-9]*)\s*(?P<instructor>[A-Z]*.*)$')
-    stack = []
-    dept = None
-    #i = 0
-    outputtxt = 'TERM,DEPT,COURSENUM,COURSETITLE,SECTION,REGNUM,CREDITTYPE,CREDITHOURS,DAYS,STARTTIME,ENDTIME,CLASSROOM,PROF,NOTES\n'
-    for line in inputtxt:
-        line = line.strip()
-        docmatch = docstart.match(line)
-        coursematch = course.match(line)
-        regsectionmatch = regsection.match(line)
-        inetsectionmatch = inetsection.match(line)
-        specsectionmatch = specsection.match(line)
-        if docmatch:
-            term = docmatch.group('term')
-            continue
-            #print 'DOCMATCH: %s,%s,%s,%s %s' % docmatch.groups()
-        elif coursematch:
-            dept = coursematch.group('coursedept')
-            number = coursematch.group('coursenumber')
-            title = coursematch.group('coursetitle')
-            continue
-            #print 'COURSE: %s,%s,%s' % coursematch.groups()
+   docstart = re.compile(r'^\s*Updated:\s+(?P<updatedate>\d{1,2}/\d{1,2}/\d{4})' \
+                        r'\s+(?P<updatetime>\d{1,2}:\d{1,2}:\d{1,2}AM|PM)' \
+                        r'\s+Term:(?P<term>\d{4})' \
+                        r'\s+(?P<session>Spring|Summer|Fall)\s+(?P<year>\d+)$')    
+   course = re.compile(r'^\s*(?P<coursedept>[A-Z]{3,4})\s(?P<coursenumber>\d{4})\s+(?P<coursetitle>.+)$')
+   section = re.compile(r'^\s*(?P<section>\d{3})\s+\((?P<regcode>\d+)\)' \
+                        r'\s+(?P<type>CRE|LAB|REC)\s+(?P<credits>[V\d.]+)\s*')
+   days = re.compile(r'\s(?P<days>[MTWRFSU]+)\s')
+   times = re.compile(r'\s(?P<starttime>\d{2}:\d{2}\s(?:am|pm))' \
+                        r'-(?P<endtime>\d{2}:\d{2}\s(?:am|pm))\s')
+   classroom = re.compile(r'\s(?P<classroom>INET|(?:[A-Z]+\s[0-9]+))\s')
+   instructor = re.compile(r'\s\s+(?P<instructor>[A-Z]{0,1}[a-z-\']*\s*[A-Z]{0,1}[a-z-\']*\s*[A-Z]{0,1}[a-z-\']*)\s*$')
 
-        if not len(stack) and dept:
-            stack.extend([term,dept,number,title])
+   stack = []
+   dept = None
+   outputtxt = 'TERM,DEPT,COURSENUM,COURSETITLE,SECTION,REGNUM,CREDITTYPE,CREDITHOURS,DAYS,STARTTIME,ENDTIME,CLASSROOM,PROF,NOTES\n'
+   for line in inputtxt:
+      line = line.strip()
+      docmatch = docstart.match(line)
+      coursematch = course.match(line)
+      sectionmatch = section.match(line)
+      daysmatch = days.search(line)
+      timesmatch = times.search(line)
+      classroommatch = classroom.search(line)
+      instructormatch = instructor.search(line)
+      if docmatch:
+         term = docmatch.group('term')
+         continue
 
-        if regsectionmatch:
-            #i = i+1
-            stack.extend(regsectionmatch.groups())
+      elif coursematch:
+         dept = coursematch.group('coursedept')
+         number = coursematch.group('coursenumber')
+         title = coursematch.group('coursetitle')
+         continue
+
+      if sectionmatch:
+         stack.append(term)
+         stack.append(dept)
+         stack.append(number)
+         stack.append(title)
+         
+         stack.append(sectionmatch.group('section'))
+         stack.append(sectionmatch.group('regcode'))
+         stack.append(sectionmatch.group('type'))
+         stack.append(sectionmatch.group('credits'))
+
+         if daysmatch:
+            stack.append(daysmatch.group('days'))
+         else:
             stack.append('')
-            outputtxt = outputtxt + string.join(stack, ',') + '\n'
-            stack = []
-        elif inetsectionmatch:
-            #i = i+1
-            stack.append(inetsectionmatch.group('section'))
-            stack.append(inetsectionmatch.group('regcode'))
-            stack.append(inetsectionmatch.group('type'))
-            stack.append(inetsectionmatch.group('credits'))
-            stack.extend(['','',''])
-            stack.append(inetsectionmatch.group('classroom'))
-            stack.append(inetsectionmatch.group('instructor'))
+
+         if timesmatch:
+            stack.append(timesmatch.group('starttime'))
+            stack.append(timesmatch.group('endtime'))
+         else:
+            stack.extend(['', ''])
+
+         if classroommatch:
+            stack.append(classroommatch.group('classroom'))
+         else:
             stack.append('')
-            outputtxt = outputtxt + string.join(stack, ',') + '\n'
-            stack = []
-        elif specsectionmatch:
-            #i = i+1
-            stack.append(specsectionmatch.group('section'))
-            stack.append(specsectionmatch.group('regcode'))
-            stack.append(specsectionmatch.group('type'))
-            stack.append(specsectionmatch.group('credits'))
-            stack.extend(['','',''])
-            stack.append(specsectionmatch.group('classroom'))
-            stack.append(specsectionmatch.group('instructor'))
+
+         if instructormatch:
+            stack.append(instructormatch.group('instructor'))
+         else:
             stack.append('')
-            outputtxt = outputtxt + string.join(stack, ',') + '\n'
-            stack = []
-        else:
-            stack = []
-            #print 'NOMATCH: %s' % line
+
+         stack.append('')
+            
+         outputtxt = outputtxt + string.join(stack, ',') + '\n'
+         stack = []       
     
-    #print i 
-    return outputtxt
+   return outputtxt
 
 def txttocsv(txtfile, csvfile=None):
-    if os.path.isdir(txtfile):
-        filelist = findtxts(txtfile)
-    else:
-        filelist = [txtfile]
+   if os.path.isdir(txtfile):
+      filelist = findtxts(txtfile)
+   else:
+      filelist = [txtfile]
 
-    if csvfile and os.path.isdir(csvfile):
-        datadir = csvfile
-    else:
-        #datadir = '../data/txt/'
-        datadir = '/var/data/www/unt.cameronpalmer.com/data/csv/'
+   if csvfile:
+      datadir = csvfile
+   else:
+      datadir = dirprefix + 'csv/'
 
-    if csvfile and os.path.isfile(txtfile):
-        input = open(txtfile, 'rb')
-        output = open(outfile, 'w')
-        outputtxt = parsetxt(input.readlines())
-        output.write(outputtxt)
-        input.close()
-        output.close()
-    else:
-        for origfile in filelist:
-            csvfile = re.split(r'[/\\]', origfile)[-1]
-            semesdir = re.split(r'[/\\]', origfile)[-2]
-            csvfile = csvfile.replace('.txt', '.csv')
-            if not os.path.exists(datadir + semesdir):
-            	try:
-             		os.makedirs(datadir + semesdir, 0755)
-            	except OSError, e:
-            		raise e
-            csvfile = datadir + semesdir + '/' + csvfile
-            input = open(origfile, 'rb')
-            output = open(csvfile, 'w')
-            outputtxt = parsetxt(input.readlines())
-            output.write(outputtxt)
-            input.close()
-            output.close()
+   for origfile in filelist:
+      csvfile = re.split(r'[/\\]', origfile)[-1]
+      semesdir = re.split(r'[/\\]', origfile)[-2]
+      csvfile = csvfile.replace('.txt', '.csv')
+      if not os.path.exists(datadir + semesdir):
+         try:
+            os.makedirs(datadir + semesdir, 0755)
+         except OSError, e:
+            raise e
+      csvfile = datadir + semesdir + '/' + csvfile
+      input = open(origfile, 'rb')
+      output = open(csvfile, 'w')
+      outputtxt = parsetxt(input.readlines())
+      output.write(outputtxt)
+      input.close()
+      output.close()
             
 
 def findtxts(directory):
@@ -167,38 +149,33 @@ def findpdfs(directory):
                 stack.append(fullname)
     return files
 
-def pdftotext(pdffile, textfile=None):
-    """Convert the PDF versions of the schedule to TXT files"""
+def pdftotxt(pdffile, txtfile=None):
+   """Convert the PDF versions of the schedule to TXT files"""
 
-    if os.path.isdir(pdffile):
-        filelist = findpdfs(pdffile)
-    else:
-        filelist = [pdffile]
+   if os.path.isdir(pdffile):
+       filelist = findpdfs(pdffile)
+   else:
+       filelist = [pdffile]
 
-    if textfile and os.path.isdir(textfile):
-        datadir = textfile
-    else:
-        #datadir = '../data/txt/'
-        datadir = '/var/data/www/unt.cameronpalmer.com/data/txt/'
+   if textfile and os.path.isdir(txtfile):
+      datadir = txtfile
+   else:
+      datadir = dirprefix + 'txt/'
 
-    if textfile and os.path.isfile(textfile):
-        handle = os.popen('pdftotext -layout %s %s' % (pdffile, textfile))
-        handle.close()
-    else:
-        for origfile in filelist:
-            textfile = re.split(r'[/\\]', origfile)[-1]
-            semesdir = re.split(r'[/\\]', origfile)[-2]
-            textfile = textfile.replace('.pdf', '.txt')
-            if not os.path.exists(datadir + semesdir):
-            	try:
-             		os.makedirs(datadir + semesdir, 0755)
-            	except OSError, e:
-            		raise e
-            textfile = datadir + semesdir + '/' + textfile          
+   for origfile in filelist:
+      txtfile = re.split(r'[/\\]', origfile)[-1]
+      semesdir = re.split(r'[/\\]', origfile)[-2]
+      txtfile = txtfile.replace('.pdf', '.txt')
+      if not os.path.exists(datadir + semesdir):
+         try:
+            os.makedirs(datadir + semesdir, 0755)
+         except OSError, e:
+            raise e
+      txtfile = datadir + semesdir + '/' + txtfile          
             
-            handle = os.popen('pdftotext -layout %s %s' % (origfile, textfile))
-            handle.close()
-    return textfile
+      handle = os.popen('pdftotext -layout %s %s' % (origfile, txtfile))
+      handle.close()
+   return txtfile
     
 if __name__ == '__main__':
     """Convert the PDF versions of the schedule to TXT files"""
@@ -206,10 +183,9 @@ if __name__ == '__main__':
     if len(sys.argv) > 1:
         pdffile =  sys.argv[1].strip()
     else:
-        #pdffile = '../data/pdf/'
-        pdffile = '/var/data/www/unt.cameronpalmer.com/data/pdf/'
+        pdffile = dirprefix + 'pdf/'
 
-    #textfile = pdftotext(pdffile)
-    textfile = '../data/txt/1061/accounting_1061.txt'
-    #textfile = '/var/data/www/unt.cameronpalmer.com/data/txt/'
+    textfile = pdftotext(pdffile)
+    #textfile = dirprefix + 'txt/1061/mathematics_1061.txt'
+    textfile = dirprefix + 'txt/'
     csvfile = txttocsv(textfile)
