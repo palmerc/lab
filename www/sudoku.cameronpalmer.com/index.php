c3e3ff448c223a750beaa3f37630c1def7faa618
palmerc <palmerc@9c8e43f7-b2fe-0310-8927-e999a62a3b53>
2006-02-15 07:14:30 +0000

    A starting point

---
+++ www/sudoku.cameronpalmer.com/index.php
@@ -0,0 +1,40 @@
+<html>
+<head>
+</head>
+<body>
+<h1>Sudoku</h1>
+<p>So I want a web-based Sudoku solver, and maybe a generator. The idea is to solve
+the Permuation Bipartite Graph using a Pile and Chain Exclusion, from ideas 
+culled from Dr. Dobb's.</p>
+<table border="1">
+<tr>
+<td>1</td><td>2</td><td>3</td><td>4</td><td>5</td><td>6</td><td>7</td><td>8</td><td>9</td>
+</tr>
+<tr>
+<td>1</td><td>2</td><td>3</td><td>4</td><td>5</td><td>6</td><td>7</td><td>8</td><td>9</td>
+</tr>
+<tr>
+<td>1</td><td>2</td><td>3</td><td>4</td><td>5</td><td>6</td><td>7</td><td>8</td><td>9</td>
+</tr>
+<tr>
+<td>1</td><td>2</td><td>3</td><td>4</td><td>5</td><td>6</td><td>7</td><td>8</td><td>9</td>
+</tr>
+<tr>
+<td>1</td><td>2</td><td>3</td><td>4</td><td>5</td><td>6</td><td>7</td><td>8</td><td>9</td>
+</tr>
+<tr>
+<td>1</td><td>2</td><td>3</td><td>4</td><td>5</td><td>6</td><td>7</td><td>8</td><td>9</td>
+</tr>
+<tr>
+<td>1</td><td>2</td><td>3</td><td>4</td><td>5</td><td>6</td><td>7</td><td>8</td><td>9</td>
+</tr>
+<tr>
+<td>1</td><td>2</td><td>3</td><td>4</td><td>5</td><td>6</td><td>7</td><td>8</td><td>9</td>
+</tr>
+<tr>
+<td>1</td><td>2</td><td>3</td><td>4</td><td>5</td><td>6</td><td>7</td><td>8</td><td>9</td>
+</tr>
+</table>
+</body>
+
+</html>
