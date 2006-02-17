60d83cb06a0ec69981f12c80860e3119984117f6
palmerc <palmerc@9c8e43f7-b2fe-0310-8927-e999a62a3b53>
2006-02-17 16:57:19 +0000

    Finished 4930 homework 2

--- classes/csce4930/homework_2.pl
+++ classes/csce4930/homework_2.pl
@@ -2,12 +2,15 @@
 
 use strict;
 
-my ($die, $coin, $accum, $move);
+my ($die, $coin, $accum, $move, $runs);
 $accum = 0;
 $move = 0;
+$runs = 0;
 
 srand();
 
+while (1) {
+   
 for (my $i=0; $i < 100000; $i++) {
    
    $coin = int(rand(2));
@@ -15,12 +18,15 @@ for (my $i=0; $i < 100000; $i++) {
    $die += 1;
    if ($coin == 0) {
       $move = -1 * $die;
-      print "MOVE: $move\n";
+#      print "MOVE: $move\n";
    } else {
       $move = 1 * $die;
-      print "MOVE: $move\n";
+#      print "MOVE: $move\n";
    }
    $accum += $move;
+}
+$runs++;
+print "Average " . int($accum/$runs) . "\n";
 }
 
 print "The sum of the moves is $accum\n";
