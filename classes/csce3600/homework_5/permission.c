066c16e06efa014311246242d58e22b284c7c254
palmerc <palmerc@9c8e43f7-b2fe-0310-8927-e999a62a3b53>
2006-02-24 05:36:59 +0000

    done

--- classes/csce3600/homework_5/permission.c
+++ classes/csce3600/homework_5/permission.c
@@ -5,10 +5,15 @@
 int main( int argc, char **argv ) {
    int arg, octy = 0;
    char* path = NULL;
-   char* newmode = NULL;
    struct stat statbuf;
    mode_t modes;
-      
+
+   if ( argc < 3 ) {
+      // How you avoid seg fault.
+      printf("USAGE: %s MODE FILE(S)\n\n", argv[0]);
+      return 1;
+   }
+   
    sscanf(argv[1], "%o", &octy);
    
    for ( arg = 2; arg < argc; arg++ ) {
