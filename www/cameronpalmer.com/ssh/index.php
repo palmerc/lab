d6420f190443573aff1a5d25e23456fd5db17949
palmerc <palmerc@9c8e43f7-b2fe-0310-8927-e999a62a3b53>
2006-03-11 07:24:15 +0000

    annoying webpage

--- www/cameronpalmer.com/ssh/index.php
+++ www/cameronpalmer.com/ssh/index.php
@@ -3,11 +3,11 @@ $title = "The Bitter Buffalo";
 $section = "Java SSH/Telnet Application";
 
 require ("../cp-template.php");
-?>
-<img class="leftside" src="<? echo "{$ptr}i/ramblo.jpg" ?>" alt="Ramblo the SSH Blowfish" width="72" height="54" border="0" />
-<?
+
 if ($_POST['applet']) {
-   echo'<p>Be patient this applet is pretty big so it may take a minute to load. Closing this browser window will terminate the applet</p>';
+   echo'
+   <p>Be patient this applet is pretty big so it may take a minute to load. Closing this browser window will terminate the applet.</p>
+          ';
    echo"
    <applet name=\"javassh\" archive=\"{$ptr}ssh/mindterm-3.0.1.jar\" code=\"com.mindbright.application.MindTerm.class\" height=\"0\" width=\"0\">
           ";
@@ -21,16 +21,23 @@ if ($_POST['applet']) {
           ';  
 } else {
    echo"
-   <form method=\"post\" action=\"{$_SERVER['PHP_SELF']}\" >
-      <p>If you are planning to use this application a lot I recommend the Java Web Start version.</p>
-      <a href=\"{$ptr}ssh/mindterm_ws.jnlp\">Java Web Start Version</a>
+   <div id=\"javabox\">
+      <form method=\"post\" action=\"{$_SERVER['PHP_SELF']}\" >
+         <table>
+           <tr>
+              <td><img src=\"{$ptr}i/ramblo.jpg\" alt=\"Ramblo the SSH Blowfish\" width=\"72\" height=\"54\" border=\"0\" /></td>
+	      <td colspan=\"2\">If you are planning to use this application a lot I recommend the Java Web Start version.</td>
+	   </tr>
+           <tr>
+	      <td>&nbsp</td>
+	      <td><a href=\"{$ptr}ssh/mindterm_ws.jnlp\">Java Web Start Version</a></td>
+	      <td><input type=\"submit\" name=\"applet\" value=\"Java Applet\" /></td>
+	   </tr>
+         </table>
+      </form>
+   
+      <p>The client is <a href=\"http://www.appgate.com/mindterm\">MindTerm</a> 3.0.1, supporting SSH v1 and v2.</p>
+      <a href=\"http://www.java.com\"><img src=\"{$ptr}i/get_java_red_button.gif\" alt=\"Get Java Here\" /></a>
+   </div>
           ";
-   echo'
-      <input type="submit" name="applet" value="Java Applet" />
-   </form>
-          ';
 }
-?>
-<p>The client is <a href="http://www.appgate.com/mindterm">MindTerm</a> 3.0.1, supporting SSH v1 and v2.</p>
-<a href="http://www.java.com"><img src="<? echo"{$ptr}i/get_java_red_button.gif" ?>" alt="Get Java Here" /></a>
-
