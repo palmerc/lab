6f5876e93cbf20018ddd5b7f8e86dd1dab084ef6
palmerc <palmerc@9c8e43f7-b2fe-0310-8927-e999a62a3b53>
2006-04-15 01:57:26 +0000

    Latest changes

--- classes/csce2610/project/mips_version/adventure.asm
+++ classes/csce2610/project/mips_version/adventure.asm
@@ -5,6 +5,8 @@
     .data
         moves: .word 1,2,3,1,1
         moves_size: .word 5
+        potion: .word 0
+        sword: .word 0
     .text
 
 main:
@@ -65,103 +67,245 @@ ifTen:
     addi $s1, $s1, -1
     jne $s1, $zero, while
 
+###
+### function StateZero
+### accepts: $a0, current state, $a1, direction to move
+### returns: $v0, new state
+###
 StateZero:
     addi $sp, $sp, -4
     sw $ra, 0($sp)
     
+    add $t0, $zero, $a1
     # if StateZero and Direction East Goto StateOne
+    addi $t1, $zero, 1
+    bne $t0, $t1, ExitZero
+    addi $v0, $zero, 3
+    j ExitZero:
     
+ExitZero:
     lw $ra, 0($sp)
     addi $sp, $sp, 4
     jr $ra
 
+###
+### function StateOne
+### accepts: $a0, current state, $a1, direction to move
+### returns: $v0, new state
+###
 StateOne:
     addi $sp, $sp, -4
     sw $ra, 0($sp)
+
+    add $t0, $zero, $a1
     # if StateOne and Direction North Goto StateTwo
+NorthOne:    
+    bne $t0, $zero, EastOne
+    addi $v0, $zero, 2
+    j ExitOne
     # if StateOne and Direction East Goto StateThree
+EastOne:
+    addi $t1, $zero, 1
+    bne $t0, $t1, SouthOne
+    addi $v0, $zero, 3
+    j ExitOne
     # if StateOne and Direction South Goto StateSix
+SouthOne:
+    addi $t1, $zero, 2
+    bne $t0, $t1, WestOne
+    addi $v0, $zero, 6
+    j ExitOne
     # if StateOne and Direction West Goto StateZero
+WestOne:
+    addi $t1, $zero, 3
+    bne $t0, $t1, ExitOne
+    addi $v0, $zero, 0
+    j ExitOne
+ExitOne:
     lw $ra, 0($sp)
     addi $sp, $sp, 4
     jr $ra
     
+###
+### function StateTwo
+### accepts: $a0, current state, $a1, direction to move
+### returns: $v0, new state
+###
 StateTwo:
     addi $sp, $sp, -4
     sw $ra, 0($sp)
     # Assert Potion
+    addi $t0, $zero, 1
+    la $t1, potion
+    sw $t0, 0($t1)
+        
+    add $t0, $zero, $a1
     # if StateTwo and Direction South Goto StateOne
+    addi $t1, $zero, 2
+    bne $t0, $t1, ExitTwo
+    addi $v0, $zero, 6
+    j ExitTwo
+
+ExitTwo:
     lw $ra, 0($sp)
     addi $sp, $sp, 4
     jr $ra
-    
+   
+###
+### function StateThree
+### accepts: $a0, current state, $a1, direction to move
+### returns: $v0, new state
+###   
 StateThree:
     addi $sp, $sp, -4
     sw $ra, 0($sp)
+    
+    add $t0, $zero, $a1
     # if StateThree and Direction North Goto StateFour
+NorthThree:
+    bne $t0, $zero, EastThree
+    addi $v0, $zero, 4
+    j ExitThree
     # if StateThree and Direction East Goto StateFive
+EastThree:
+    addi $t1, $zero, 1
+    bne $t0, $t1, WestThree
+    addi $v0, $zero, 5
+    j ExitThree
     # if StateThree and Direction West Goto StateOne
+WestThree:
+    addi $t1, $zero, 3
+    bne $t0, $t1, ExitThree
+    addi $v0, $zero, 1
+    j ExitThree
+
+ExitThree:
     lw $ra, 0($sp)
     addi $sp, $sp, 4
     jr $ra
     
+###
+### function StateFour
+### accepts: $a0, current state, $a1, direction to move
+### returns: $v0, new state
+###
 StateFour:
     addi $sp, $sp, -4
     sw $ra, 0($sp)
-    # if StateFour and Direction South Goto StateThree
+    
+    add $t0, $zero, $a1
     # if StateFour and Direction East Goto StateFive
+    addi $t1, $zero, 1
+    bne $t0, $t1, SouthFour
+    addi $v0, $zero, 5
+    j ExitFour
+    # if StateFour and Direction South Goto StateThree
+SouthFour:
+    addi $t1, $zero, 2
+    bne $t0, $t1, ExitFour
+    addi $v0, $zero, 3
+    j ExitFour
+    
+ExitFour:
     lw $ra, 0($sp)
     addi $sp, $sp, 4
     jr $ra
     
+###
+### function StateFive
+### accepts: $a0, current state, $a1, direction to move
+### returns: $v0, new state
+###
 StateFive:
     addi $sp, $sp, -4
     sw $ra, 0($sp)
+    add $t0, $zero, $a1
     # if StateFive and Direction West Goto StateFour
     # if StateFive and Direction South Goto StateThree
+
+ExitFive:
     lw $ra, 0($sp)
     addi $sp, $sp, 4
     jr $ra
     
+###
+### function StateSix
+### accepts: $a0, current state, $a1, direction to move
+### returns: $v0, new state
+###
 StateSix:
     addi $sp, $sp, -4
     sw $ra, 0($sp)
+    add $t0, $zero, $a1
     # if StateSix and Direction West Goto StateSeven
     # if StateSix and Direction East Goto StateEight
+ExitSix:
     lw $ra, 0($sp)
     addi $sp, $sp, 4
     jr $ra
-    
+
+###
+### function StateSeven
+### accepts: $a0, current state, $a1, direction to move
+### returns: $v0, new state
+###
 StateSeven:
     addi $sp, $sp, -4
     sw $ra, 0($sp)
-    # Asset Sword
+    # Assert Sword
+    addi $t0, $zero, 1
+    la $t1, sword
+    sw $t0, 0($t1)
+    
+    add $t0, $zero, $a1
     # if StateSeven and Direction East Goto StateSix
+ExitSeven:
     lw $ra, 0($sp)
     addi $sp, $sp, 4
     jr $ra
-    
+
+###
+### function StateEight
+### accepts: $a0, current state, $a1, direction to move
+### returns: $v0, new state
+###
 StateEight:
     addi $sp, $sp, -4
     sw $ra, 0($sp)
+    add $t0, $zero, $a1
     # if StateEight and Sword Goto StateTen
     # if StateEight and No Sword Goto StateNine
+ExitEight:
     lw $ra, 0($sp)
     addi $sp, $sp, 4
     jr $ra
     
+###
+### function StateNine
+### accepts: $a0, current state, $a1, direction to move
+### returns: $v0, new state
+###
 StateNine:
     addi $sp, $sp, -4
     sw $ra, 0($sp)
+    add $t0, $zero, $a1
     # if StateNine and Potion Goto StateZero and Deassert potion
+ExitNine:
     lw $ra, 0($sp)
     addi $sp, $sp, 4
     jr $ra
-    
+
+###
+### function StateTen
+### accepts: $a0, current state, $a1, direction to move
+### returns: $v0, new state
+###
 StateTen:
     addi $sp, $sp, -4
     sw $ra, 0($sp)
+    add $t0, $zero, $a1
     # if StateTen Assert WIN
+ExitTen:
     lw $ra, 0($sp)
     addi $sp, $sp, 4
     jr $ra
