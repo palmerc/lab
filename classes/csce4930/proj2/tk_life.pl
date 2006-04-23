49b42c270689e1dfe713b979802a5c383d50b4bc
palmerc <palmerc@9c8e43f7-b2fe-0310-8927-e999a62a3b53>
2006-04-23 22:16:35 +0000

    Select a shape now works

--- classes/csce4930/proj2/tk_life.pl
+++ classes/csce4930/proj2/tk_life.pl
@@ -207,6 +207,10 @@ sub drawGUI {
                 -activebackground=>'grey',
                 -command=>\&stop);
         
+        $file_mb->command(-label=>'To PostScript',
+                -activebackground=>'grey',
+                -command=>\&postit);
+        
         $file_mb->separator();
 
         $file_mb->command(-label=>'Exit',
@@ -221,7 +225,9 @@ sub drawGUI {
         $edit_mb->command(-label=>'Grid Size',
                 -activebackground=>'grey',
                 -command=> \&grid_dialog);
-
+        $edit_mb->command(-label=>'Shape',
+                -activebackground=>'grey',
+                -command=> \&shape_dialog);
 
     my $help_mb=$menu_bar->Menubutton(-text=>'Help',
                 -background=>'grey',
@@ -241,6 +247,7 @@ sub gridDraw {
     if ($canvas) {
         $canvas->destroy();
     }
+    
     $canvas = $main->Canvas(-bg => 'grey',
          -width  => $yMax * $size,
          -height => $xMax * $size,
@@ -262,19 +269,76 @@ sub gridDraw {
     }
                  
     # Fill in the starting pattern.
-    fill($canvas);    
+    fill($canvas);
+}
+
+sub shape_dialog {
+    my $shape;
+    my $popup=$main->DialogBox(-title=>"Shapes", -background => 'grey',
+            -buttons=>["OK", "Cancel"],);
+    
+    my $right1 = $popup->Frame(-background => 'grey')->pack(-side => 'left', -pady => 2, -padx => 2);
+    my $des1 = $right1->Label(-text => 'Choose from', -background => 'grey', -pady => 4)->pack(-side => 'left');
+    my $rdb0 = $right1-> Radiobutton(-text=>"Gosper Glider Gun", -background => 'grey',
+            -activebackground=>'grey', -highlightbackground => 'grey', -value=>"gosper",  -variable=>\$shape)->pack(-side => 'top', -anchor=>'w');
+    my $rdb1 = $right1-> Radiobutton(-text=>"Lightweight Spaceship", -background => 'grey',
+            -activebackground=>'grey', -highlightbackground => 'grey', -value=>"lwss",  -variable=>\$shape)->pack(-side => 'top', -anchor=>'w');
+    my $rdb2 = $right1-> Radiobutton(-text=>"Cameron", -background => 'grey',
+            -activebackground=>'grey', -highlightbackground => 'grey', -value=>"cameron",  -variable=>\$shape)->pack(-side => 'top', -anchor=>'w');
+    my $rdb3 = $right1-> Radiobutton(-text=>"Acorn", -background => 'grey',
+            -activebackground=>'grey', -highlightbackground => 'grey', -value=>"acorn",  -variable=>\$shape)->pack(-side => 'top', -anchor=>'w');
+    my $rdb4 = $right1-> Radiobutton(-text=>"Diehard", -background => 'grey',
+            -activebackground=>'grey', -highlightbackground => 'grey', -value=>"diehard",  -variable=>\$shape)->pack(-side => 'top', -anchor=>'w');
+
+    my $button = $popup->Show();
+    
+    if ($button eq "OK") {
+        initializeArray();
+        if ($shape eq "cameron") {
+            copyShapeInto(@cameron);
+        } elsif ($shape eq "gosper") {
+            copyShapeInto(@gosper);
+        } elsif ($shape eq "lwss") {
+            copyShapeInto(@lwss);
+        } elsif ($shape eq "acorn") {
+            copyShapeInto(@acorn);
+        } elsif ($shape eq "diehard") {
+            copyShapeInto(@diehard);
+        } else {
+            print "How on earth did you get here?\n";
+        }
+            
+        gridDraw();
+    }
+}
+
+sub postit {
+    my $popup=$main->DialogBox(-title=>"PostScript Ouput", -background => 'grey',
+            -buttons=>["OK", "Cancel"],);
+    my $right1 = $popup->Frame(-background => 'grey')->pack(-side => 'left', -pady => 2, -padx => 2);
+    my $des1 = $right1->Label(-text => 'File name', -background => 'grey', -pady => 4)->pack();
+    my $right2 = $popup->Frame(-background => 'grey')->pack(-side => 'left', -pady => 2, -padx => 2);
+    my $ent0 = $right2->Entry(-width => 15)->pack(-anchor=>'n',-fill=>'x');
+    
+    my $button = $popup->Show();
+    
+    if ($button eq "OK") {
+        my $file = $ent0->get();
+        $canvas->update();
+        $canvas->postscript(-file => $file);
+    }
 }
 
 sub about_txt {
   my $popup=$main->DialogBox(-title=>"About",
-  -buttons=>["OK"],);
+            -buttons=>["OK"],);
   $popup->add("Label", -text=>"Conway's Game of Life\nCameron Palmer, Mitra Mahadavian, and Kristina Ratliff\nCSCE 4930\nApril 2006\n")->pack;
   $popup->Show;
 }
 
 sub grid_dialog {
-  my $popup=$main->DialogBox(-title=>"Grid Size", -background => 'grey',
-  -buttons=>["OK", "Cancel"],);
+    my $popup=$main->DialogBox(-title=>"Grid Size", -background => 'grey',
+            -buttons=>["OK", "Cancel"],);
   
     my $right1 = $popup->Frame(-background => 'grey')->pack(-side => 'left', -pady => 2, -padx => 2);
     my $des1 = $right1->Label(-text => 'X Size', -background => 'grey', -pady => 4)->pack();
@@ -293,7 +357,6 @@ sub grid_dialog {
             $xMax = $x;
             $yMax = $y;
             initializeArray();
-            copyShapeInto(@gosper);
             gridDraw();
         }
     }
@@ -329,6 +392,5 @@ sub unfill {
 ####
 
 initializeArray();
-copyShapeInto(@gosper);
 drawGUI();
 MainLoop();
