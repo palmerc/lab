41002a1eede711e7c00e3d49da955adcb7c88fc0
palmerc <palmerc@9c8e43f7-b2fe-0310-8927-e999a62a3b53>
2006-05-03 16:30:10 +0000

    Latest broken code

--- classes/csce3600/ysh/ysh.c
+++ classes/csce3600/ysh/ysh.c
@@ -60,30 +60,24 @@ int profile_importer(char *prompt) {
     while ((fgets(file_line, MAX_LINE, in)) != NULL) {
         result = strtok(file_line, delims);
         while (result != NULL) {
-            //printf("I was given ->%s\n", result);
             int start = 0;
             int stop = 0;
             if (strcmp(result, "PROMPT") == 0) {
                 result = strtok(NULL, delims);
                 
                 start = strcspn(result, single_quote) + 1;
-                //stop = 7;
                 stop = strlen(result);
                 substr(result, start, stop);
                 
                 start = 0;
                 stop = strcspn(result, single_quote);
-                //stop = 7;
-                //printf("STOP %d\n", stop);
                 substr(result, start, stop);
                 
                 strncpy(prompt, result, MAX_LINE);
-                //printf("%s", prompt);
             }
             if (strcmp(result, "PATH") == 0) {
                 result = strtok(NULL, delims);
                 strncpy(search_path, result, MAX_LINE);
-                //printf("Search PATH=%s\n", search_path);
             }
             result = strtok(NULL, delims);
         }
@@ -92,7 +86,7 @@ int profile_importer(char *prompt) {
     return 0;   
 }
 
-void parse_cl(char *line, char **env_argv) {
+void parse_cl(char *line, char **env_argv, size_t *env_argv_len) {
     char *copy = line;
     char temp[MAX_LINE + 1];
     char *temp_ptr = temp;
@@ -103,18 +97,14 @@ void parse_cl(char *line, char **env_argv) {
 
     bzero(temp, MAX_LINE + 1);
     strncpy(temp, line, strlen(copy));
-    //printf("parse_cl start=>%s\n", copy);
+
     while (copy[i] != '\0') {
-        //printf("parse_cl loop =>%d\n", i);
         if (copy[i] == ' ') {
-            //printf("parse_cl space =>%d\n", j);
             substr(temp_ptr, start, i);
-            //strncat(temp, '\0', 1);
             arg = (char *)malloc(sizeof(char) * strlen(temp));
             strncpy(arg, temp, strlen(temp));
             env_argv[j] = arg;
             
-            //printf("parse_cl argv =>%s<=\n", &env_argv[j]);
             start = i + 1;
             j++;
         }
@@ -122,17 +112,13 @@ void parse_cl(char *line, char **env_argv) {
         strncpy(temp, copy, strlen(copy));
         i++;
     }
-    //printf("parse_cl final\n", j);
     substr(temp_ptr, start, strlen(copy));
     arg = (char *)malloc(sizeof(char) * strlen(temp));
     strncpy(arg, temp, strlen(temp));
     env_argv[j] = arg;
     
-    //strncpy(&env_argv[j], temp_ptr, strlen(temp));
     for (i=0; i < sizeof(&env_argv); i++)
         printf("parse_cl argv[%d] =>%s<=\n", i, &env_argv[i]);
-    //printf("parse_cl end=>%s\n", temp);
-    //strncpy(&env_argv[j++], NULL, 1);   
 }
 
 int main()
@@ -140,20 +126,21 @@ int main()
     char line[MAX_LINE];
     //char *line;
     char *tmp_path;
+
     prompt = (char *)malloc(sizeof(char) * (MAX_LINE + 1));
     search_path = (char *)malloc(sizeof(char) * (MAX_LINE + 1));
     tmp_path = (char *)malloc(sizeof(char) * (MAX_LINE + 1));
+
 	signal(SIGINT, SIG_IGN);
 	signal(SIGINT, handle_signal);
-    //line = (char *)malloc(sizeof(char) * (MAX_LINE + 1));    
+
     pid_t pid, child_pid;
     int stat_val;
-    //char env_argv[MAX_LINE];
+
     char *env_argv[100];
-    //env_argv = (char *)malloc(sizeof(char) * (MAX_LINE + 1));
+    size_t env_argv_len;
     
     profile_importer(prompt);
-    
     char *env_envp[2];
     bzero(env_envp, sizeof(char) * 2);
     
@@ -163,7 +150,6 @@ int main()
     free(tmp_path);
     env_envp[0] = search_path;
     env_envp[1] = 0;
-    //printf("ENV %s", env_envp[0]);    
     printf("%s", prompt);
     
     /* The main loop terminates on Ctrl-D */
@@ -172,19 +158,16 @@ int main()
         if (line[strlen(line) - 1] == '\n')
             line[strlen(line) - 1] = '\0';
         
+        parse_cl(line, env_argv, &env_argv_len);
+        
         pid = fork();
         switch(pid) {
             case -1:
                 perror("fork() error");
                 break;
             case 0:
-                bzero(env_argv, 100);
-                parse_cl(line, env_argv);
                 int i = 0;
-                //for (i=0; i < sizeof(env_argv); i++)
-                //    printf("parse_cl argv[%d] =>%s<=\n", i, env_argv[i]);
                 execlp(env_argv, env_argv, NULL);
-                //printf("Exec Failure\n");
                 exit(0);
                 break;
             default:
