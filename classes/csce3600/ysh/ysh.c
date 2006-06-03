7b92e0906609906c8b507bbea8fec2f967de423f
palmerc <palmerc@9c8e43f7-b2fe-0310-8927-e999a62a3b53>
2006-06-03 15:33:04 +0000

    moving main.c to ysh.c

--- classes/csce3600/ysh/ysh.c
+++ classes/csce3600/ysh/ysh.c
@@ -5,10 +5,17 @@
 #include <string.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <wait.h>
+#include <ctype.h>
 #include <sys/param.h>
 
 const size_t MAX_LINE = 256;
+
+typedef struct {
+    char *cmd;
+    char *cmd_loc;
+    char **argv;
+    int *argv_count;
+} command_t;
 
 /* function prototype */
 pid_t waitpid(pid_t pid, int *status, int options);
@@ -24,9 +31,14 @@ int profile_importer(char **prompt, char **search_path) {
     /* fopen returns NULL if unable to open */
     in = fopen(".ysh_profile", "r");
     if (in == NULL) {
+		perror(".ysh_profile");
+		result = "ysh % ";
+		*prompt = (char *)malloc(sizeof(char) * (strlen(result) + 1));
+		strcpy(*prompt, result); 
+		result = "/bin:/sbin:/usr/bin:/usr/sbin";
+		*search_path = (char *)malloc(sizeof(char) * (strlen(result) + 1));
+		strcpy(*search_path, result);
         return 1;
-    } else {
-        perror(".ysh_profile");
     }
     
     /* fgets returns a null pointer when it encounters EOF */
@@ -52,6 +64,7 @@ int profile_importer(char **prompt, char **search_path) {
                 *prompt = (char *)malloc(sizeof(char) * (strlen(result) + 1));
                 strcpy(*prompt, result);
             }
+			
             if (strcmp(result, "PATH") == 0) {
                 result = strtok(NULL, delims);
                 *search_path = (char *)malloc(sizeof(char) * (strlen(result) + 1));
@@ -79,40 +92,105 @@ void substr(char *string, int start, int stop) {
     free(buf_temp);
 }
 
-void parse_cl(char *line, char **env_argv, size_t *env_argv_len) {
-    char *buf = malloc(sizeof(char) * (strlen(line) + 1));
+void parse_cl(char *line, command_t *command_list, size_t *command_list_len) {
+    char *buf;
+    buf = (char *)malloc(sizeof(char) * (strlen(line)+1));
     char *arg;
-    *env_argv_len = 0;
     
-    strcpy(buf, line);
+    strncpy(buf, line, strlen(line)+1);
     
-    int i = 0;
-    int j = 0;
+    int bg_process = 0;
+    int pipe_count = 0; /* number of pipes */
+    int arg_count = 0; /* number of arguments */
+    int i = 0; /* position in the string */
     int start = 0;
+    //int end = 0;
+    command_list[pipe_count].argv = (char **)malloc(sizeof(char *)*256);
     while (buf[i] != '\0') {
-        if (buf[i] == ' ') {
+        if (buf[i] == ' ' || buf[i] == '\t' || buf[i] == '\n') {
+            /* This means this is a seperate thing */
+            /* Turn the argument into a little string and add it to the array */
             substr(buf, start, i-1);
             if (strlen(buf) > 0) {
-                arg = (char *)malloc(sizeof(char) * strlen(buf)+1);
-                strcpy(arg, buf);
-                env_argv[j] = arg;
-                j++;
+                arg = (char *)malloc(sizeof(char) * (strlen(buf)+1));
+                strncpy(arg, buf, strlen(buf)+1);
+                //printf("space: %s\n", arg);
+                command_list[pipe_count].argv[arg_count] = arg;
+                arg_count++;
             }
             start = i+1;
+            //printf("%s\n", buf);
+            strncpy(buf, line, strlen(buf)+1);
+        } else if (buf[i] == '.') {
+            //printf("Dot\n");
+            /* If I find a dot, should mean the current directory */
+            
+        } else if (buf[i] == '-') {
+            //printf("Minus\n");
+            /* This means an option to a command */
+        } else if (buf[i] == '*') {
+            //printf("Asterisk\n");
+            /* This is a wildcard and should be expanded */
+        } else if (buf[i] == '?') {
+            //printf("Question\n");
+            /* This is a single character wildcard and should be expanded */
+        } else if (buf[i] == '~') {
+            //printf("Tilde\n");
+            /* This means the users home directory */
+        } else if (buf[i] == '&') {
+            //printf("Ampersand\n");
+            /* This indicates a background process
+               or if following a > (eg. 2>&1 tie stdout to stderr) */
+            bg_process = 1;
+        } else if (buf[i] == '|') {
+            //printf("Pipe\n");
+            /* Connect stdout of the previous app to the stdin of the next */
+            /* Close out the previous string before moving to the new one */
+            substr(buf, start, i-1);
+            if (strlen(buf) > 0) {
+                arg = (char *)malloc(sizeof(char *) * (strlen(buf)+1));
+                strncpy(arg, buf, strlen(buf)+1);
+                printf("pipe: %s\n", arg);
+                command_list[pipe_count].argv[arg_count] = arg;
+                arg_count++;
+            }
+            start = i+1;
+            //printf("%s\n", buf);
+            strncpy(buf, line, strlen(buf)+1);
+            /* Close out the previous one */
+            //printf("Pipecount %d Argcount %d\n", pipe_count, arg_count);
+            //command_list[pipe_count].argv[arg_count] = NULL;
+            
+            arg_count = 0;
+            command_list[++pipe_count].argv = (char **)malloc(sizeof(char *)*256);
+        } else if (buf[i] == '>') {
+            //printf("Stdout\n");
+            /* Connect stdout to whatever follows */
+        } else if (buf[i] == '<') {
+            //printf("Stdin\n");
+            /* Connect stdin to whatever follows */
+        } else if (buf[i] == '/' 
+                || isdigit(buf[i]) 
+                || isalpha(buf[i])) {
+            //printf("Character: %c\n", buf[i]);
+            /* A regular character */
+        } else {
+            printf("error parsing");
         }
-        strcpy(buf, line);
         i++;
     }
-    substr(buf, start, strlen(line)-1);
+    /* Null was encountered so grab the last thing on the line */
+    substr(buf, start, i-1);
     if (strlen(buf) > 0) {
-        arg = (char *)malloc(sizeof(char) * strlen(buf)+1);
-        strcpy(arg, buf);
-        env_argv[j] = arg;
-        j++;
+        arg = (char *)malloc(sizeof(char) * (strlen(buf)+1));
+        strncpy(arg, buf, strlen(buf)+1);
+        command_list[pipe_count].argv[arg_count] = arg;
+        arg_count++;
     }
-    env_argv[j] = NULL;
-    *env_argv_len = j;
-    free(buf);
+    /* Close the last item */
+    //printf("Lastcount %d Argcount %d\n", pipe_count, arg_count);
+    //command_list[pipe_count].argv[arg_count] = NULL;
+    *command_list_len = pipe_count + 1;
 }
 
 void path_finder(char *search_path, char *prog_name) {
@@ -138,18 +216,18 @@ void path_finder(char *search_path, char *prog_name) {
     }
 }
 
-int main()
-{
+int main() {
     char *prompt;
     char *search_path;
     pid_t pid;
-    int stat_val;
+    pid_t pid_two;
+	int fd[2];
     
     char line[MAX_LINE];
 
-    size_t env_argv_len;
-    char **env_argv;
-    env_argv = (char**) malloc(sizeof(char *) * MAX_LINE);
+    size_t command_list_len;
+    command_t *command_list;
+    command_list = (command_t*)malloc(sizeof(command_t)*MAX_LINE);
     
     /* Ignore Ctrl-C */
     signal(SIGINT, SIG_IGN);
@@ -157,35 +235,101 @@ int main()
     
     printf("%s", prompt);
     
+	/* Before calling fork we create the pipe */
+    if (pipe(fd) < 0)
+        perror("pipe error");
+    
+    int readflags = fcntl(fd[0], F_GETFD, 0);
+    int writeflags = fcntl(fd[1], F_GETFD, 0);
+       
+    readflags |= FD_CLOEXEC;
+    writeflags |= FD_CLOEXEC;
+    fcntl(fd[0], F_SETFD, readflags);
+    fcntl(fd[1], F_SETFD, writeflags);
+
     /* The main loop terminates on Ctrl-D */
 	while(fgets(line, MAX_LINE, stdin) != NULL) {
         if (line[strlen(line) - 1] == '\n')
             line[strlen(line) - 1] = '\0';
         
-        parse_cl(line, env_argv, &env_argv_len);
+        parse_cl(line, command_list, &command_list_len);
         
+		int i = 0;
+		
         pid = fork();
+        
         switch(pid) {
             case -1:
                 perror("fork() error");
                 break;
             case 0:
-                path_finder(search_path, env_argv[0]);
-                execv(env_argv[0], env_argv);
-                int arg_index;
-                for (arg_index=0; arg_index < env_argv_len; arg_index++)
-                    free(env_argv[arg_index]);
+                printf("process: %d - last\n", i);
+                
+                for (i = 0; i < command_list_len - 1; i++) {
+                    pid_two = fork();
+                    
+                    switch(pid_two) {
+                        case -1 :
+                            perror("fork() error");
+                            break;
+                        case 0:
+                            if ((command_list_len > 1) && (i == 0)) {
+                                printf("process: 0 - first\n");
+                                close(fd[0]); /* Close the read end */
+                                if (fd[1] != STDOUT_FILENO) {
+                                    if (dup2(fd[1], STDOUT_FILENO) != STDOUT_FILENO)
+                                        perror("dup2 error to stdout");
+                                    dup2(fd[1], STDOUT_FILENO);
+                                    close(fd[1]);
+                                }
+                            } else if ((command_list_len > 1) && (i != command_list_len - 1)) {
+                                printf("process: %d - middle\n", i);
+                                if (fd[0] != STDIN_FILENO) {
+                                    if (dup2(fd[0], STDIN_FILENO) != STDIN_FILENO)
+                                        perror("dup2 error to stdin");
+                                    close(fd[0]);
+                                }
+                                if (fd[1] != STDOUT_FILENO) {
+                                    if (dup2(fd[1], STDOUT_FILENO) != STDOUT_FILENO)
+                                        perror("dup2 error to stdout");
+                                    dup2(fd[1], STDOUT_FILENO);
+                                    close(fd[1]);
+                                }
+                            }
+                            path_finder(search_path, command_list[i].argv[0]);
+                            execv(command_list[i].argv[0], command_list[i].argv);
+                            exit(0);
+                            break;
+                        default:
+                            if (waitpid(pid_two, NULL, 0) < 0)
+                                perror("waitpid error");
+
+                    } /* switch two */
+                } 
+                close(fd[1]); /* Close the write end */
+                if (fd[0] != STDIN_FILENO) {
+                    if (dup2(fd[0], STDIN_FILENO) != STDIN_FILENO)
+                        perror("dup2 error to stdin");
+                    close(fd[0]);
+                }
+                path_finder(search_path, command_list[command_list_len - 1].argv[0]);
+                execv(command_list[command_list_len - 1].argv[0], command_list[command_list_len - 1].argv);
                 exit(0);
                 break;
             default:
-                if ((pid = waitpid(pid, &stat_val, 0)) < 0)
+                if (waitpid(pid, NULL, 0) < 0)
                     perror("waitpid error");
-                printf("%s", prompt);
+
+                //path_finder(search_path, command_list[command_list_len - 1].argv[0]);
+                //execv(command_list[command_list_len - 1].argv[0], command_list[command_list_len - 1].argv);
+                //exit(0);
                 break;
-        }       
+        }        
+        printf("%s", prompt);
 	}
 	printf("\n");
     free(prompt);
     free(search_path);
 	return 0;
 }
+
