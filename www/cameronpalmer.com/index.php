febfad183dcbf1f60b22670fedbeb0b44cab98db
palmerc <palmerc@9c8e43f7-b2fe-0310-8927-e999a62a3b53>
2006-08-28 00:54:02 +0000

    bitter buffalo explanation

--- www/cameronpalmer.com/index.php
+++ www/cameronpalmer.com/index.php
@@ -14,11 +14,12 @@ would make a good name for a pub. So if you would like to use the name for a pub
 please make checks payable to Cameron Palmer. As a side note the image of the
 buffalo (american bison) that I used comes from the back of the american five
 cent piece re-issue, which happens to look strikingly similar to the Modest Mouse
-concert t-shirt that was out for this album. Unfortunately my favorite t-shirt,
-met with an untimely demise when changing the lead-acid car battery. So if you
-know where I can get a genuine, not cheap knock-off from ebay, medium sized, brown
-Modest Mouse t-shirt please contact me. In the mean time only have this logo to
-remind me of that shirt.</p>
+concert t-shirt that was out for this album (Lonesome Crowded West). 
+Unfortunately my favorite t-shirt, met with an untimely demise when changing the 
+lead-acid car battery. So if you know where I can get a genuine, not a cheap 
+knock-off from ebay, medium sized, brown Modest Mouse bitter buffalo t-shirt 
+please contact me. In the mean time I only have this logo to remind me of that 
+shirt.</p>
 <p>I am currently working on a project to provide a better search solution for
 the UNT course schedules and catalog. Come along and see the progress of 
 <a href="http://unt.cameronpalmer.com">UNT Course Search</a>.</p>
