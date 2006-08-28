4802ca3a0930bd900168cb5bd0a6bc0d0e178df7
palmerc <palmerc@9c8e43f7-b2fe-0310-8927-e999a62a3b53>
2006-08-28 00:51:15 +0000

    bitter buffalo explanation

--- www/cameronpalmer.com/index.php
+++ www/cameronpalmer.com/index.php
@@ -7,6 +7,18 @@ require ("cp-template.php");
 <p>Hi. My name is Cameron Palmer and I am a student at University of North Texas.
 This site is dedicated to my interests and documenting my work. For documents and 
 assignments checkout the Wiki section.</p>
+<p>As a point of explanation regarding the name of this website, I am not a 
+Bitter Buffalo. The Bitter Buffalo is a specific reference to a song called
+Heart Cooks Brain by Modest Mouse. Furthermore, I think the The Bitter Buffalo
+would make a good name for a pub. So if you would like to use the name for a pub
+please make checks payable to Cameron Palmer. As a side note the image of the
+buffalo (american bison) that I used comes from the back of the american five
+cent piece re-issue, which happens to look strikingly similar to the Modest Mouse
+concert t-shirt that was out for this album. Unfortunately my favorite t-shirt,
+met with an untimely demise when changing the lead-acid car battery. So if you
+know where I can get a genuine, not cheap knock-off from ebay, medium sized, brown
+Modest Mouse t-shirt please contact me. In the mean time only have this logo to
+remind me of that shirt.</p>
 <p>I am currently working on a project to provide a better search solution for
 the UNT course schedules and catalog. Come along and see the progress of 
 <a href="http://unt.cameronpalmer.com">UNT Course Search</a>.</p>
