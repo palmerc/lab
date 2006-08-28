eb6409e8d4c6c0d6950daa14b5ccb59e0245466e
palmerc <palmerc@9c8e43f7-b2fe-0310-8927-e999a62a3b53>
2006-08-28 00:33:22 +0000

    The UNT Course Search tie-in

--- www/cameronpalmer.com/index.php
+++ www/cameronpalmer.com/index.php
@@ -7,3 +7,6 @@ require ("cp-template.php");
 <p>Hi. My name is Cameron Palmer and I am a student at University of North Texas.
 This site is dedicated to my interests and documenting my work. For documents and 
 assignments checkout the Wiki section.</p>
+<p>I am currently working on a project to provide a better search solution for
+the UNT course schedules and catalog. Come along and see the progress of 
+<a href="http://unt.cameronpalmer.com">UNT Course Search</a>.</p>
