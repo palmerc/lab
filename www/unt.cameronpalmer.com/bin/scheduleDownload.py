249e7c356d600d1b66e00a678d04b69e9510355f
palmerc <palmerc@9c8e43f7-b2fe-0310-8927-e999a62a3b53>
2006-08-28 13:35:20 +0000

    Had to change the directory where the files are downloaded to be fully qualified

--- www/unt.cameronpalmer.com/bin/scheduleDownload.py
+++ www/unt.cameronpalmer.com/bin/scheduleDownload.py
@@ -14,8 +14,8 @@ if __name__ == '__main__':
     
     base = 'http://essc.unt.edu/registrar/SOCbydept/'
     url = 'SOCbydeptA.htm'
-    datadir = '../data/pdf/'
-    #datadir = '/var/www/unt.cameronpalmer.com/data/pdf/'
+    #datadir = '../data/pdf/'
+    datadir = '/var/www/unt.cameronpalmer.com/data/pdf/'
     html = urllib2.urlopen(base+url) # A file like object
     soup = BeautifulSoup(html)
     for anchor in soup('a'):
