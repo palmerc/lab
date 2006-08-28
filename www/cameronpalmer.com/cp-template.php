9f66899372f6a724946ee6a6c8b099203bf56c89
palmerc <palmerc@9c8e43f7-b2fe-0310-8927-e999a62a3b53>
2006-08-28 00:40:48 +0000

    Removing the ads and stuff

--- www/cameronpalmer.com/cp-template.php
+++ www/cameronpalmer.com/cp-template.php
@@ -34,15 +34,7 @@ function do_header() {
 
     echo'
         <link rel="shortcut icon" href="favicon.ico" type="image/vnd.microsoft.icon" />
-        <link rel="icon" href="favicon.ico" type="image/vnd.microsoft.icon" />
-        <meta name="verify-v1" content="cFzlCLLQcxEzzO7l942x6zerGHKWsB+clywkUg3HijI=" />
-	
-	<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
-	</script>
-	<script type="text/javascript">
-		_uacct="UA-68677-1";
-		urchinTracker();
-	</script>
+        <link rel="icon" href="favicon.ico" type="image/vnd.microsoft.icon" />      
         ';
 ?>
     <script type="text/javascript"><!--
@@ -64,41 +56,10 @@ function do_header() {
     </head>
 
 
-<body onload="javascript:hasIE_hideAndShow();">
+<body>
 
 <span style="position:absolute; width: 0px; height:0px; left:-1000px; top: -1000px"><img id="getIE_pingimage0"/></span>
 
-<!-- LEVEL 1: SWITCH BAR ON TOP OF PAGE --> 
-
-<div id="hasIE_level1" style="display: none;">
-    <span style="position:absolute; width: 0px; height:0px; left:-1000px; top: -1000px"><img id="getIE_pingimage1"/></span>
-    <div style="padding: 20px; background-color: #ffffbb; font-family: arial; font-size: 15px; font-weight: normal; color: #111111; line-height: 17px;">
-        <div style="width: 650px; margin: 0 auto 0 auto;">
-            <div style="padding-left: 8px; padding-top: 0px; float: right;">
-                <!-- REPLACE THIS BLOCK OF SCRIPT WITH YOUR OWN FIREFOX REFERRAL BUTTON SCRIPT -->
-                <script type="text/javascript"><!--
-                google_ad_client = "pub-2386209491833952";
-                google_ad_width = 125;
-                google_ad_height = 125;
-                google_ad_format = "125x125_as_rimg";
-                google_cpa_choice = "CAAQweaZzgEaCA2ZyYC_NXeAKK2293M";
-                //--></script>
-                <script type="text/javascript" src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
-                </script>
-            </div>
-            <strong>We see you're using Internet Explorer.&nbsp;&nbsp;Try Firefox, you'll like it better.</strong>  
-            <br /><br />
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong>&middot;</strong> Firefox blocks pop-up windows.
-            <br />
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong>&middot;</strong> It stops viruses and spyware.
-            <br />
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong>&middot;</strong> It keeps Microsoft from controlling the future of the internet.
-            <br /><br />
-            Click the button on the right to download Firefox.&nbsp;&nbsp;It's free.
-        </div>
-
-    </div>
-</div> 
 <?php
         echo'
         <div id="pbody">
@@ -141,19 +102,13 @@ function do_footer($buf) {
             <div id="footer">
                 Copyright &copy; 2005 Cameron Palmer<br />
                 cameron DOT palmer AT gmail DOT com
-                <div style="float: right;">
-                    <script type="text/javascript"><!--
-                    google_ad_client = "pub-2386209491833952";
-                    google_ad_width = 180;
-                    google_ad_height = 60;
-                    google_ad_format = "180x60_as_rimg";
-                    google_cpa_choice = "CAAQhan8zwEaCFDy0q47oTV9KMu293M";
-                    //--></script>
-                    <script type="text/javascript" src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
-                    </script>
-                </div>
             </div>
         </div>
+        <script src="http://www.google-analytics.com/urchin.js" type="text/javascript"></script>
+        <script type="text/javascript">
+            _uacct="UA-68677-1";
+            urchinTracker();
+        </script>
     </body>
 </html>
 ';
