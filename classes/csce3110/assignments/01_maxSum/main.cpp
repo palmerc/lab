6b6f45b11ed1fdb618bb57bc47883400f974cff3
palmerc <palmerc@9c8e43f7-b2fe-0310-8927-e999a62a3b53>
2006-09-06 05:28:02 +0000

    forgot to include string

--- classes/csce3110/assignments/01_maxSum/main.cpp
+++ classes/csce3110/assignments/01_maxSum/main.cpp
@@ -2,51 +2,52 @@
 #include <fstream>
 #include <vector>
 #include <ctime>
+#include <string>
 using namespace std;
 
 int maxSubsequence(vector<int> a) {
-	int i, j, k, sum = 0, max = 0;
-	for (i = 0; i < a.size() - 1; i++) {
-		for (j = i; j < a.size() - 1; j++) {
-			sum = 0;
+    int i, j, k, sum = 0, max = 0;
+    for (i = 0; i < a.size() - 1; i++) {
+        for (j = i; j < a.size() - 1; j++) {
+            sum = 0;
 			
-			for (k = i; k <= j; k++)
-				sum += a[k];
-			if (sum > max)
-				max = sum;
-		}
-	}
-	return max;
+            for (k = i; k <= j; k++)
+                sum += a[k];
+            if (sum > max)
+                max = sum;
+        }
+    }
+    return max;
 }
 
 int main () {
     clock_t start, finish;
     double time;
-	string iFileName;
-	int number;
-	vector<int> a;
+    string iFileName;
+    int number;
+    vector<int> a;
 	
     cout << "Enter the name of the file: ";
-	cin >> iFileName;
+    cin >> iFileName;
 	
-	ifstream inFile;
-	ofstream outFile;
-	inFile.open(iFileName.c_str(), ios::in);
-	outFile.open("output.txt", ios::out);
+    ifstream inFile;
+    ofstream outFile;
+    inFile.open(iFileName.c_str(), ios::in);
+    outFile.open("output.txt", ios::out);
 
-	while (inFile >> number)
-		a.push_back( (int)number );
+    while (inFile >> number)
+        a.push_back( (int)number );
 	
     start = clock();
-	int max = maxSubsequence( a );
+    int max = maxSubsequence( a );
     finish = clock();
     time = (double(finish)-double(start))/CLOCKS_PER_SEC;
-	cout << "The maximum subsequence is " << max << endl;
+    cout << "The maximum subsequence is " << max << endl;
     cout << "The time it took to execute " << time << endl;
-	outFile << "The maximum subsequence is " << max << endl;
-	outFile << "The time it took to execute " << time << endl;
+    outFile << "The maximum subsequence is " << max << endl;
+    outFile << "The time it took to execute " << time << endl;
     
-	inFile.close();
-	outFile.close();
+    inFile.close();
+    outFile.close();
     return 0;
 }
