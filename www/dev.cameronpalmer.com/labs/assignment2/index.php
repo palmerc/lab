6565ac9a28e9ec58507b5d09decd43457432b0ac
palmerc <palmerc@9c8e43f7-b2fe-0310-8927-e999a62a3b53>
2006-09-12 23:47:02 +0000

    starting assignment 2

--- www/dev.cameronpalmer.com/labs/assignment2/index.php
+++ www/dev.cameronpalmer.com/labs/assignment2/index.php
@@ -3,9 +3,27 @@
 	$section = "Familiarity Assignment 2";
 	require("../../dev-template.php");
 ?>
+
+if ($_SERVER['REQUEST_METHOD'] == "POST"){
+   
+   $name = $_POST['name'];
+   $email = $_POST['email'];
+   $sex = $_POST['sex'];
+   $age = $_POST['age'];
+   
+   echo "
+      $age<br />
+      $sex<br />
+      $email<br />
+      $name<br />
+      ";
+}
+
 <div class="form">
-   Enter name (Last, First, Middle): <input type="text" name="name" /><br />
-   Enter email address: <input type="text" name="email" /><br />
-   Enter your sex: Male <input type="radio" name="sex" /> Female <input type="radio" name="sex" /> Age: <input type="text" name="age" /><br />
-   <input type="button" name="reset" value="Reset" /> <input type="button" name="submit" value="Press here to submit your information" /><br />
+   <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+      Enter name (Last, First, Middle): <input type="text" name="name" /><br />
+      Enter email address: <input type="text" name="email" /><br />
+      Enter your sex: Male <input type="radio" name="sex" /> Female <input type="radio" name="sex" /> Age: <input type="text" name="age" /><br />
+      <input type="button" name="reset" value="Reset" /> <input type="submit" name="submit" value="Press here to submit your information" size="3" /><br />
+   </form>
 </div>
