79b0a791f9eac9bc73d4d6b5ec52e728c55eae1b
palmerc <palmerc@9c8e43f7-b2fe-0310-8927-e999a62a3b53>
2006-09-12 22:11:39 +0000

    software development

---
+++ www/dev.cameronpalmer.com/index.php
@@ -0,0 +1,28 @@
+<?php
+	$title = "CSE 4410 - Software Development 1";
+	$section = "Welcome";
+	require("dev-template.php");
+?>
+	<div id="frontpage">
+		<div class="leftside">
+			<h2>Personal Details</h2>
+			<table style="float: left; clear: left;">
+				<tr>
+					<th>Student</th><td><strong>Cameron L Palmer</strong></td>
+				</tr>
+				<tr>
+					<th>Picture</th><td><img src="i/ninja_cam.jpg" alt="Ninja Cameron" width="128" /></td>
+				</tr>
+				<tr>
+					<th>Email</th><td><a href="mailto:cameron.palmer@gmail.com">cameron.palmer@gmail.com</a></td>
+				</tr>
+			</table>
+		</div>
+
+		<div class="leftside">
+			<h2>About Me</h2>
+			<p>I work for Dr. Sweany doing Compiler research. I am Senior Computer 
+         Science student and have already taken the PHP programming lab.</p>
+			<p>For this assigment I will use Color, Font, Font-Size, and Weight via CSS</p>
+		</div>
+  </div>
