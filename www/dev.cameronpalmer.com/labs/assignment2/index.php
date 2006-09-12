ac25be7ee6f1b3724df9ffd61565e4299d1b183a
palmerc <palmerc@9c8e43f7-b2fe-0310-8927-e999a62a3b53>
2006-09-12 22:29:58 +0000

    starting assignment 2

--- www/dev.cameronpalmer.com/labs/assignment2/index.php
+++ www/dev.cameronpalmer.com/labs/assignment2/index.php
@@ -4,8 +4,8 @@
 	require("../../dev-template.php");
 ?>
 <div>
-   Enter name (Last, First, Middle): <input type="text" name="name" />
-   Enter email address: <input type="text" name="email" />
-   Enter your sex: Male <input type="radio" name="sex" /> Female <input type="radio" name="sex" /> Age: <input type="text" name="age" />
-   <input type="button" name="Reset" /> <input type="button" name="Press here to submit your information" />
+   Enter name (Last, First, Middle): <input type="text" name="name" /><br />
+   Enter email address: <input type="text" name="email" /><br />
+   Enter your sex: Male <input type="radio" name="sex" /> Female <input type="radio" name="sex" /> Age: <input type="text" name="age" /><br />
+   <input type="button" name="Reset" /> <input type="button" name="Press here to submit your information" /><br />
 </div>
