33010d08962da6196ebb37380ddce2d05bf637a5
palmerc <palmerc@9c8e43f7-b2fe-0310-8927-e999a62a3b53>
2006-09-12 22:32:22 +0000

    starting assignment 2

--- www/dev.cameronpalmer.com/labs/assignment2/index.php
+++ www/dev.cameronpalmer.com/labs/assignment2/index.php
@@ -7,5 +7,5 @@
    Enter name (Last, First, Middle): <input type="text" name="name" /><br />
    Enter email address: <input type="text" name="email" /><br />
    Enter your sex: Male <input type="radio" name="sex" /> Female <input type="radio" name="sex" /> Age: <input type="text" name="age" /><br />
-   <input type="button" name="Reset" /> <input type="button" name="Press here to submit your information" /><br />
+   <input type="button" name="reset" value="Reset" /> <input type="button" name="submit" value="Press here to submit your information" /><br />
 </div>
