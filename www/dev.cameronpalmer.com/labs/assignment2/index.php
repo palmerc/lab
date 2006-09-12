ad333b47e3b6d69f17b12a234996b737a44a84fe
palmerc <palmerc@9c8e43f7-b2fe-0310-8927-e999a62a3b53>
2006-09-12 23:51:41 +0000

    starting assignment 2

--- www/dev.cameronpalmer.com/labs/assignment2/index.php
+++ www/dev.cameronpalmer.com/labs/assignment2/index.php
@@ -2,23 +2,21 @@
 	$title = "CSE 4410 - Assignment 2";
 	$section = "Familiarity Assignment 2";
 	require("../../dev-template.php");
+
+   if ($_SERVER['REQUEST_METHOD'] == "POST"){  
+      $name = $_POST['name'];
+      $email = $_POST['email'];
+      $sex = $_POST['sex'];
+      $age = $_POST['age'];
+      
+      echo"
+         {$age}
+         {$sex}
+         {$email}
+         {$name}
+         ";      
+   }
 ?>
-
-if ($_SERVER['REQUEST_METHOD'] == "POST"){
-   
-   $name = $_POST['name'];
-   $email = $_POST['email'];
-   $sex = $_POST['sex'];
-   $age = $_POST['age'];
-   
-   echo "
-      $age<br />
-      $sex<br />
-      $email<br />
-      $name<br />
-      ";
-}
-
 <div class="form">
    <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
       Enter name (Last, First, Middle): <input type="text" name="name" /><br />
