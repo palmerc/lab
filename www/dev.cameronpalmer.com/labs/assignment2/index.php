4ef95dd8c523d2e896f4d01526330ea11678fa2e
palmerc <palmerc@9c8e43f7-b2fe-0310-8927-e999a62a3b53>
2006-09-13 00:04:02 +0000

    i am working on assignment 2

--- www/dev.cameronpalmer.com/labs/assignment2/index.php
+++ www/dev.cameronpalmer.com/labs/assignment2/index.php
@@ -14,12 +14,21 @@
    <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
       Enter name (Last, First, Middle): <input type="text" name="name" /><br />
       Enter email address: <input type="text" name="email" /><br />
-      Enter your sex: Male <input type="radio" name="sex" value="male" /> Female <input type="radio" name="sex" value="female" /> Age: <input type="text" name="age" /><br />
-      <input type="button" name="reset" value="Reset" /> <input type="submit" name="submit" value="Press here to submit your information" size="3" /><br />
+      Enter your sex: Male <input type="radio" name="sex" value="male" /> Female <input type="radio" name="sex" value="female" /><br />
+      Age: <input type="text" name="age" /><br />
+      <input type="button" name="reset" value="Reset" />
+      <input type="submit" name="submit" value="Press here to submit your information" size="3" /><br />
    </form>
    <?php
-         echo"
-         <h3>Submitted values</h3>
+      if ($age < 1 || $age > 100)
+         echo "Error: Age out of range (1-100)<br />";
+      if (!$email)
+         echo "Error: Failed to enter email address. <br />";
+      if (!$name)
+         echo "Error: Failed to enter name. <br />";
+         
+      echo"
+         <h3>Submitted values:</h3>
          Age: {$age}<br />
          Sex: {$sex}<br />
          Email: {$email}<br />
