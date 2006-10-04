92aac111ca5ac6f6713d45e14af84d374d999b81
hexarobi <hexarobi@9c8e43f7-b2fe-0310-8927-e999a62a3b53>
2006-10-04 21:09:17 +0000

    more changes to input file format implemented reading input from file

--- classes/csce3110/project/PathFinder.java
+++ classes/csce3110/project/PathFinder.java
@@ -1,55 +1,32 @@
 /*
- * SwingApplication.java is a 1.4 example that requires
- * no other files.
+ * PathFinder.java
+ * Project for Data Structures TTh 5:30
+ * Tyler Chamberlain, Cameron Palmer, Brian Stair, Kristina Ratliff 
+ * 
  */
+
 import javax.swing.*;
-import javax.swing.event.*;
-import javax.swing.border.*;
 import java.awt.*;
-import java.awt.event.*;
-import java.awt.geom.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class PathFinder {
 
    //Specify the look and feel to use.  Valid values:
    //null (use the default), "Metal", "System", "Motif", "GTK+"
    final static String LOOKANDFEEL = null;
-   private int nodeSize = 20;
-   private int gridSize = 20;
-   private int Xmax = gridSize * nodeSize;
-   private int Ymax = gridSize * nodeSize;
-   private boolean drawGridLines = false;
-   
-   private int[][] nodeList = {
-			  { 1, 2, 1 },
-			  { 4, 9, 0 },
-			  { 3, 3, 0 },
-			  { 13, 9, 0 },
-			  { 18, 2, 2 },
-			  { 19, 19, 0 },
-			  { 11, 2, 2},
-			  { 17, 18, 0 },
-			  { 8, 16, 0 },
-			  { 2, 10, 2 },
-			  { 3, 18, 0 },
-			  { 7, 12, 0 }
-			  
-	  };
-   
-   private int[][] pathList = {
-		   { 0, 2, 6 },
-		   { 0, 1, 5 },
-		   { 2, 3, 4 },
-		   { 3, 4, 2 },
-		   { 4, 5, 3 },
-		   { 4, 6, 3 },
-		   { 5, 7, 2 },
-		   { 7, 8, 4 },
-		   { 8, 9, 10 },
-		   { 8, 10, 5 },
-		   { 8, 11, 3 }
-		  
-   };
+   private static int nodeSize = 20;
+   private static int gridSize = 20;
+   private static int Xmax = gridSize * nodeSize;
+   private static int Ymax = gridSize * nodeSize;
+   private static boolean drawGridLines = false;
+   private static int nodeCount = 0;  // Line number of count 
+   private static int pathCount = 0;
+   private static int[][] nodeList = new int[gridSize^2][3];
+   private static int[][] pathList = new int[gridSize^4][3];
 
    public PathFinder() {
 
@@ -111,42 +88,46 @@ public class PathFinder {
 
 			//Draw paths
             for (int i=0; i < pathList.length; i++) {
-            	g.setColor(Color.black);
-            	int n1 = pathList[i][0];
-            	int n2 = pathList[i][1];
-            	int x1 = nodeList[n1][0] * nodeSize + nodeSize/2;
-            	int y1 = nodeList[n1][1] * nodeSize + nodeSize/2;
-            	int x2 = nodeList[n2][0] * nodeSize + nodeSize/2;
-            	int y2 = nodeList[n2][1] * nodeSize + nodeSize/2;
-            	g.drawLine(x1, y1, x2, y2);
-
-            	//Draw weights
-            	g.setColor(Color.red);
-            	g.drawString("" + pathList[i][2], (x1+x2)/2, (y1+y2)/2 );
+            	if (pathList[i][0] != -1) {
+	            	g.setColor(Color.black);
+	            	int n1 = pathList[i][0];
+	            	int n2 = pathList[i][1];
+	            	int x1 = nodeList[n1][0] * nodeSize + nodeSize/2;
+	            	int y1 = nodeList[n1][1] * nodeSize + nodeSize/2;
+	            	int x2 = nodeList[n2][0] * nodeSize + nodeSize/2;
+	            	int y2 = nodeList[n2][1] * nodeSize + nodeSize/2;
+	            	g.drawLine(x1, y1, x2, y2);
+	
+	            	//Draw weights
+	            	g.setColor(Color.red);
+	            	g.drawString("" + pathList[i][2], (x1+x2)/2, (y1+y2)/2 );
+            	}
             }
             
             //Draw nodes
             for (int i=0; i < nodeList.length; i++) {
-            	if (nodeList[i][2]==1) {
-            		//Start Node (dark green)
-            		g.setColor(new Color(0, 200, 0));
-            	} else if (nodeList[i][2]==2) {
-            		//Destination Node
-            		g.setColor(Color.red);
-            	} else {
-					//Regular Node
-            		g.setColor(Color.blue);
-            	}
-            	g.fillOval(nodeList[i][0]*nodeSize, nodeList[i][1]*nodeSize, nodeSize, nodeSize);
-            	g.setColor(Color.black);
-            	g.drawOval(nodeList[i][0]*nodeSize, nodeList[i][1]*nodeSize, nodeSize-1, nodeSize-1);
-            	
-            	//Draw node labels
-            	g.setColor(Color.white);
-            	if (i < 10) {
-	            	g.drawString(""+i, nodeList[i][0]*nodeSize + 7, nodeList[i][1]*nodeSize + 14);
-            	} else {
-	            	g.drawString(""+i, nodeList[i][0]*nodeSize + 3, nodeList[i][1]*nodeSize + 14);            		
+            	if (nodeList[i][0] != -1) {
+	            	if (nodeList[i][2]==1) {
+	            		//Start Node (dark green)
+	            		g.setColor(new Color(0, 200, 0));
+	            	} else if (nodeList[i][2]==2) {
+	            		//Destination Node
+	            		g.setColor(Color.red);
+	            	} else {
+						//Regular Node
+	            		g.setColor(Color.blue);
+	            	}
+	            	g.fillOval(nodeList[i][0]*nodeSize, nodeList[i][1]*nodeSize, nodeSize, nodeSize);
+	            	g.setColor(Color.black);
+	            	g.drawOval(nodeList[i][0]*nodeSize, nodeList[i][1]*nodeSize, nodeSize-1, nodeSize-1);
+	            	
+	            	//Draw node labels
+	            	g.setColor(Color.white);
+	            	if (i < 10) {
+		            	g.drawString(""+i, nodeList[i][0]*nodeSize + 7, nodeList[i][1]*nodeSize + 14);
+	            	} else {
+		            	g.drawString(""+i, nodeList[i][0]*nodeSize + 3, nodeList[i][1]*nodeSize + 14);            		
+	            	}
             	}
             	
             }
@@ -165,8 +146,101 @@ public class PathFinder {
       //Display the window.
       f.setVisible(true);
    }
+   
+	private static void readFile(String filename)
+		{
+	  	try{
+	  		String type;
+	  		int arg1, arg2, arg3, arg4;
+	  		
+	        FileReader input = new FileReader(filename);
+	        BufferedReader bufRead = new BufferedReader(input);
+	        
+	        String line;    // String that holds current file line
+	        
+	        // Read first line
+	        line = bufRead.readLine();
+	                
+	        // Read through file one line at time.
+	        while (line != null){
+	        	//System.out.println(line);
+	            Pattern p = Pattern.compile("^([A-Z]+) ([0-9]+) ([0-9]+) ([0-9]+) ([0-9]+)$");
+	            Matcher m = p.matcher(line);
+	            if(m.find()) {
+	            	type = m.group(1);
+		            arg1 = Integer.valueOf( m.group(2) ).intValue();
+		            arg2 = Integer.valueOf( m.group(3) ).intValue();
+		            arg3 = Integer.valueOf( m.group(4) ).intValue();
+		            if ( m.group(5) != null)
+		            	arg4 = Integer.valueOf( m.group(5) ).intValue();
+		            else
+		            	arg4 = 0;
+		        	
+		            //System.out.println("type="+type+" arg1="+arg1+" 2="+arg2+" 3="+arg3+" 4="+arg4);
+		            
+		        	if ( type.equals("NODE") ) {
+		        		//add node to list
+		        		int[] newNode = new int[3];
+		        		newNode[0] = arg2;
+		        		newNode[1] = arg3;
+		        		newNode[2] = arg4;
+		        		nodeList[arg1] = newNode;
+		                nodeCount++;
+		        	} else if ( type.equals("PATH") ) {
+		        		//add to path list
+		        		int[] newPath = new int[3];
+		        		newPath[0] = arg2;
+		        		newPath[1] = arg3;
+		        		newPath[2] = arg4;
+		        		pathList[arg1] = newPath; 	
+		        		pathCount ++;	        		
+		        	} else {
+		        		System.out.println("Invalid Line: " + line);
+		        	}
+		        	
+	        	}
+	        	line = bufRead.readLine();
+	        }
+	        
+	        bufRead.close();
+	        return;
+	        
+	    }catch (ArrayIndexOutOfBoundsException e){
+	        /* If no file was passed on the command line, this expception is
+	        generated. A message indicating how to the class should be
+	        called is displayed */
+	        System.out.println("Usage: java ReadFile filename\n");      
+	
+	        return;
+	
+	    }catch (IOException e){
+	        // If another exception is generated, print a stack trace
+	        e.printStackTrace();
+	        return;
+	    }	
+			
+	}; 
+	
+	//inits lists to have first field of each row as a -1, used later as check if empty
+	public static void initLists() {
+		
+		for (int i = 0; i < nodeList.length; i++) {
+			nodeList[i][0] = -1;	
+		}
+		
+		for (int i = 0; i < pathList.length; i++) {
+			pathList[i][0] = -1;
+		}
+		
+	}
 
    public static void main(String[] argv) {
-     PathFinder x = new PathFinder();
+	   
+	   initLists();
+	   
+	   readFile("input.txt");
+	   
+	   PathFinder x = new PathFinder();
+     
    }
 }
