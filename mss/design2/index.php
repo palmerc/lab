f42e1b2fb2acb1198c9b6f2edd496d8eb6860897
palmerc <palmerc@9c8e43f7-b2fe-0310-8927-e999a62a3b53>
2006-11-07 14:46:45 +0000

    Nov 7 update

--- mss/design2/index.php
+++ mss/design2/index.php
@@ -1,44 +1,11 @@
 <?php
-$authorized = FALSE;  // Initialize a variable.
-
-// Check for authentication submission.
-if ( (isset($_SERVER['PHP_AUTH_USER']) AND isset($_SERVER['PHP_AUTH_PW'])) ) {
-
-	// Set the database access information as constants.
-	define ('DB_USER', 'stcdb');
-	define ('DB_PASSWORD', 'stcdbpw');
-	define ('DB_HOST', 'localhost');
-	define ('DB_NAME', 'stc');
-	
-	// Make the connnection and then select the database.
-	$dbc = @mysql_connect (DB_HOST, DB_USER, DB_PASSWORD) OR die ('Could not connect to MySQL: ' . mysql_error() );
-	mysql_select_db (DB_NAME) OR die ('Could not select the database: ' . mysql_error() );
-	
-	// Query the database.
-	$query = "SELECT first_name FROM users WHERE email='{$_SERVER['PHP_AUTH_USER']}' and password=SHA('{$_SERVER['PHP_AUTH_PW']}')";
-	$result = mysql_query ($query);
-	$row = @mysql_fetch_array ($result);
-	if ($row) { // If a record was returned...
-		$authorized = TRUE;
-	}	
-} 
-
-// If they haven't been authorized, create the pop-up window.
-if (!$authorized) {
-	header('WWW-Authenticate: Basic realm="STC"');
-	header('HTTP/1.0 401 Unauthorized'); // For cancellations.
-} else {
-
-?>
-
-<?php
    include('includes/header.html');
 ?>
 <body>
 	<div id="container">
 		<div id="header">
          <div id="topbanner">
-            <img src="i/orangetop3slim.gif" alt="The Lone Star Community" />
+            <img src="i/purpletopbar.gif" alt="The Lone Star Community" />
          </div>
          <div id="topnav">
             <ul id="topbar">
@@ -79,5 +46,4 @@ if (!$authorized) {
       </div>
 <?php
    include('includes/footer.html');
-   }
 ?>
