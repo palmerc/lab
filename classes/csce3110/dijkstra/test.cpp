c45d346a0a82c2c8c3993ad5bb00c72ed4b03655
palmerc <palmerc@9c8e43f7-b2fe-0310-8927-e999a62a3b53>
2006-11-14 07:46:01 +0000

    so i can read in a text file

--- classes/csce3110/dijkstra/test.cpp
+++ classes/csce3110/dijkstra/test.cpp
@@ -1,11 +1,16 @@
 #include <stdlib.h>
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <boost/regex.hpp>
 
 using namespace std;
 
 int main(int argc, char* argv[])
 {
+   boost::regex re("^\\s*v\\s+(\\d+)\\s+(\\d+)\\s+(\\d+)\\s*$", boost::regex::perl);
+   boost::cmatch matches;
+   std::string str;
    // So yes argv is there
    for (int i = 0; i < argc; i++)
       cout << argv[i] << endl;
@@ -13,9 +18,22 @@ int main(int argc, char* argv[])
    ifstream in;
    
    in.open(argv[1]);
-   while (in.good())
-      cout << (char) in.get();
    
+   std::string vertex, next, weight;
+   int i = 1;
+   getline(in, str);
+   while ( in )
+   {
+      if (boost::regex_match(str.c_str(), matches, re))
+      {
+         vertex = matches[1];
+         next = matches[2];
+         weight = matches[3];
+         cout << "Line: " << i << " Vertex: " << vertex << " Next: " << next << " Weight: " << weight << endl;
+      }
+      getline(in, str);
+      i++;
+   }
    in.close();
    return 0;
 }
