12e7206c2908a18e3121423ba2c1e386200311ef
palmerc <palmerc@9c8e43f7-b2fe-0310-8927-e999a62a3b53>
2006-11-28 03:37:42 +0000

    Manual calculations abound but I made it to step 3

--- classes/csce3110/project/kmb.cpp
+++ classes/csce3110/project/kmb.cpp
@@ -6,15 +6,17 @@
 #include <boost/graph/graph_traits.hpp>
 #include <boost/graph/adjacency_list.hpp>
 #include <boost/graph/dijkstra_shortest_paths.hpp>
+#include <boost/graph/kruskal_min_spanning_tree.hpp>
 
 using namespace boost;
 using namespace std;
 
+//graph_t is the adjacency_list typedef
 typedef adjacency_list < listS, vecS, directedS, no_property, property < edge_weight_t, int > > graph_t;
 typedef graph_traits < graph_t >::vertex_descriptor vertex_descriptor;
 typedef graph_traits < graph_t >::edge_descriptor edge_descriptor;
 typedef pair<int, int> Edge;
-
+  
 void output_dot(string filename, graph_t g, vector<int> d, vector<vertex_descriptor> p, property_map<graph_t, edge_weight_t>::type weightmap, char name[])
 {
 
@@ -47,7 +49,7 @@ void output_dot(string filename, graph_t g, vector<int> d, vector<vertex_descrip
       dot_file << ", color=\"grey\"";
     dot_file << "]";
   }
-  dot_file << "}";  
+  dot_file << "}";
 }
 
 int main(int, char *[])
@@ -71,16 +73,69 @@ int main(int, char *[])
   vector<vertex_descriptor> p(num_vertices(g));
   vector<int> d(num_vertices(g));
 
-  // Is this the start? Let's find out
+  // Calculate Dijkstra's
   vertex_descriptor s = vertex(A, g);
   dijkstra_shortest_paths(g, s, predecessor_map(&p[0]).distance_map(&d[0]));
+  cout << "Starting point " << name[s] << endl;
+  cout << " to E " << d[4] << endl;
+  cout << " to G " << d[6] << endl;
   output_dot("figs/kmb1.dot", g, d, p, weightmap, name);
+  
   vertex_descriptor t = vertex(E, g);
   dijkstra_shortest_paths(g, t, predecessor_map(&p[0]).distance_map(&d[0]));
+  cout << "Starting point " << name[t] << endl;
+  cout << " to A " << d[0] << endl;
+  cout << " to G " << d[6] << endl;
   output_dot("figs/kmb2.dot", g, d, p, weightmap, name);
+     
   vertex_descriptor u = vertex(G, g);
   dijkstra_shortest_paths(g, u, predecessor_map(&p[0]).distance_map(&d[0]));
+  cout << "Starting point " << name[u] << endl;
+  cout << " to A " << d[0] << endl;
+  cout << " to E " << d[4] << endl;
   output_dot("figs/kmb3.dot", g, d, p, weightmap, name);
+  
+  // Calculate the MST 
+  const int mst_num_nodes = 3;
+  Edge mst_edge_array[] = { Edge(A, E), Edge(A, G), Edge(E, G)
+  };
+  int mst_weights[] = { 6, 4, 5 };
+  size_t mst_num_edges = sizeof(mst_edge_array) / sizeof(Edge);
+  graph_t mst_g(mst_edge_array, mst_edge_array + mst_num_edges, mst_weights, mst_num_nodes);
+  property_map < graph_t, edge_weight_t >::type weight = get(edge_weight, mst_g);
+  vector < edge_descriptor > spanning_tree;
+
+  kruskal_minimum_spanning_tree(mst_g, back_inserter(spanning_tree));
+  
+  // MST Printing begins here
+  cout << "Print the edges in the MST:" << endl;
+  for (vector < edge_descriptor >::iterator ei = spanning_tree.begin();
+       ei != spanning_tree.end(); ++ei) {
+    cout << source(*ei, mst_g) << " <--> " << target(*ei, mst_g)
+      << " with weight of " << weight[*ei]
+      << endl;
+  }
+
+  ofstream fout("figs/kmb4.dot");
+  fout << "graph A {\n"
+    << " rankdir=LR\n"
+    << " size=\"3,3\"\n"
+    << " ratio=\"filled\"\n"
+    << " edge[style=\"bold\"]\n" << " node[shape=\"circle\"]\n";
+  graph_traits<graph_t>::edge_iterator eiter, eiter_end;
+  for (tie(eiter, eiter_end) = edges(mst_g); eiter != eiter_end; ++eiter) {
+     graph_traits < graph_t >::vertex_descriptor 
+     u = source(*eiter, mst_g), v = target(*eiter, mst_g);
+    fout << name[u] << " -- " << name[v];
+    if (find(spanning_tree.begin(), spanning_tree.end(), *eiter)
+        != spanning_tree.end())
+      fout << "[color=\"black\", label=\"" << get(edge_weight, mst_g, *eiter)
+           << "\"];\n";
+    else
+      fout << "[color=\"gray\", label=\"" << get(edge_weight, mst_g, *eiter)
+           << "\"];\n";
+  }
+  fout << "}\n";
   
   return EXIT_SUCCESS;
 }
