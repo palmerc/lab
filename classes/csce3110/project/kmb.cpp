a89d731a4a08cfabd9e2e5ae6355befe68a2bf86
palmerc <palmerc@9c8e43f7-b2fe-0310-8927-e999a62a3b53>
2006-11-28 16:55:06 +0000

    graph progress

--- classes/csce3110/project/kmb.cpp
+++ classes/csce3110/project/kmb.cpp
@@ -12,83 +12,85 @@ using namespace boost;
 using namespace std;
 
 //graph_t is the adjacency_list typedef
-typedef adjacency_list < listS, vecS, directedS, no_property, property < edge_weight_t, int > > graph_t;
-typedef graph_traits < graph_t >::vertex_descriptor vertex_descriptor;
-typedef graph_traits < graph_t >::edge_descriptor edge_descriptor;
-typedef pair<int, int> Edge;
-  
-void output_dot(string filename, graph_t g, vector<int> d, vector<vertex_descriptor> p, property_map<graph_t, edge_weight_t>::type weightmap, char name[])
+typedef adjacency_list <listS, vecS, undirectedS, no_property, property<edge_weight_t, int> > Graph;
+typedef graph_traits <Graph>::vertex_descriptor Vertex;
+typedef graph_traits <Graph>::edge_descriptor Edge;
+typedef pair<int, int> E;
+
+void output_dot(string filename, Graph g, vector<int> d, vector<Vertex> p, property_map<Graph, edge_weight_t>::type weightmap)
 {
+   cout << "distances and parents:" << endl;
+   graph_traits <Graph>::vertex_iterator vi, vend;
+   for (tie(vi, vend) = vertices(g); vi != vend; ++vi)
+   {
+      cout << "distance(" << *vi << ") = " << d[*vi] << ", ";
+      cout << "parent(" << *vi << ") = " << p[*vi] << endl;
+   }
+   cout << endl;
 
-  cout << "distances and parents:" << endl;
-  graph_traits < graph_t >::vertex_iterator vi, vend;
-  for (tie(vi, vend) = vertices(g); vi != vend; ++vi) {
-    cout << "distance(" << name[*vi] << ") = " << d[*vi] << ", ";
-    cout << "parent(" << name[*vi] << ") = " << name[p[*vi]] << endl;
-  }
-  cout << endl;
+   ofstream dot_file(filename.c_str());
 
-  ofstream dot_file(filename.c_str());
+   dot_file << "graph G {\n"
+      << "  edge[style=\"bold\"]\n" << "  node[shape=\"circle\"]\n";
 
-  dot_file << "digraph D {\n"
-    << "  rankdir=LR\n"
-    << "  size=\"4,3\"\n"
-    << "  ratio=\"fill\"\n"
-    << "  edge[style=\"bold\"]\n" << "  node[shape=\"circle\"]\n";
-
-  graph_traits < graph_t >::edge_iterator ei, ei_end;
-  for (tie(ei, ei_end) = edges(g); ei != ei_end; ++ei) {
-    graph_traits < graph_t >::edge_descriptor e = *ei;
-    graph_traits < graph_t >::vertex_descriptor
-      u = source(e, g), v = target(e, g);
-    dot_file << name[u] << " -> " << name[v]
-      << "[label=\"" << get(weightmap, e) << "\"";
-    if (p[v] == u)
-      dot_file << ", color=\"black\"";
-    else
-      dot_file << ", color=\"grey\"";
-    dot_file << "]";
-  }
-  dot_file << "}";
+   graph_traits <Graph>::edge_iterator ei, ei_end;
+   for (tie(ei, ei_end) = edges(g); ei != ei_end; ++ei)
+   {
+      graph_traits <Graph>::edge_descriptor e = *ei;
+      graph_traits <Graph>::vertex_descriptor u = source(e, g), v = target(e, g);
+      dot_file << u << " -- " << v
+         << " [label=\"" << get(weightmap, e) << "\"";
+      cout << u << " " << v << " " << p[v] << endl;
+      if ((int) p[v] == u)
+         dot_file << ", color=\"black\"";
+      else
+         dot_file << ", color=\"grey\"";
+      dot_file << "]\n";
+   }
+   dot_file << "}";
 }
 
 int main(int, char *[])
 {
-  const int num_nodes = 5;
-  enum nodes { A, B, C, D, E, F, G };
-  char name[] = "ABCDEFG";
-  Edge edge_array[] = { Edge(A, B), Edge(B, A), Edge(A, C), Edge(C, A), 
-     Edge(A, D), Edge(D, A), Edge(B, C), Edge(C, B), Edge(B, E), Edge(E, B),
-     Edge(C, F), Edge(F, C), Edge(D, F), Edge(F, D), Edge(D, G), Edge(G, D),
-     Edge(E, F), Edge(F, E), Edge(F, G), Edge(G, F)
-  };
-  int weights[] = { 6, 6, 2, 2, 1, 1, 3, 3, 1, 1, 1, 1, 3, 3, 3, 3, 3, 3, 2, 2 };
+   const int num_nodes = 5;
+   E edge_array[] = {
+      E(0, 1), 
+      E(0, 2), 
+      E(0, 3), 
+      E(1, 2), 
+      E(1, 4), 
+      E(2, 5), 
+      E(3, 5), 
+      E(3, 6),
+      E(4, 5), 
+      E(5, 6)
+   };
+   int weights[] = { 6, 2, 1, 3, 1, 1, 3, 3, 3, 2 };
+   int dests[] = { 0, 4, 6 };
+   int num_arcs = sizeof(edge_array) / sizeof(E);
+   Graph g(edge_array, edge_array + num_arcs, weights, num_nodes);
   
-  int num_arcs = sizeof(edge_array) / sizeof(Edge);
+   property_map<Graph, edge_weight_t>::type weightmap = get(edge_weight, g);
   
-  graph_t g(edge_array, edge_array + num_arcs, weights, num_nodes);
-  
-  property_map<graph_t, edge_weight_t>::type weightmap = get(edge_weight, g);
-  
-  vector<vertex_descriptor> p(num_vertices(g));
-  vector<int> d(num_vertices(g));
+   vector<Vertex> p(num_vertices(g));
+   vector<int> d(num_vertices(g));
 
-  // Calculate Dijkstra's
-  vertex_descriptor s = vertex(A, g);
-  dijkstra_shortest_paths(g, s, predecessor_map(&p[0]).distance_map(&d[0]));
-  cout << "Starting point " << name[s] << endl;
-  cout << " to E " << d[4] << endl;
-  cout << " to G " << d[6] << endl;
-  output_dot("figs/kmb1.dot", g, d, p, weightmap, name);
-  
-  vertex_descriptor t = vertex(E, g);
+   // Calculate Dijkstra's
+   for (int i=0; i < (int) sizeof(dests); ++i)
+   {
+      Vertex s = vertex(dests[i], g);
+      dijkstra_shortest_paths(g, s, predecessor_map(&p[0]).distance_map(&d[0]));
+      output_dot("figs/kmb11.dot", g, d, p, weightmap);
+   }
+/*  
+  Vertex t = vertex(E, g);
   dijkstra_shortest_paths(g, t, predecessor_map(&p[0]).distance_map(&d[0]));
   cout << "Starting point " << name[t] << endl;
   cout << " to A " << d[0] << endl;
   cout << " to G " << d[6] << endl;
   output_dot("figs/kmb2.dot", g, d, p, weightmap, name);
      
-  vertex_descriptor u = vertex(G, g);
+  Vertex u = vertex(G, g);
   dijkstra_shortest_paths(g, u, predecessor_map(&p[0]).distance_map(&d[0]));
   cout << "Starting point " << name[u] << endl;
   cout << " to A " << d[0] << endl;
@@ -138,6 +140,6 @@ int main(int, char *[])
            << "\"];\n";
   }
   fout << "}\n";
-  
+*/
   return EXIT_SUCCESS;
 }
