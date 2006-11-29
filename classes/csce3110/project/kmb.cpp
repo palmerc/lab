e1caf409f3b4dca44f40516047b4fbfcb958d52e
palmerc <palmerc@9c8e43f7-b2fe-0310-8927-e999a62a3b53>
2006-11-29 01:07:35 +0000

    major work

--- classes/csce3110/project/kmb.cpp
+++ classes/csce3110/project/kmb.cpp
@@ -12,7 +12,7 @@ using namespace boost;
 using namespace std;
 
 //graph_t is the adjacency_list typedef
-typedef adjacency_list <listS, vecS, undirectedS, no_property, property<edge_weight_t, int> > Graph;
+typedef adjacency_list <vecS, vecS, undirectedS, no_property, property<edge_weight_t, int> > Graph;
 typedef graph_traits <Graph>::vertex_descriptor Vertex;
 typedef graph_traits <Graph>::edge_descriptor Edge;
 typedef pair<int, int> E;
@@ -41,7 +41,7 @@ void output_dot(string filename, Graph g, vector<int> d, vector<Vertex> p, prope
       dot_file << u << " -- " << v
          << " [label=\"" << get(weightmap, e) << "\"";
       cout << u << " " << v << " " << p[v] << endl;
-      if ((int) p[v] == u)
+      if (p[v] == u)
          dot_file << ", color=\"black\"";
       else
          dot_file << ", color=\"grey\"";
@@ -66,22 +66,94 @@ int main(int, char *[])
       E(5, 6)
    };
    int weights[] = { 6, 2, 1, 3, 1, 1, 3, 3, 3, 2 };
-   int dests[] = { 0, 4, 6 };
    int num_arcs = sizeof(edge_array) / sizeof(E);
    Graph g(edge_array, edge_array + num_arcs, weights, num_nodes);
-  
+   map<E, int> path_info;
    property_map<Graph, edge_weight_t>::type weightmap = get(edge_weight, g);
-  
+   
+   set<int> starts;
+   starts.insert(0);
+   starts.insert(4);
+   starts.insert(6);
+   
    vector<Vertex> p(num_vertices(g));
    vector<int> d(num_vertices(g));
 
    // Calculate Dijkstra's
-   for (int i=0; i < (int) sizeof(dests); ++i)
+   for (set<int>::iterator i = starts.begin(); i != starts.end(); ++i)
    {
-      Vertex s = vertex(dests[i], g);
+      Vertex s = vertex(*i, g);
       dijkstra_shortest_paths(g, s, predecessor_map(&p[0]).distance_map(&d[0]));
-      output_dot("figs/kmb11.dot", g, d, p, weightmap);
+      for (set<int>::iterator q = starts.begin(); q != starts.end(); ++q)
+      {
+         if (*q != s)
+         {
+            int cur = *q;
+            cout << "Starting with " << cur << endl;
+            while (cur != s)
+            {
+               cout << p[cur] << endl;
+               cur = p[cur];
+            }
+         }
+      }
+      graph_traits <Graph>::vertex_iterator vi, vend;
+      for (tie(vi, vend) = vertices(g); vi != vend; ++vi)
+      {
+         int u = s;
+         int v = *vi;
+         int distance = d[*vi];
+         if ( starts.find(v) != starts.end() )
+            path_info[E(u, v)] = distance;
+      }
    }
+   
+   //for (map<E, int>::const_iterator i = path_info.begin(); i != path_info.end(); ++i)
+   //   cout << i->first.first << " " << i->first.second << " " << i->second << endl;
+   
+   // Construct the new graph
+   int const mst_num_nodes = starts.size();
+   
+   vector<E> mst_edge_array;
+   vector<int> mst_weights;
+   for (map<E, int>::const_iterator i = path_info.begin(); i != path_info.end(); ++i)
+   {
+      int u = i->first.first;
+      int v = i->first.second;
+      int distance = i->second;
+      mst_edge_array.push_back(E(u, v));
+      mst_weights.push_back(distance);
+   }
+   size_t mst_num_edges = mst_edge_array.size();
+   Graph mst_g(&mst_edge_array[0], &mst_edge_array[0] + mst_num_edges, &mst_weights[0], mst_num_nodes);
+   property_map <Graph, edge_weight_t >::type weight = get(edge_weight, mst_g);
+   vector <Edge> spanning_tree;
+
+   // Calculate the MST
+   kruskal_minimum_spanning_tree(mst_g, back_inserter(spanning_tree));
+   // MST Printing begins here
+   //cout << "Print the edges in the MST:" << endl;
+//   for (size_t i = 0; i < sizeof(p) / sizeof(Vertex); ++i)
+//      cout << i << " " << p[i] << endl;
+   /*
+   for (vector <Edge>::iterator ei = spanning_tree.begin(); ei != spanning_tree.end(); ++ei) {
+      Vertex source_node = source(*ei, mst_g);
+      Vertex target_node = target(*ei, mst_g);
+      Vertex test = target(target_node, g);
+      
+      graph_traits <Graph>::vertex_iterator vi, vend;
+      int current_node = target_node;
+      while (current_node != source_node)
+      {
+         current_node = p[current_node];
+         cout << current_node;
+      }
+
+   }*/
+   
+   // Look through the original graph and remove links not in the MST
+
+   
 /*  
   Vertex t = vertex(E, g);
   dijkstra_shortest_paths(g, t, predecessor_map(&p[0]).distance_map(&d[0]));
@@ -110,10 +182,10 @@ int main(int, char *[])
 
   // Calculate the MST
   kruskal_minimum_spanning_tree(mst_g, back_inserter(spanning_tree));
-  
+ 
   // MST Printing begins here
   cout << "Print the edges in the MST:" << endl;
-  for (vector < edge_descriptor >::iterator ei = spanning_tree.begin();
+  for (vector <Edge>::iterator ei = spanning_tree.begin();
        ei != spanning_tree.end(); ++ei) {
     cout << source(*ei, mst_g) << " <--> " << target(*ei, mst_g)
       << " with weight of " << weight[*ei]
