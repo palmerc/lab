c0eceb0478618df1902d823988383e1625c71575
palmerc <palmerc@9c8e43f7-b2fe-0310-8927-e999a62a3b53>
2006-12-11 23:44:08 +0000

    student averages are working

--- www/gradebook/gradebook.php
+++ www/gradebook/gradebook.php
@@ -1,20 +1,349 @@
 <?
+
+//
+//
+// Function declarations
+//
+function table_header($assignment_array)
+{
+   global $assignment_count;
+   ?>
+      <table width="200" border="1">
+      <tr>
+         <!-- equal the the categories rows -->
+         <th rowspan="3" scope="col">Student</th>
+         <!-- categories colspan=number of assignments -->
+         <th colspan="<? echo $assignment_count ?>" scope="col">Categories</th>
+         <!-- equal the the categories rows -->
+         <th rowspan="3" scope="col">Student Average</th>
+      </tr>
+         <tr>
+   <?
+   // Create the header section
+   foreach ($assignment_array as $category)
+   {
+      $category_title = $category['categoryTitle'];
+      $category_assignment_count = sizeof($category['assignments']);
+   ?>
+         <!-- colspan should equal the number of assignments -->
+         <th colspan="<? echo $category_assignment_count ?>"><? echo $category_title ?><input type="checkbox" alt="hide" checked="checked" /></th>
+         <!-- if colspan=1 then rowspan=2 -->
+   <?php
+   }
+   ?>
+      </tr>
+      <tr>
+   <?
+   foreach ($assignment_array as $category)
+   {
+      foreach ($category['assignments'] as $assignment)
+      {
+         $assignment_title = $assignment['assignmentTitle'];
+         echo"
+         <th>{$assignment_title}</th>
+            ";
+      }
+   }
+   ?>
+      </tr>
+   <?
+}
+
+function table_footer()
+{
+   ?>
+   </table>
+   <?
+}
+
+function student_rows($assignment_array, $student_array, $student_grade_array)
+{
+   global $class_key, $assignment_count;
+   
+   //echo "<pre>";
+   //print_r($students);
+   //echo "</pre>";
+
+   foreach ($student_array as $student)
+   {
+      $student_key = $student['student_key'];
+      $first_name = $student['first_name'];
+      $last_name =  $student['last_name'];
+      
+   ?>
+   
+         <tr class="student">
+            <th scope="col"><? echo "{$last_name}, {$first_name}" ?></th>
+   <?php
+   //echo "<pre>";
+   //print_r($assignment_array);
+   //echo "</pre>";
+   foreach ($assignment_array as $categoryKey => $categoryData)
+   {
+      foreach ($categoryData['assignments'] as $assignmentKey => $assignmentData)
+      {
+         if ($student_grade_array[$student_key]['categories'][$categoryKey]['assignments'][$assignmentKey])
+            $grade = $student_grade_array[$student_key]['categories'][$categoryKey]['assignments'][$assignmentKey]['gradesGrade'];
+         else
+            $grade = "";
+         // Check if the student has a grade for this entry and set the field
+         // name to be a combination of studentKey and assignmentKey
+         echo"
+         <td><input name=\"grade[{$student_key}][{$assignmentKey}]\" type=\"text\" size=\"3\" value=\"{$grade}\" /></td>
+            ";
+      }
+   }
+   $average = get_student_avg($student_key, $assignment_array, $student_grade_array);
+   echo"
+            <td>{$average}</td>
+         </tr>
+      ";
+   }
+}
+
+function subcategory_averages()
+{
+   global $assignment_count;
+   ?>
+         <tr>
+            <th scope="col"><p>Subcategory Averages</p></th>
+   <?php
+   for ($i = 0; $i < $assignment_count; $i++)
+   {
+      echo "<td>avg</td>";
+   }
+   ?>
+            <td>avg</td>
+         </tr>
+   <?
+}
+
+function category_averages($assignment_array)
+{
+   ?>
+         <tr>
+            <th scope="col">Category Averages </th>
+   <?php
+   foreach ($assignment_array as $category)
+   {
+      $category_assignment_count = sizeof($category['assignments']);
+   ?>
+         <!-- colspan should equal the number of assignments -->
+         <td colspan="<? echo $category_assignment_count ?>">avg</td>
+         <!-- if colspan=1 then rowspan=2 -->
+   <?php
+   }
+   ?>
+            <td>avg</td>
+         </tr>
+   <?
+}
+
+function get_assignments()
+{
+   global $class_key, $assignment_count, $category_count;
+   $results = array();
+
+   // Query to select all assignments in a given class
+   $query = "SELECT assignment.categoryKey, 
+               category.categoryTitle,
+               category.classKey,
+               category.categoryPercentage,
+               category.categoryRank,
+               assignment.assignmentKey,
+               assignment.assignmentTitle,
+               assignment.assignmentMaxPoints,
+               assignment.assignmentDueDate,
+               assignment.assignmentRank
+            FROM category, assignment
+            WHERE category.categoryKey=assignment.categoryKey
+               AND assignment.classKey={$class_key}
+            ORDER BY category.categoryRank,
+               assignment.categoryKey,
+               assignment.assignmentRank,
+               assignment.assignmentKey";
+   $result = mysql_query($query);
+   while (@$row = mysql_fetch_array($result, MYSQL_ASSOC))
+      $results[] = $row;
+      
+   $assignment_count = sizeof($results);
+   $assignment_array = array();
+   //echo "<pre>";
+   foreach ($results as $record)
+   {
+      if (!array_key_exists($record['categoryKey'], $assignment_array))
+      {
+         $assignment_array[$record['categoryKey']] = 
+         array(
+         'categoryTitle' => $record['categoryTitle'],
+         'categoryPercentage' => $record['categoryPercentage'],
+         'categoryRank' => $record['categoryRank'],
+         'assignments' => array()
+         );
+      }
+
+      $assignment_array[$record['categoryKey']]['assignments'][$record['assignmentKey']] = 
+         array(
+         'assignmentTitle' => $record['assignmentTitle'],
+         'assignmentMaxPoints' => $record['assignmentMaxPoints'],
+         'assignmentDueDate' => $record['assignmentDueDate'],
+         'assignmentRank' => $record['assignmentRank']
+         );
+   }
+   //echo "<pre>";
+   //print_r($results);
+   //echo "</pre>";
+   $category_count = sizeof($assignment_array);
+   return $assignment_array;
+}
+
+function get_students()
+{
+   global $class_key;
+   // This query is need to generate the table with all the students
+   $query="SELECT student_class.student_key, student.first_name, student.last_name
+         FROM student, student_class
+         WHERE student_class.student_key=student.student_key
+            AND student_class.class_key={$class_key}
+            AND student.is_active=1";
+   $result = mysql_query($query);
+   $results = array();
+   while (@$row = mysql_fetch_array($result, MYSQL_ASSOC))
+      $results[] = $row;
+   $students = $results;
+   return $students;
+}
+
+function get_students_grades()
+{
+   global $class_key;
+   // Query for all students grades
+   $query = "SELECT assignment.categoryKey, 
+               category.categoryTitle,
+               category.classKey,
+               category.categoryPercentage,
+               category.categoryRank,
+               assignment.assignmentKey,
+               assignment.assignmentTitle,
+               assignment.assignmentMaxPoints,
+               assignment.assignmentRank,
+               student.student_key,
+               student.first_name,
+               student.last_name,
+               grades.gradeKey,
+               grades.grade
+            FROM category, assignment, grades, student
+            WHERE category.categoryKey=assignment.categoryKey
+               AND assignment.assignmentKey=grades.assignmentKey
+               AND student.student_key=grades.studentKey
+               AND assignment.classKey={$class_key}
+            ORDER BY category.categoryRank,
+               assignment.categoryKey,
+               assignment.assignmentRank,
+               assignment.assignmentKey";
+
+   $results = array();
+   $result = mysql_query($query);
+   while (@$row = mysql_fetch_array($result, MYSQL_ASSOC))
+      $results[] = $row;
+   $student_grade_array = array();
+
+   //echo "<pre>";
+   //print_r($results);
+   //echo "</pre>";
+
+
+   // This creates an array of students and their grades, it does not include the
+   // information about assignments without grades.
+   foreach ($results as $record)
+   {
+      if (!array_key_exists($record['student_key'], $student_grade_array))
+      {
+         $student_grade_array[$record['student_key']] =
+         array(
+         'studentFirst' => $record['first_name'],
+         'studentLast' => $record['last_name'],
+         'categories' => array()
+         );
+      }
+      
+      if (!array_key_exists($record['categoryKey'], $student_grade_array[$record['student_key']]['categories']))
+      {
+         $student_grade_array[$record['student_key']]['categories'][$record['categoryKey']] =
+         array(
+         'categoryTitle' => $record['categoryTitle'],
+         'categoryPercentage' => $record['categoryPercentage'],
+         'categoryRank' => $record['categoryRank'],
+         'assignments' => array()
+         );
+      }
+      
+      $student_grade_array[$record['student_key']]['categories'][$record['categoryKey']]['assignments'][$record['assignmentKey']] =
+         array(
+         'assignmentTitle' => $record['assignmentTitle'],
+         'assignmentMaxPoints' => $record['assignmentMaxPoints'],
+         'assignmentRank' => $record['assignmentRank'],
+         'gradesGrade' => $record['grade'],
+         'gradesGradeKey' => $record['gradeKey']
+         );
+   }
+
+   //echo "<pre>";
+   //print_r($student_grade_array);
+   //echo "</pre>";
+   
+   return $student_grade_array;
+}
+
+function get_student_avg($student_key, $assignment_array, $student_grade_array)
+{
+   // category_avg = sum of assignments in category / # of items
+   // points_attained in category = category percentage * category_avg
+   // student_average = sum of points attained in each category
+   //echo "<pre>";
+   //print_r($student_grade_array);
+   //print_r($assignment_array);
+   //echo "</pre>";
+      
+   if ($student_grade_array[$student_key])
+   {
+      foreach ($student_grade_array[$student_key]['categories'] as $categoryKey => $categoryData)
+      {
+         $categorySum = 0;
+         $count = 0;
+         $categoryPercentage = $categoryData['categoryPercentage'];
+         foreach ($categoryData['assignments'] as $assignment)
+         {
+            $categorySum += $assignment['gradesGrade'];
+            $count++;
+         }
+         $categoryAvg = $categorySum / $count;
+         $studentCategories[$categoryKey] = $categoryAvg * ($categoryPercentage / 100);
+      }
+      foreach ($assignment_array as $categoryKey => $categoryData)
+      {
+         if (!$studentCategories[$categoryKey])
+            $studentCategories[$categoryKey] = $categoryData['categoryPercentage'];
+      }
+      foreach ($studentCategories as $categoryPoints)
+         $average += $categoryPoints;
+   }
+   return $average;
+}
+
+
+//
+//
+// Main program
+//
+
 require('database.php');
 //require('grades.php');
 $class_key = $_REQUEST['class_key'];
 
 // If no class_key is provided default to somewhere else
 if ($class_key == null)
-   header("location:assignmentMain.php");
-
-// Before we can layout the gradebook we need to know the following:
-//   The number and names of the categories
-//   The number and names of assignments under each category
-//   The number and names of the students
-//   The grade fields need to be tied to the student and assignment
-//   Don't show a hidden category
-//   Phase One - Create the header
-
+   header("location:index.php");
 database_connect();
 
 // This query gets the class DEPT NUM.SEC information
@@ -29,96 +358,10 @@ $results = $results[0];
 $dept_key = $results['dept_key'];
 $course_no = $results['course_no'];
 $section = $results['section'];
-$results = null;
 
-// Query to select all assignments in a given class
-$query = "SELECT assignment.categoryKey, 
-            category.categoryTitle,
-            category.classKey,
-            category.categoryPercentage,
-            category.categoryRank,
-            assignment.assignmentKey,
-            assignment.assignmentTitle,
-            assignment.assignmentMaxPoints,
-            assignment.assignmentDueDate,
-            assignment.assignmentRank
-         FROM category, assignment
-         WHERE category.categoryKey=assignment.categoryKey
-            AND assignment.classKey={$class_key}
-         ORDER BY category.categoryRank,
-            assignment.categoryKey,
-            assignment.assignmentRank,
-            assignment.assignmentKey";
-$result = mysql_query($query);
-while (@$row = mysql_fetch_array($result, MYSQL_ASSOC))
-   $results[] = $row;
-$assignment_array = array();
-//echo "<pre>";
-foreach ($results as $record)
-{
-   if (!array_key_exists($record['categoryKey'], $assignment_array))
-   {
-      $assignment_array[$record['categoryKey']] = 
-      array(
-      'categoryTitle' => $record['categoryTitle'],
-      'categoryPercentage' => $record['categoryPercentage'],
-      'categoryRank' => $record['categoryRank'],
-      'assignments' => array()
-      );
-   }
-
-   $assignment_array[$record['categoryKey']]['assignments'][$record['assignmentKey']] = 
-      array(
-      'assignmentTitle' => $record['assignmentTitle'],
-      'assignmentMaxPoints' => $record['assignmentMaxPoints'],
-      'assignmentDueDate' => $record['assignmentDueDate'],
-      'assignmentRank' => $record['assignmentRank']
-      );
-}
-echo "<pre>";
-print_r($results);
-echo "</pre>";
-$assignment_count = sizeof($results);
-$category_count = sizeof($assignment_array);
-
-
-
-$student_array = array();
-foreach ($results as $record)
-{
-   if (!array_key_exists($record['studentKey'], $student_array))
-   {
-      $student_array[$record['studentKey']] =
-      array(
-      'studentFirst' => $record['firstName'],
-      'studentLast' => $record['lastName'],
-      'average' => // some calculation
-      'assignments' => array()
-      );
-   }
-   
-   $student_array[$record['studentKey']['assignments'][$record['assignmentKey']] =
-      array(
-      'assignmentTitle' => $record['assignmentTitle'],
-      'assignmentMaxPoints' => $record['assignmentMaxPoints'],
-      'assignmentRank' => $record['assignmentRank'],
-      'assignmentGrade' => $record['assignmentGrade']
-      );
-}
-
-//print_r($assignment_array);
-//echo"
-//   Number of categories: {$category_count}
-//   Number of assignments: {$assignment_count}
-//   ";
-//echo "</pre>";
-
-//$assignments_rows = sizeof($results); // This is the total number of assignments
-
-// Lets calculate the averages of the assignments and students
-
-
-
+$assignment_array = get_assignments();
+$student_array = get_students();
+$student_grade_array = get_students_grades();
 
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
@@ -141,119 +384,16 @@ foreach ($results as $record)
       <div id="gradebook">
          <form action="<? echo $_SERVER['PHP_SELF'] ?>" method="post">
          <input type="hidden" name="class_key" value="<? echo $class_key ?>" />
-      <table width="200" border="1">
-      <tr>
-         <!-- equal the the categories rows -->
-         <th rowspan="3" scope="col">Student</th>
-         <!-- categories colspan=number of assignments -->
-         <th colspan="<? echo $assignment_count ?>" scope="col">Categories</th>
-         <!-- equal the the categories rows -->
-         <th rowspan="3" scope="col">Student Average</th>
-      </tr>
-      <tr>
-<?
-// Create the header section
-foreach ($assignment_array as $category)
-{
-   $category_title = $category['categoryTitle'];
-   $category_assignment_count = sizeof($category['assignments']);
-?>
-      <!-- colspan should equal the number of assignments -->
-      <th colspan="<? echo $category_assignment_count ?>"><? echo $category_title ?><input type="checkbox" alt="hide" checked="checked" /></th>
-      <!-- if colspan=1 then rowspan=2 -->
-<?php
-}
-?>
-   </tr>
-   <tr>
-<?
-foreach ($assignment_array as $category)
-{
-   foreach ($category['assignments'] as $assignment)
-   {
-      $assignment_title = $assignment['assignmentTitle'];
-      echo"
-      <th>{$assignment_title}</th>
-         ";
-   }
-}
-?>
-   </tr>
-   
-<?php
 
-$query="SELECT student_class.student_key, student.first_name, student.last_name
-      FROM student, student_class
-      WHERE student_class.student_key=student.student_key
-         AND student_class.class_key={$class_key}
-         AND student.is_active=1";
-$result = mysql_query($query);
-$results = null;
-while (@$row = mysql_fetch_array($result, MYSQL_ASSOC))
-   $results[] = $row;
-$students = $results;
-
-//echo "<pre>";
-//print_r($students);
-//echo "</pre>";
-
-foreach ($students as $student)
-{
-   $student_key = $student['student_key'];
-   $first_name = $student['first_name'];
-   $last_name =  $student['last_name'];
-   
-?>
-     <tr class="student">
-         <th scope="col"><? echo "{$last_name}, {$first_name}" ?></th>
 <?php
-foreach ($assignment_array as $category)
-{
-   foreach ($category['assignments'] as $assignment)
-   {
-      $assignment_title = urlencode($assignment['assignmentTitle']);
-      echo"
-      <td><input name=\"{$assignment_title}[{$student_key}]\" type=\"text\" size=\"3\" value=\"\" /></td>
-         ";
-   }
-}
-      for ($i = 0; $i < $assignment_count; $i++)
-         echo "";     
-?>   
-         <td>avg</td>
-      </tr>
-<?php
-}
+table_header($assignment_array);
+student_rows($assignment_array, $student_array, $student_grade_array);
 database_disconnect();
-?>      
-      <tr>
-         <th scope="col"><p>Subcategory Averages</p></th>
-<?php
-for ($i = 0; $i < $assignment_count; $i++)
-{
-   echo "<td>avg</td>";
-}
+subcategory_averages($assignment_count);
+category_averages($assignment_array);
+table_footer();
 ?>
-         <td>avg</td>
-      </tr>
-      </tr>
-      <tr>
-         <th scope="col">Category Averages </th>
-<?php
-foreach ($assignment_array as $category)
-{
-   $category_assignment_count = sizeof($category['assignments']);
-?>
-      <!-- colspan should equal the number of assignments -->
-      <td colspan="<? echo $category_assignment_count ?>">avg</td>
-      <!-- if colspan=1 then rowspan=2 -->
-<?php
-}
-?>
-         <td>avg</td>
-      </tr>
-      
-      </table>
+
          <input type="submit" id="submit" value="Submit Grades" />
          <input type="button" id="reset" value="Undo Changes" />
          <input type="button" id="reset" value="Edit Assignments" />
---
+++ www/tooltips/js/ajax-tooltip.js
@@ -0,0 +1,121 @@
+/************************************************************************************************************
+(C) www.dhtmlgoodies.com, June 2006
+
+This is a script from www.dhtmlgoodies.com. You will find this and a lot of other scripts at our website.	
+
+Terms of use:
+You are free to use this script as long as the copyright message is kept intact. However, you may not
+redistribute, sell or repost it without our permission.
+
+Thank you!
+
+www.dhtmlgoodies.com
+Alf Magne Kalleland
+
+************************************************************************************************************/
+
+
+/* Custom variables */
+
+/* Offset position of tooltip */
+var x_offset_tooltip = 5;
+var y_offset_tooltip = 0;
+
+/* Don't change anything below here */
+
+
+var ajax_tooltipObj = false;
+var ajax_tooltipObj_iframe = false;
+
+var ajax_tooltip_MSIE = false;
+if(navigator.userAgent.indexOf('MSIE')>=0)ajax_tooltip_MSIE=true;
+
+
+function ajax_showTooltip(externalFile,inputObj)
+{
+	if(!ajax_tooltipObj)	/* Tooltip div not created yet ? */
+	{
+		ajax_tooltipObj = document.createElement('DIV');
+		ajax_tooltipObj.style.position = 'absolute';
+		ajax_tooltipObj.id = 'ajax_tooltipObj';		
+		document.body.appendChild(ajax_tooltipObj);
+
+		
+		var leftDiv = document.createElement('DIV');	/* Create arrow div */
+		leftDiv.className='ajax_tooltip_arrow';
+		leftDiv.id = 'ajax_tooltip_arrow';
+		ajax_tooltipObj.appendChild(leftDiv);
+		
+		var contentDiv = document.createElement('DIV'); /* Create tooltip content div */
+		contentDiv.className = 'ajax_tooltip_content';
+		ajax_tooltipObj.appendChild(contentDiv);
+		contentDiv.id = 'ajax_tooltip_content';
+		
+		if(ajax_tooltip_MSIE){	/* Create iframe object for MSIE in order to make the tooltip cover select boxes */
+			ajax_tooltipObj_iframe = document.createElement('<IFRAME frameborder="0">');
+			ajax_tooltipObj_iframe.style.position = 'absolute';
+			ajax_tooltipObj_iframe.border='0';
+			ajax_tooltipObj_iframe.frameborder=0;
+			ajax_tooltipObj_iframe.style.backgroundColor='#FFF';
+			ajax_tooltipObj_iframe.src = 'about:blank';
+			contentDiv.appendChild(ajax_tooltipObj_iframe);
+			ajax_tooltipObj_iframe.style.left = '0px';
+			ajax_tooltipObj_iframe.style.top = '0px';
+		}
+
+			
+	}
+	// Find position of tooltip
+	ajax_tooltipObj.style.display='block';
+	ajax_loadContent('ajax_tooltip_content',externalFile);
+	if(ajax_tooltip_MSIE){
+		ajax_tooltipObj_iframe.style.width = ajax_tooltipObj.clientWidth + 'px';
+		ajax_tooltipObj_iframe.style.height = ajax_tooltipObj.clientHeight + 'px';
+	}
+
+	ajax_positionTooltip(inputObj);
+}
+
+function ajax_positionTooltip(inputObj)
+{
+	var leftPos = (ajaxTooltip_getLeftPos(inputObj) + inputObj.offsetWidth);
+	var topPos = ajaxTooltip_getTopPos(inputObj);
+	
+	/*
+	var rightedge=ajax_tooltip_MSIE? document.body.clientWidth-leftPos : window.innerWidth-leftPos
+	var bottomedge=ajax_tooltip_MSIE? document.body.clientHeight-topPos : window.innerHeight-topPos
+	*/
+	var tooltipWidth = document.getElementById('ajax_tooltip_content').offsetWidth +  document.getElementById('ajax_tooltip_arrow').offsetWidth; 
+	// Dropping this reposition for now because of flickering
+	//var offset = tooltipWidth - rightedge; 
+	//if(offset>0)leftPos = Math.max(0,leftPos - offset - 5);
+	
+	ajax_tooltipObj.style.left = leftPos + 'px';
+	ajax_tooltipObj.style.top = topPos + 'px';	
+	
+	
+}
+
+
+function ajax_hideTooltip()
+{
+	ajax_tooltipObj.style.display='none';
+}
+
+function ajaxTooltip_getTopPos(inputObj)
+{		
+  var returnValue = inputObj.offsetTop;
+  while((inputObj = inputObj.offsetParent) != null){
+  	if(inputObj.tagName!='HTML')returnValue += inputObj.offsetTop;
+  }
+  return returnValue;
+}
+
+function ajaxTooltip_getLeftPos(inputObj)
+{
+  var returnValue = inputObj.offsetLeft;
+  while((inputObj = inputObj.offsetParent) != null){
+  	if(inputObj.tagName!='HTML')returnValue += inputObj.offsetLeft;
+  }
+  return returnValue;
+}
