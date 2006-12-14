c604430add698e6208d0ae4b18e615fe3c265b6f
palmerc <palmerc@9c8e43f7-b2fe-0310-8927-e999a62a3b53>
2006-12-14 21:18:11 +0000

    working version

--- www/gradebook/studentImport.php
+++ www/gradebook/studentImport.php
@@ -4,19 +4,19 @@
    if ($_SERVER['REQUEST_METHOD'] == "POST")
    {
       $tmp_name = $_FILES['student_file']['tmp_name'];
-      $file = file_get_contents($tmp_name);
+      $file = file($tmp_name);
 
       database_connect();      
       foreach($file as $line)
       {
-         $student_info = preg_split(",", $line);
-         student_create($student_info[0], $student_info[1], $student_info[2], $student_info[3], $student_info[4], $student_info[5], $student_info[6], $student_info[7], 1);
+         $student_info = preg_split("/,/", $line);
+         student_create($student_info[0], $student_info[1], $student_info[2], $student_info[4], $student_info[6], $student_info[3], $student_info[5], $student_info[7], 1);
       }
       // Validate submission
       //print_r($_REQUEST);     
       
       database_disconnect();
-      //header('location:studentMain.php');
+      header('location:studentMain.php');
    }
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
