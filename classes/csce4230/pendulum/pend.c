d13edafbefbca00d60fcd66ed431e5b9f8a82074
palmerc <palmerc@9c8e43f7-b2fe-0310-8927-e999a62a3b53>
2007-02-20 00:54:16 +0000

    pendulum worky

--- classes/csce4230/pendulum/pend.c
+++ classes/csce4230/pendulum/pend.c
@@ -1,80 +1,125 @@
 #include <GL/glut.h>
 #include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-GLfloat angle = 180.0;
-GLint direction = 0;
+static GLfloat spin = 180.0;
+static GLfloat r = 1.0;
+static GLfloat l = 10.0;
+static GLuint direction = 0;
 
-/* x and y represent the center of the circle */
-void drawSolidCircle(float radius, float x, float y)
+void drawSolidCircle(void)
 {
-   int i;
-   double radians, vectorX, vectorY, lastX, lastY;
-   
-   lastX = x;
-   lastY = y;
-   glBegin(GL_TRIANGLES);
-   for (i = 0; i < 360; i++)
+   GLUquadric *qobj;
+   /* Note that no positional information is contained in this function */
+   qobj = gluNewQuadric();
+
+   gluQuadricDrawStyle(qobj, GLU_FILL); /* smooth shaded */
+   gluDisk(qobj, 0.0, r, 60, 1);
+}
+
+void spinDisplay(void)
+{
++_+++   if (direction == 0)
    {
-      /* Multiply an degree angle times pi/180 to get radians */
-      radians = (3.14159 * i) / 180;
-      vectorX = x + (radius * sin(radians));
-      vectorY = y + (radius * cos(radians));
-      glVertex2d(x, y);
-      glVertex2d(lastX, lastY);
-      glVertex2d(vectorX, vectorY);
-      lastX = vectorX;
-      lastY = vectorY;
+      spin = spin - 0.1;
+      if (spin < 0.0)
+         spin = spin + 360.0;
    }
-   glEnd();
+   else if (direction == 1)
+   {
+      spin = spin + 0.1;
+      if (spin > 360.0)
+         spin = spin - 360.0;
+   }
+   
+   glutPostRedisplay();
+}
+
+void reshape(int w, int h)
+{
+   GLfloat a = 1.25 * l;
+   
+   glViewport(0, 0, (GLsizei) w, (GLsizei) h);
+   if (w < h)
+   {
+      w = h;
+      glutReshapeWindow((GLsizei) w, (GLsizei) h);
+   }
+   else if (h < w)
+   {
+      h = w;
+      glutReshapeWindow((GLsizei) w, (GLsizei) h);
+   }
+   
+   glMatrixMode(GL_PROJECTION);
+   glLoadIdentity();
+   gluOrtho2D(-a, a, -a, a);
+   glMatrixMode(GL_MODELVIEW);
+   glLoadIdentity();
 }
 
 void display(void)
 {
 /* clear all pixels  */
    glClear (GL_COLOR_BUFFER_BIT);
-
-   glColor3f (1.0, 1.0, 1.0);
-   
-   glPushMatrix();
-      glRotatef(angle, 0.0, 0.0, 1.0);
+  
+   glPushMatrix();    
+      glRotatef(-spin, 0.0, 0.0, 1.0);
+      glColor3f (1.0, 1.0, 1.0);
       glBegin(GL_LINES);
-         glVertex3f (0.0, 0.0, 0.0);
-         glVertex3f (0.0, 0.50, 0.0);
+         glVertex2f (0.0, 0.0);
+         glVertex2f (0.0, l);
       glEnd();
+      glTranslatef(0.0, l, 0.0);
       glColor3f (1.0, 0.0, 0.0);
-      drawSolidCircle(0.10, 0.0, 0.60);
+      drawSolidCircle();
    glPopMatrix();
-   
-   glFinish ();
-   glutSwapBuffers();
+   glutSwapBuffers();   
 }
 
-void swing(void)
+void mouse(int button, int state, int x, int y)
 {
-   if (angle < 135.0)
-      direction = 0;
-   else if (angle > 225.0)
-      direction = 1;
-   
-   if (direction == 0)
-      angle += 0.025;
-   else if (direction == 1)
-      angle -= 0.025;
-   
-   glutPostRedisplay();
+   switch (button) {
+      case GLUT_LEFT_BUTTON:
+         if (state == GLUT_DOWN)
+            glutIdleFunc(spinDisplay);
+         break;
+         
+      case GLUT_MIDDLE_BUTTON:
+         if (state == GLUT_DOWN)
+            glutIdleFunc(NULL);
+         break;
+      default:
+         break;
+   }
+}
+
+void keyboard(unsigned char key, int x, int y)
+{
+   switch (key)
+   {
+      case 27:
+         exit(0);
+         break;
+      case 32:
+         glutIdleFunc(NULL);
+         break;
+      case 43:
+         direction = 0; /* Plus - counterclockwise */
+         glutIdleFunc(spinDisplay);
+         break;
+      case 45:
+         direction = 1; /* Minus - clockwise */
+         glutIdleFunc(spinDisplay);
+         break;
+   }
 }
 
 void init (void) 
 {
    glClearColor (0.0, 0.0, 0.0, 0.0);
-
-   glMatrixMode(GL_PROJECTION);
-   glLoadIdentity();
-   glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
-   glMatrixMode(GL_MODELVIEW);
-   glLoadIdentity();
-   glEnable(GL_LINE_SMOOTH);
+   glShadeModel(GL_FLAT);
 }
 
 int main(int argc, char** argv)
@@ -84,9 +129,11 @@ int main(int argc, char** argv)
    glutInitWindowSize (500, 500); 
    glutInitWindowPosition (100, 100);
    glutCreateWindow ("pendulum");
-   init ();
+   init();
    glutDisplayFunc(display);
-   glutIdleFunc(swing);
+   glutReshapeFunc(reshape);
+   glutKeyboardFunc(keyboard);
+   glutMouseFunc(mouse);
    glutMainLoop();
    return 0;   /* ANSI C requires main to return int. */
 }
