2560ffc18d7d92e60511087f1337e358e139a53c
palmerc <palmerc@9c8e43f7-b2fe-0310-8927-e999a62a3b53>
2007-05-01 03:34:16 +0000

    A Floyd Warshall calculator that is really only meant to demonstrate theoretical runtime

--- classes/csce4110/project/floyd-warshall/floyd-warshall.cpp
+++ classes/csce4110/project/floyd-warshall/floyd-warshall.cpp
@@ -1,15 +1,12 @@
 #include <limits>
+#include <map>
 #include <iostream>
+#include <fstream>
+#include <boost/regex.hpp>
+#include <boost/lexical_cast.hpp>
 
-int MAX_INT = 1000; // I need a better choice here
-
-int W[5][5] = { {0, 3, 8, MAX_INT, -4},
-	{MAX_INT, 0, MAX_INT, 1, 7},
-	{MAX_INT, 4, 0, MAX_INT, MAX_INT},
-	{2, MAX_INT, -5, 0, MAX_INT},
-	{MAX_INT, MAX_INT, MAX_INT, 6, 0} };
-int rows = 5;
-
+int adj[1000][1000];
+int rows;
 int min(int x, int y)
 {
 	return ((x) < (y) ? (x) : (y));
@@ -18,26 +15,62 @@ int min(int x, int y)
 void FloydWarshall()
 {
 	for (int k = 0; k < rows; ++k)
-    {
+    	{
 		for (int i = 0; i < rows; ++i)
 		{
 			for (int j = 0; j < rows; ++j)
 			{
-				W[i][j] = min(W[i][j], W[i][k] + W[k][j]);
+				adj[i][j] = min(adj[i][j], adj[i][k] + adj[k][j]);
 			}
 		}
-    }
+    	}
 }
 
-int main()
+int main(int argc, char *argv[])
 {
+	std::ifstream in;
+	in.open(argv[1]);
+	boost::regex re_rows("^\\s*r\\s+(\\d+)\\s*$", boost::regex::perl);
+	boost::regex re_vertex("^\\s*v\\s+(\\d+)\\s+(\\d+)\\s+(\\d+)\\s*$", boost::regex::perl);
+	boost::cmatch matches;
+	std::string str;
+
+	getline(in, str);
+	
+	for (int i = 0; i < 1000; ++i)
+	{
+		for (int j = 0; j < 1000; ++j)
+		{
+			adj[i][j] = 100000;
+		}
+	} 
+	while ( in )
+	{	
+		if (boost::regex_match(str.c_str(), matches, re_rows))
+		{
+			rows = boost::lexical_cast<int>(matches[1]);
+		}
+		else if (boost::regex_match(str.c_str(), matches, re_vertex))
+		{
+			int vertex = boost::lexical_cast<int>(matches[1]);
+			int node = boost::lexical_cast<int>(matches[2]);
+			int weight = boost::lexical_cast<int>(matches[3]);
+			// if it does exist just add the next item and distance to the appropriate node
+			adj[node][vertex] = weight;
+		}
+		getline(in, str);
+	}
+	in.close();
 	FloydWarshall();
 	
 	for (int i=0; i < rows; ++i)
     {
 	   for (int j=0; j < rows; ++j)
 	   {
-	      std::cout << W[i][j] << " ";
+		if (i == j)
+			std::cout << "0 ";
+		else
+			std::cout << adj[i][j] << " ";
        }
        std::cout << std::endl;
     }
