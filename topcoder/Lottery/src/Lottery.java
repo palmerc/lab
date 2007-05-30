1fe083d4d8bb96b50fb8b93c1209436795c165a2
palmerc <palmerc@9c8e43f7-b2fe-0310-8927-e999a62a3b53>
2007-05-30 22:01:45 +0000

    made some progress on the lottery

--- topcoder/Lottery/src/Lottery.java
+++ topcoder/Lottery/src/Lottery.java
@@ -1,7 +1,97 @@
+/* 
+	In most states, gamblers can choose from a wide variety of different lottery games.  The rules of a lottery are		
+	defined by two integers (choices and blanks) and two boolean variables (sorted and unique).  choices represents the 
+	highest valid number that you may use on your lottery ticket.  (All integers between 1 and choices, inclusive, are
+	valid and can appear on your ticket.)  blanks represents the number of spots on your ticket where numbers can be written.
+
+	The sorted and unique variables indicate restrictions on the tickets you can create.  If sorted is set to true, then 
+	the numbers on your ticket must be written in non-descending order.  If sorted is set to false, then the numbers may 
+	be written in any order.  Likewise, if unique is set to true, then each number you write on your ticket must be distinct. 
+	If unique is set to false, then repeats are allowed.
+
+Here are some example lottery tickets, where choices = 15 and blanks = 4:
+{3, 7, 12, 14} -- this ticket is unconditionally valid.
+{13, 4, 1, 9} -- because the numbers are not in nondescending order, this ticket is valid only if sorted = false.
+{8, 8, 8, 15} -- because there are repeated numbers, this ticket is valid only if unique = false.
+{11, 6, 2, 6} -- this ticket is valid only if sorted = false and unique = false.
+
+Given a list of lotteries and their corresponding rules, return a list of lottery names sorted by how easy they are to win. 
+The probability that you will win a lottery is equal to (1 / (number of valid lottery tickets for that game)).  The easiest
+lottery to win should appear at the front of the list.  Ties should be broken alphabetically (see example 1).
+
+DEFINITION
+Class:Lottery
+Method:sortByOdds
+Parameters:String[]
+Returns:String[]
+Method signature:String[] sortByOdds(String[] rules)
+
+
+CONSTRAINTS
+-rules will contain between 0 and 50 elements, inclusive.
+-Each element of rules will contain between 11 and 50 characters, inclusive.
+-Each element of rules will be in the format "<NAME>:_<CHOICES>_<BLANKS>_<SORTED>_<UNIQUE>" (quotes for clarity). The underscore
+character represents exactly one space. The string will have no leading or trailing spaces.
+-<NAME> will contain between 1 and 40 characters, inclusive, and will consist of only uppercase letters ('A'-'Z') and spaces (' '),
+with no leading or trailing spaces.
+-<CHOICES> will be an integer between 10 and 100, inclusive, with no leading zeroes.
+-<BLANKS> will be an integer between 1 and 8, inclusive, with no leading zeroes.
+-<SORTED> will be either 'T' (true) or 'F' (false).
+-<UNIQUE> will be either 'T' (true) or 'F' (false).
+-No two elements in rules will have the same name.
+
+
+EXAMPLES
+
+0)
+{"PICK ANY TWO: 10 2 F F"
+,"PICK TWO IN ORDER: 10 2 T F"
+,"PICK TWO DIFFERENT: 10 2 F T"
+,"PICK TWO LIMITED: 10 2 T T"}
+
+Returns: { "PICK TWO LIMITED",  "PICK TWO IN ORDER",  "PICK TWO DIFFERENT",  "PICK ANY TWO" }
+
+The "PICK ANY TWO" game lets either blank be a number from 1 to 10.  Therefore, there are 10 * 10 = 100 possible tickets, and
+your odds of winning are 1/100.
+
+The "PICK TWO IN ORDER" game means that the first number cannot be greater than the second number.  This eliminates 45 possible
+tickets, leaving us with 55 valid ones.  The odds of winning are 1/55.
+
+The "PICK TWO DIFFERENT" game only disallows tickets where the first and second numbers are the same.  There are 10 such tickets,
+leaving the odds of winning at 1/90.
+
+Finally, the "PICK TWO LIMITED" game disallows an additional 10 tickets from the 45 disallowed in "PICK TWO IN ORDER".  The 
+odds of winning this game are 1/45.
+
+1)
+{"INDIGO: 93 8 T F",
+ "ORANGE: 29 8 F T",
+ "VIOLET: 76 6 F F",
+ "BLUE: 100 8 T T",
+ "RED: 99 8 T T",
+ "GREEN: 78 6 F T",
+ "YELLOW: 75 6 F F"}
+
+
+Returns: { "RED",  "ORANGE",  "YELLOW",  "GREEN",  "BLUE",  "INDIGO",  "VIOLET" }
+
+Note that INDIGO and BLUE both have the exact same odds (1/186087894300).  BLUE is listed first because it comes before INDIGO
+alphabetically.
+
+2)
+{}
+
+Returns: { }
+
+Empty case
+*/
+
 import java.util.*;
 
 public class Lottery {
     public String[] sortByOdds(String[] rules) {
+                double possibilities;
+                
 		if((rules.length == 0) || (rules.length > 50)) {
 			return rules;
 		}
@@ -9,47 +99,54 @@ public class Lottery {
 		for(int i = 0; i < rules.length; ++i)
 		{
 			String game = new String();
-			int choices, blanks, start;
+			int choices, blanks, start, j;
 			boolean sorted, unique;
 			// Add check for >11 and <50
-			int j;
 			start = 0;
-			for(j = 0; j < rules[i].length(); ++j) {
-				if(rules[i].charAt(j) == ':')
-					break;
-			}
+                        j = rules[i].indexOf(':', start);
 			game = rules[i].substring(start, j);
-			System.out.println("Captured " + game);
 			
-			start = j+2;
-			for(; j < rules[i].length(); ++j) {
-				if(rules[i].charAt(j) == ' ')
-					break;
-			}
-			System.out.println("Choices " + rules[i].substring(start, j));
+                        start = j+2;
+                        j = rules[i].indexOf(' ', start);
 			choices = Integer.parseInt(rules[i].substring(start, j));
-			
-			
+						
 			start = j+1;
-			for(; j < rules[i].length(); ++j) {
-				if(rules[i].charAt(j) == ' ')
-					break;
-			}
+                        j = rules[i].indexOf(' ', start);
 			blanks = Integer.parseInt(rules[i].substring(start, j));
 			
 			start = j+1;
-			for(; j < rules[i].length(); ++j) {
-				if(rules[i].charAt(j) == ' ')
-					break;
-			}
-			sorted = Boolean.parseBoolean(rules[i].substring(start, j));
-			
+                        j = rules[i].indexOf(' ', start);
+                        if(rules[i].substring(start, j).equals("T")) {
+                            sorted = true;
+                        } else {
+                            sorted = false;
+                        }
+			//sorted = Boolean.parseBoolean(rules[i].substring(start, j));
+                        			
 			start = j+1;
-			for(; j < rules[i].length(); ++j) {
-				if(rules[i].charAt(j) == ' ')
-					break;
-			}
-			unique = Boolean.parseBoolean(rules[i].substring(start, j));
+                        if(rules[i].substring(start, start+1).equals("T")) {
+                            unique = true;
+                        } else {
+                            unique = false;
+                        }
+			//unique = Boolean.parseBoolean(rules[i].substring(start, start+1));
+                                                
+                        System.out.println("Parsed-> name: " + game + " choices: " + choices + " blanks: " + blanks + " sorted: " + sorted + " unique: " + unique);
+                        
+                        if(unique == true) {
+                            System.out.println("Factorial");
+                            // Factorial
+                            int factor = choices;
+                            possibilities = 1;
+                            for(int count = blanks - 1; count >= 0; count--) {
+                                possibilities *= choices - count;
+                            }      
+            
+                        } else {
+                            System.out.println("Power");
+                            possibilities = Math.pow(choices, blanks);
+                        }
+                        System.out.println("Possibilities " + possibilities);
 		}
 		return rules;
     }
@@ -191,90 +288,3 @@ public class Lottery {
     }
 }
 
-/* 
-	In most states, gamblers can choose from a wide variety of different lottery games.  The rules of a lottery are		
-	defined by two integers (choices and blanks) and two boolean variables (sorted and unique).  choices represents the 
-	highest valid number that you may use on your lottery ticket.  (All integers between 1 and choices, inclusive, are
-	valid and can appear on your ticket.)  blanks represents the number of spots on your ticket where numbers can be written.
-
-	The sorted and unique variables indicate restrictions on the tickets you can create.  If sorted is set to true, then 
-	the numbers on your ticket must be written in non-descending order.  If sorted is set to false, then the numbers may 
-	be written in any order.  Likewise, if unique is set to true, then each number you write on your ticket must be distinct. 
-	If unique is set to false, then repeats are allowed.
-
-Here are some example lottery tickets, where choices = 15 and blanks = 4:
-{3, 7, 12, 14} -- this ticket is unconditionally valid.
-{13, 4, 1, 9} -- because the numbers are not in nondescending order, this ticket is valid only if sorted = false.
-{8, 8, 8, 15} -- because there are repeated numbers, this ticket is valid only if unique = false.
-{11, 6, 2, 6} -- this ticket is valid only if sorted = false and unique = false.
-
-Given a list of lotteries and their corresponding rules, return a list of lottery names sorted by how easy they are to win. 
-The probability that you will win a lottery is equal to (1 / (number of valid lottery tickets for that game)).  The easiest
-lottery to win should appear at the front of the list.  Ties should be broken alphabetically (see example 1).
-
-DEFINITION
-Class:Lottery
-Method:sortByOdds
-Parameters:String[]
-Returns:String[]
-Method signature:String[] sortByOdds(String[] rules)
-
-
-CONSTRAINTS
--rules will contain between 0 and 50 elements, inclusive.
--Each element of rules will contain between 11 and 50 characters, inclusive.
--Each element of rules will be in the format "<NAME>:_<CHOICES>_<BLANKS>_<SORTED>_<UNIQUE>" (quotes for clarity). The underscore
-character represents exactly one space. The string will have no leading or trailing spaces.
--<NAME> will contain between 1 and 40 characters, inclusive, and will consist of only uppercase letters ('A'-'Z') and spaces (' '),
-with no leading or trailing spaces.
--<CHOICES> will be an integer between 10 and 100, inclusive, with no leading zeroes.
--<BLANKS> will be an integer between 1 and 8, inclusive, with no leading zeroes.
--<SORTED> will be either 'T' (true) or 'F' (false).
--<UNIQUE> will be either 'T' (true) or 'F' (false).
--No two elements in rules will have the same name.
-
-
-EXAMPLES
-
-0)
-{"PICK ANY TWO: 10 2 F F"
-,"PICK TWO IN ORDER: 10 2 T F"
-,"PICK TWO DIFFERENT: 10 2 F T"
-,"PICK TWO LIMITED: 10 2 T T"}
-
-Returns: { "PICK TWO LIMITED",  "PICK TWO IN ORDER",  "PICK TWO DIFFERENT",  "PICK ANY TWO" }
-
-The "PICK ANY TWO" game lets either blank be a number from 1 to 10.  Therefore, there are 10 * 10 = 100 possible tickets, and
-your odds of winning are 1/100.
-
-The "PICK TWO IN ORDER" game means that the first number cannot be greater than the second number.  This eliminates 45 possible
-tickets, leaving us with 55 valid ones.  The odds of winning are 1/55.
-
-The "PICK TWO DIFFERENT" game only disallows tickets where the first and second numbers are the same.  There are 10 such tickets,
-leaving the odds of winning at 1/90.
-
-Finally, the "PICK TWO LIMITED" game disallows an additional 10 tickets from the 45 disallowed in "PICK TWO IN ORDER".  The 
-odds of winning this game are 1/45.
-
-1)
-{"INDIGO: 93 8 T F",
- "ORANGE: 29 8 F T",
- "VIOLET: 76 6 F F",
- "BLUE: 100 8 T T",
- "RED: 99 8 T T",
- "GREEN: 78 6 F T",
- "YELLOW: 75 6 F F"}
-
-
-Returns: { "RED",  "ORANGE",  "YELLOW",  "GREEN",  "BLUE",  "INDIGO",  "VIOLET" }
-
-Note that INDIGO and BLUE both have the exact same odds (1/186087894300).  BLUE is listed first because it comes before INDIGO
-alphabetically.
-
-2)
-{}
-
-Returns: { }
-
-Empty case
-*/
