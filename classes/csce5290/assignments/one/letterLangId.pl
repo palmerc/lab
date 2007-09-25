036317a55b4a56e3f7374a88b8d3905258837ada
palmerc <palmerc@9c8e43f7-b2fe-0310-8927-e999a62a3b53>
2007-09-25 07:26:49 +0000

    part one done

---
+++ classes/csce5290/assignments/one/letterLangId.pl
@@ -0,0 +1,65 @@
+#!/usr/bin/perl -W
+
+use strict;
+
+use open ":utf8";
+binmode(STDOUT, ":utf8");
+open FRENCH, "<French.model";
+open ITALIAN, "<Italian.model";
+open ENGLISH, "<English.model";
+open TEST, "<LangId.test.utf8";
+
+# Read in language models
+my %frenchModel;
+foreach my $line (<FRENCH>) {
+	$line =~ m/(.{2})=> -?(\d+)/;
+	my $key = $1;
+	my $value = $2;
+	$frenchModel{"$key"} = $value;
+}
+my %italianModel;
+foreach my $line (<ITALIAN>) {
+	$line =~ m/(.{2})=> -?(\d+)/;
+	my $key = $1;
+	my $value = $2;
+	$italianModel{"$key"} = $value;
+}
+my %englishModel;
+foreach my $line (<ENGLISH>) {
+	$line =~ m/(.{2})=> -?(\d+)/;
+	my $key = $1;
+	my $value = $2;
+	$englishModel{"$key"} = $value;
+}
+
+# Generate results
+my $counter = 1;
+my $language;
+foreach my $line (<TEST>) {
+	chomp $line;
+	my $frenchProb;
+	my $italianProb;
+	my $englishProb;
+	my $previous = "\x{2318}";
+	my @chars = split("", $line);
+	foreach my $char (@chars) {
+                my $key = $previous . $char;
+		if (exists $frenchModel{"$key"}) {
+			$frenchProb += $frenchModel{"$key"};
+		}
+		if (exists $italianModel{"$key"}) {
+			$italianProb += $italianModel{"$key"};
+		}
+		if (exists $englishModel{"$key"}) {
+			$englishProb += $englishModel{"$key"};
+		}
+		$previous = $char;
+	}
+	my @stupidArray = ($frenchProb, $italianProb, $englishProb);
+	my $winner = (sort @stupidArray)[0];
+	if ($winner == $frenchProb) {$language = "French"};
+	if ($winner == $italianProb) {$language = "Italian"};
+	if ($winner == $englishProb) {$language = "English"};
+	print $counter . " " . $language . "\n";
+	$counter++;
+}
