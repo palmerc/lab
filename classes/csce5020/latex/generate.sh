f352dabb0bf92c84465e975cdb16042b7b335a89
palmerc <palmerc@9c8e43f7-b2fe-0310-8927-e999a62a3b53>
2007-10-28 22:56:39 +0000

    the latex sample document provides a reasonable run of the mill report

--- classes/csce5020/latex/generate.sh
+++ classes/csce5020/latex/generate.sh
@@ -3,6 +3,8 @@
 FILE=sample
 
 latex $FILE.tex
+# dvips will not respect letter size in the tex file it must be explicitly
+# set here or you will get A4
 dvips -t letter $FILE.dvi -o $FILE.ps
 ps2pdf $FILE.ps
 rm $FILE.log $FILE.aux $FILE.dvi $FILE.ps
