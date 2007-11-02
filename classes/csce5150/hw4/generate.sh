ccfaaf432f4583f42b98b2a87cce26b07c46313d
palmerc <palmerc@9c8e43f7-b2fe-0310-8927-e999a62a3b53>
2007-11-02 17:22:02 +0000

    done with hw4

--- classes/csce5150/hw4/generate.sh
+++ classes/csce5150/hw4/generate.sh
@@ -3,9 +3,9 @@
 FILE=hw4
 
 latex $FILE
-bibtex $FILE
-latex $FILE
-latex $FILE
+#bibtex $FILE
+#latex $FILE
+#latex $FILE
 dvips -t letter $FILE.dvi -o $FILE.ps
 ps2pdf $FILE.ps
 rm $FILE.log $FILE.aux $FILE.dvi $FILE.ps $FILE.blg $FILE.bbl
