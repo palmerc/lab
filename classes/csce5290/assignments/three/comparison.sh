a1aa56827057eec0d615a0420040ba8c419bfdbd
palmerc <palmerc@9c8e43f7-b2fe-0310-8927-e999a62a3b53>
2007-11-18 02:17:48 +0000

    about to start adding keywords in context

--- classes/csce5290/assignments/three/comparison.sh
+++ classes/csce5290/assignments/three/comparison.sh
@@ -3,6 +3,7 @@
 for word in bass crane motion palm plant tank
 do
 	./baseline.pl TWA.sensetagged/$word.train TWA.sensetagged/$word.test	
-	./program3.pl TWA.sensetagged/$word.train TWA.sensetagged/$word.test	
+	./program3-1.pl TWA.sensetagged/$word.train TWA.sensetagged/$word.test	
+	./program3-2.pl TWA.sensetagged/$word.train TWA.sensetagged/$word.test	
 	echo
 done
