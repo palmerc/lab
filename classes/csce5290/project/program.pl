dfc3073e72cc37cf329e71eff386fb442546140c
palmerc <palmerc@9c8e43f7-b2fe-0310-8927-e999a62a3b53>
2007-11-26 02:35:53 +0000

    seems the problem with the parsing of the data file is once again diacritics

--- classes/csce5290/project/program.pl
+++ classes/csce5290/project/program.pl
@@ -7,25 +7,27 @@ use HTML::Strip;
 binmode(STDOUT, ":utf8");
 
 my $data;
-my $xml;
-my $inputFile;
+my ($xml, $webDataXml, $webPagesXml);
+my ($webDataFile, $webPagesFile);
 
-if (scalar(@ARGV) < 1) {
-	print("Usage: $0 inputFile\n");
+if (scalar(@ARGV) < 2) {
+	print("Usage: $0 webDataFile webPagesFile\n");
 	exit 1;
 } else {
-	$inputFile = $ARGV[0];
+	$webDataFile = $ARGV[0];
+	$webPagesFile = $ARGV[1];
 }
 
 $xml = new XML::Simple;
-$data = $xml->XMLin($inputFile);
+$webDataXml = $xml->XMLin($webDataFile);
+$webPagesXml = $xml->XMLin($webPagesFile);
 
-my $person = $data->{'name'};
-foreach my $documentId (keys %{$data->{'WEPS:document'}}) {
+my $person = $webPagesXml->{'name'};
+foreach my $documentId (keys %{$webPagesXml->{'WEPS:document'}}) {
 	print "\n\nKey: $documentId\n";
 	my $fileNameTmp = "/tmp/weps$documentId.tmp";
 	open(TEMP, ">", "$fileNameTmp");
-	print TEMP $data->{'WEPS:document'}{$documentId}{'content'};
+	print TEMP $webPagesXml->{'WEPS:document'}{$documentId}{'content'};
 	close(TEMP);
 	my $tidyHtml = `/usr/bin/tidy -q -i -c -b $fileNameTmp 2>/dev/null`;
 	`rm $fileNameTmp`;
@@ -35,3 +37,5 @@ foreach my $documentId (keys %{$data->{'WEPS:document'}}) {
 	$strippedHtml =~ s/\s\s*/ /g;
 	print $strippedHtml;
 }
+
+print Dumper($webDataXml);
