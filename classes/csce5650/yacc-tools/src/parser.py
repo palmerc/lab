b32a4aa01e765cd287b8f1a56145fed24d279467
palmerc <palmerc@9c8e43f7-b2fe-0310-8927-e999a62a3b53>
2008-02-19 19:53:22 +0000

    progress on the parser for yacc grammar

--- classes/csce5650/yacc-tools/src/parser.py
+++ classes/csce5650/yacc-tools/src/parser.py
@@ -1,28 +1,85 @@
 import sys
 from lexer import lex
 
+token = None
+grammar = None
+
+def yaccgrammar():
+	tokens()
+	start()
+	productions()
+
+def tokens():
+	while token[0] == 'TOKEN':
+		match('TOKEN')
+		while token[0] == 'TERM':
+			match('TERM')
+	
+def start():
+	match('START')
+	match('NONTERM')
+	
+def productions():
+	match('BLOCK')
+	while token != 'BLOCK':
+		left_side()
+	match('BLOCK')
+	
+def left_side():
+	match('NONTERM')
+	match('COLON')
+	right_side()
+
+def right_side():
+	if 
+	match('TERM')
+	expression()
+	match('PIPE')
+	if token == 'NONTERM':
+		match('NONTERM')
+	match('SEMI')
+	
+def match(m):
+	global token, grammar
+	
+	print token
+	if m == token[0]:
+		print m
+		token = lex(grammar)
+	else:
+		print 'error', m
+
+
 def parser():
-	pass
+	global token, grammar
+	
+	token = lex(grammar)
+	yaccgrammar()
         
 def main(argv):
-    try:
-        filename = argv[0]
-    except IndexError:
-        print "Usage: %s grammar.yacc" % sys.argv[0]
-        sys.exit(2)
+	global grammar
+	
+	try:
+		filename = argv[0]
+	except IndexError:
+		print "Usage: %s grammar.yacc" % sys.argv[0]
+		sys.exit(2)
     
-    try:
-        f = open(filename, "r")
-    except IOError:
-        print "Unable to open file", filename
-        sys.exit(2)
+	try:
+		f = open(filename, "r")
+	except IOError:
+		print "Unable to open file", filename
+		sys.exit(2)
 
-    G = ""
-    for line in f:
-        G += line
-    f.close()
+	G = ""
+	for line in f:
+		G += line
+	f.close()
+      
+	grammar = iter(G)
     
-    parser()
+	parser()
+
 
 if __name__ == "__main__":
     main(sys.argv[1:])
