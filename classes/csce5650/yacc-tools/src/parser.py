cf6c63423503c76f14970a38e98488e1c9c78af9
palmerc <palmerc@9c8e43f7-b2fe-0310-8927-e999a62a3b53>
2008-02-20 22:34:08 +0000

    first version of the roundtrip yacc parser works. Now for that left recursion elimination

--- classes/csce5650/yacc-tools/src/parser.py
+++ classes/csce5650/yacc-tools/src/parser.py
@@ -1,8 +1,18 @@
 import sys
 from lexer import lex
+from grammar import Rule
+from grammar import Production
+from grammar import Grammar
+from grammar import PrettyPrint
 
+source = None
 token = None
 grammar = None
+rule = None
+rules = []
+pductions = []
+terminals = []
+start = ''
 
 def yaccgrammar():
 	tokens()
@@ -10,57 +20,82 @@ def yaccgrammar():
 	productions()
 
 def tokens():
+	global terminals
+	
 	while token[0] == 'TOKEN':
 		match('TOKEN')
 		while token[0] == 'TERM':
+			terminals.append(token[1])
 			match('TERM')
 	
 def start():
+	global start
+	
 	match('START')
+	start = token[1]
 	match('NONTERM')
 	
 def productions():
+	global grammar, start, terminals
+	
 	match('BLOCK')
 	while token[0] != 'BLOCK':
 		left_side()
+	grammar = Grammar(pductions)
+	grammar.start = start
+	grammar.tokens = terminals
 	
 def left_side():
+	global rules
+	
+	rules = []
+	production = token[1]
 	match('NONTERM')
 	match('COLON')
 	right_side()
+	rules.append(Rule(rule))
+	pductions.append(Production(production, rules))
 	match('SEMI')
 
 def right_side():
+	global rule
+	rule = ''
+	
 	while token[0] != 'SEMI':
 		if token[0] == 'TERM':
+			rule += token[1]
 			match('TERM')
 		elif token[0] == 'NONTERM':
+			rule += token[1]
 			match('NONTERM')
 		elif token[0] == 'CHAR':
+			rule += token[1]
 			match('CHAR')
 		elif token[0] == 'PIPE':
+			rules.append(Rule(rule))
 			match('PIPE')
 			right_side()
 		else:
 			print 'right_side:', token
+		rule += ' '
 	
 def match(m):
-	global token, grammar
+	global token, source
 	
 	if m == token[0]:
-		token = lex(grammar)
+		token = lex(source)
 	else:
 		print 'match error', m
 
 
 def parser():
-	global token, grammar
+	global token, source
 	
-	token = lex(grammar)
+	token = lex(source)
 	yaccgrammar()
         
 def main(argv):
-	global grammar
+	global source, grammar
 	
 	try:
 		filename = argv[0]
@@ -79,10 +114,11 @@ def main(argv):
 		G += line
 	f.close()
       
-	grammar = iter(G)
+	source = iter(G)
     
 	parser()
-
+	
+	PrettyPrint(grammar).printer()
 
 if __name__ == "__main__":
     main(sys.argv[1:])
