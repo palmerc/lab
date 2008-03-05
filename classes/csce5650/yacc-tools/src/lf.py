bc8a1029d328cc2af04aa6b6b0e8ebc7e07b0c23
palmerc <palmerc@9c8e43f7-b2fe-0310-8927-e999a62a3b53>
2008-03-05 16:13:15 +0000

    Created the left factoring only version of the main program

---
+++ classes/csce5650/yacc-tools/src/lf.py
@@ -0,0 +1,30 @@
+import sys
+from parser import Parser
+from grammar import Transform
+
+def main(argv):
+	# open file
+	try:
+		filename = argv[0]
+	except IndexError:
+		print "Usage: %s grammar.yacc" % sys.argv[0]
+		sys.exit(2)
+	
+	try:
+		f = open(filename, "r")
+	except IOError:
+		print "Unable to open file", filename
+		sys.exit(2)
+
+	G = f.read()
+	f.close()
+       
+	grammar = Parser(G)
+	
+	t = Transform(grammar)
+	t.lf()
+	
+	grammar.printer()
+	
+if __name__ == "__main__":
+	main(sys.argv[1:])
