1f772e418dc0c75147b7362e0eeae8ef9e8718ea
palmerc <palmerc@9c8e43f7-b2fe-0310-8927-e999a62a3b53>
2008-09-22 15:47:27 +0000

    Starting homework one for proglang

---
+++ classes/csce5450/haskell/hw1.hs
@@ -0,0 +1,20 @@
+data T = C [T] deriving (Eq,Ord,Read,Show)
+
+parser cs = parse_pars '(' ')' cs
+
+parse_pars l r cs | newcs == [] = t where
+   (t,newcs)=pars_expr l r cs
+
+   pars_expr l r (c:cs) | c==1 = ((C ts),newcs) where
+      (ts,newcs)=pars_list l r cs
+
+   pars_list l r (c:cs) | c==r = ([],cs)
+   pars_list l r (c:cs) = ((t:ts),cs2) where
+      (t,cs1)=pars_expr l r (c:cs)
+      (ts,cs2)=pars_list l r cs1
+
+printer = collect_pars "(" ")" where
+   collect_pars l r (C ns) =
+      l++
+         (concatMap (collect_pars l r) ns)
+      ++r
