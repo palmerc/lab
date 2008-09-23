ae2276b4d68542695709386e153cb40ac4d9aa7a
palmerc <palmerc@9c8e43f7-b2fe-0310-8927-e999a62a3b53>
2008-09-23 21:06:03 +0000

    Simplified version of Tarau program

--- classes/csce5450/haskell/hw1.hs
+++ classes/csce5450/haskell/hw1.hs
@@ -10,16 +10,17 @@ t1 = "f(one,g(two,three),h3(four))"
 t2 = "term1(atom1,term2(atom2,atom3))"
 
 -- Parse the string representation of the tree
-stringToTree cs | newcs == [] = t where
-	(t,newcs)=pars_expr cs
+stringToTree chars | newcs == [] = t where
+	(t,newcs)=pars_expr chars
 
-	pars_expr (c:cs) | c=='(' = ((Term ts),newcs) where
-		(ts,newcs) = pars_list cs
+	pars_expr (char:chars) | char=='(' = ((Term ts),newcs) where
+		(ts,newcs) = pars_list chars
 
-	pars_list (c:cs) | c==')' = ([],cs)
-	pars_list (c:cs) = ((t:ts),cs2) where
-		(t,cs1)=pars_expr (c:cs)
-		(ts,cs2)=pars_list cs1
+	pars_list (char:chars) | char==')' = ([],chars)
+
+	pars_list (char:chars) = ((t:ts),chars2) where
+		(t,chars1)=pars_expr (char:chars)
+		(ts,chars2)=pars_list chars1
 
 -- Generate the string representation of the tree
 -- toString :: Tree -> String
