9bbc41e207d5ec58cef889fcc6621287609a13b1
palmerc <palmerc@9c8e43f7-b2fe-0310-8927-e999a62a3b53>
2008-09-23 20:22:11 +0000

    Simplified version of Tarau program

--- classes/csce5450/haskell/hw1.hs
+++ classes/csce5450/haskell/hw1.hs
@@ -1,16 +1,25 @@
 module Homework1 where
 
 -- The provided data structure
-data Tree = Atom String
-	| Term String [Tree] deriving (Eq,Ord,Read,Show)
+-- data Tree a = Atom String
+--	| Term String (Tree a) deriving (Eq,Ord,Read,Show)
+data Tree = Term [Tree] deriving (Eq,Ord,Read,Show)
 
 -- These are the sample trees
 t1 = "f(one,g(two,three),h3(four))"
 t2 = "term1(atom1,term2(atom2,atom3))"
 
 -- Parse the string representation of the tree
-toTree :: String -> Tree
-toTree tstring = 
+stringToTree cs | newcs == [] = t where
+	(t,newcs)=pars_expr cs
+
+	pars_expr (c:cs) | c=='(' = ((Term ts),newcs) where
+		(ts,newcs) = pars_list cs
+
+	pars_list (c:cs) | c==')' = ([],cs)
+	pars_list (c:cs) = ((t:ts),cs2) where
+		(t,cs1)=pars_expr (c:cs)
+		(ts,cs2)=pars_list cs1
 
 -- Generate the string representation of the tree
-toString :: Tree -> String
+-- toString :: Tree -> String
