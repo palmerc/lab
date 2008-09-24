a7512aa9b51dde5ba0d856b8a1cbafb3892d5fd3
palmerc <palmerc@9c8e43f7-b2fe-0310-8927-e999a62a3b53>
2008-09-24 03:14:50 +0000

    latest broken version

--- classes/csce5450/haskell/hw1.hs
+++ classes/csce5450/haskell/hw1.hs
@@ -1,26 +1,14 @@
 module Homework1 where
 
 -- The provided data structure
--- data Tree a = Atom String
---	| Term String (Tree a) deriving (Eq,Ord,Read,Show)
-data Tree = Term [Tree] deriving (Eq,Ord,Read,Show)
+data Tree = Atom String
+	| Term String [Tree] deriving (Eq,Ord,Read,Show)
 
 -- These are the sample trees
-t1 = "f(one,g(two,three),h3(four))"
-t2 = "term1(atom1,term2(atom2,atom3))"
+s1 = "f(one,g(two,three),h3(four))"
+s2 = "term1(atom1,term2(atom2,atom3))"
+t1 = Term "f" [Atom "one", Term "g" [Atom "two", Atom "three"], Term "h3" [Atom "four"]]
+t2 = Term "term1" [Atom "atom1", Term "term2" [Atom "atom2", Atom "atom3"]]
 
 -- Parse the string representation of the tree
-stringToTree chars | newcs == [] = t where
-	(t,newcs)=pars_expr chars
-
-	pars_expr (char:chars) | char=='(' = ((Term ts),newcs) where
-		(ts,newcs) = pars_list chars
-
-	pars_list (char:chars) | char==')' = ([],chars)
-
-	pars_list (char:chars) = ((t:ts),chars2) where
-		(t,chars1)=pars_expr (char:chars)
-		(ts,chars2)=pars_list chars1
-
 -- Generate the string representation of the tree
--- toString :: Tree -> String
