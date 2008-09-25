27c67ad886f47de15bda10b46f3dfe98dcb83a39
palmerc <palmerc@9c8e43f7-b2fe-0310-8927-e999a62a3b53>
2008-09-25 04:50:31 +0000

    Well almost there

--- classes/csce5450/haskell/cleaner.hs
+++ classes/csce5450/haskell/cleaner.hs
@@ -1,5 +1,7 @@
 module TreeThings where
 
+import Data.Char
+
 data T = C [T] deriving (Eq,Ord,Read,Show)
 data Term a = Atom String
 	| Term String [Term a] deriving (Eq,Ord,Read,Show)
@@ -9,16 +11,23 @@ s2 = "term1(atom1,term2(atom2,atom3))"
 t1 = Term "f" [Atom "one", Term "g" [Atom "two", Atom "three"], Term "h3" [Atom "four"]]
 t2 = Term "term1" [Atom "atom1", Term "term2" [Atom "atom2", Atom "atom3"]]
 
-parse_pars cs = case pars_expr cs of
+stringToTerm :: String -> Maybe (Term a)
+stringToTerm cs = case pars_expr cs of
                   Just (t, []) -> Just t
                   _            -> Nothing
-  where pars_expr (c:cs) | c=='(' = do (ts, cs') <- pars_list cs
-                                       return (C ts, cs')
-        pars_expr _ = Nothing
+  where pars_expr cs = let (ident, rest) = span isAlphaNum cs
+                       in case rest of
+                         '(' : rest' -> do (ts, cs') <- pars_list rest'
+                                           return (Term ident ts, cs')
+                         _           -> return (Atom ident, rest)
 
         pars_list (c:cs) | c==')' = return ([],cs)
-
         pars_list (c:cs) = do (t, cs1) <- pars_expr (c:cs)
-                              (ts,cs2) <- pars_list cs1
-                              return ((t:ts),cs2)
+                              let cs2 = case cs1 of
+                                    ',' : x -> x
+                                    _       -> cs1
+                              (ts,cs3) <- pars_list cs2
+                              return ((t:ts),cs3)
         pars_list _ = Nothing
+
+
--- classes/csce5450/haskell/hw1.hs
+++ classes/csce5450/haskell/hw1.hs
@@ -1,8 +1,9 @@
 -- Cameron Palmer, Jorge Reyes, Kaci Irvin
 module TreeThings where
 
+import Data.Char
+
 -- The provided data structure
-data T = C [T] deriving (Eq,Ord,Read,Show)
 data Term a = Atom String
         | Term String [Term a] deriving (Eq,Ord,Read,Show)
 
@@ -13,21 +14,24 @@ t1 = Term "f" [Atom "one", Term "g" [Atom "two", Atom "three"], Term "h3" [Atom 
 t2 = Term "term1" [Atom "atom1", Term "term2" [Atom "atom2", Atom "atom3"]]
 
 -- Parse the string representation of the tree
-stringToTerm :: String -> Maybe T
+stringToTerm :: String -> Maybe (Term a)
 stringToTerm cs = case pars_expr cs of
                   Just (t, []) -> Just t
                   _            -> Nothing
-  where pars_expr (c:cs) | c=='(' = do (ts, cs') <- pars_list cs
-                                       return (C ts, cs')
-        pars_expr _ = Nothing
+  where pars_expr cs = let (ident, rest) = span isAlphaNum cs
+                       in case rest of
+                         '(' : rest' -> do (ts, cs') <- pars_list rest'
+                                           return (Term ident ts, cs')
+                         _           -> return (Atom ident, rest)
 
         pars_list (c:cs) | c==')' = return ([],cs)
-
         pars_list (c:cs) = do (t, cs1) <- pars_expr (c:cs)
-                              (ts,cs2) <- pars_list cs1
-                              return ((t:ts),cs2)
+                              let cs2 = case cs1 of
+                                    ',' : x -> x
+                                    _       -> cs1
+                              (ts,cs3) <- pars_list cs2
+                              return ((t:ts),cs3)
         pars_list _ = Nothing
-
 
 -- Generate the string representation of the tree
 termToString :: Term a -> String
