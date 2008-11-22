d555deee5fff2f7e0798eabb354d327efd2636f0
palmerc <palmerc@9c8e43f7-b2fe-0310-8927-e999a62a3b53>
2008-11-22 04:38:04 +0000

    We can now grab the buildings from the campus map

--- www/cameronpalmer.com/unt/bin/untBuildingDownload.py
+++ www/cameronpalmer.com/unt/bin/untBuildingDownload.py
@@ -5,15 +5,25 @@ __copyright__ = "Copyright 2008, Cameron Palmer"
 __version__ = "$Rev$"
 __license__ = "GPL"
 
+import re
 import urllib2
+from htmlentitydefs import name2codepoint
 
-url = 'http://www.unt.edu/pais/map/campusmap.htm'
-dataDir = '/var/www/cameronpalmer.com/unt/data/buildings/'
-csvFile = 'untBuildings.csv'
+url = "http://web3.unt.edu/urcm/campusmap/buildings.cfm"
+
+def utf8(s):
+    return unicode(s, "utf-8")
+
+def htmlentitydecode(s):
+    return re.sub('&(%s);' % '|'.join(name2codepoint), 
+            lambda m: unichr(name2codepoint[m.group(1)]), s)
+
+bldgFromJson = re.compile("bldgFromJson\((.+)\);")
 
 r = urllib2.urlopen(url)
+x = r.read()
 
-d = r.read().splitlines(True)
-buildings = eval(d[238][14:-5])
+buildings = eval(bldgFromJson.search(x).group(1))
 
-print buildings
+for building in buildings:
+    print htmlentitydecode(utf8('"' + building['id'] + '","' + building['label'] + '"'))
