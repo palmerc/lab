c5d81100d0515700e447bc5c0ad4a0e9788759f7
palmerc <palmerc@9c8e43f7-b2fe-0310-8927-e999a62a3b53>
2008-11-23 17:31:18 +0000

    Removed stray slash

--- www/cameronpalmer.com/unt/untClassScheduleHandler.sh
+++ www/cameronpalmer.com/unt/untClassScheduleHandler.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /var/www/cameronpalmer.com/unt/bin
 /var/www/cameronpalmer.com/unt/bin/untClassScheduleDownload.py
-/var/www/cameronpalmer.com//unt/bin/untPDFTranslate.py
+/var/www/cameronpalmer.com/unt/bin/untPDFTranslate.py
