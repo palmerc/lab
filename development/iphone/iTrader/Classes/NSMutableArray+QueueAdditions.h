4dabfbc76076a20d36b5d0a85ddb39a52ebff284
palmerc <palmerc@9c8e43f7-b2fe-0310-8927-e999a62a3b53>
2010-01-19 11:51:54 +0000

    Changed the name to go along with the official standard

---
+++ development/iphone/iTrader/Classes/NSMutableArray+QueueAdditions.h
@@ -0,0 +1,17 @@
+//
+//  queue.h
+//  mTrader
+//
+//  Created by Cameron Lowell Palmer on 21.12.09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface NSMutableArray (QueueAdditions) 
+
+-(void) enQueue:(id)anObject;
+-(id) deQueue;
+
+@end
