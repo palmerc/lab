1f213d20234545b329dfdbb1889b126c09f86221
palmerc <palmerc@9c8e43f7-b2fe-0310-8927-e999a62a3b53>
2010-05-20 12:35:07 +0000

    The pyTrader Tool

---
+++ development/tools/pyTrader/Login.py
@@ -0,0 +1,34 @@
+class Login:
+	"""Represents an authentication message"""
+
+	def __init__(self):
+		self.username = ''
+		self.password = ''
+		self.platform = ''
+		self.client = ''
+		self.version = ''
+		self.protocol = ''
+		self.connection = 'socket'
+		self.streaming = 1
+
+
+	def __str__(self):
+		actionString = 'Action: Login\n'
+		authorizationString = 'Authorization: %s/%s\n' % (self.username, self.password)
+		platformString = 'Platform: %s\n' % (self.platform)
+		clientString = 'Client: %s\n' % (self.client)
+		protocolString = 'Protocol: %s\n' % (self.protocol)
+		versionString = 'VerType: %s\n' % (self.version)
+		connectionString = 'ConnType: %s\n' % (self.connection)
+		streamingString = 'Streaming: %d\n' % (self.streaming)
+
+		loginRequest = actionString + \
+			authorizationString + \
+			platformString + \
+			clientString + \
+			protocolString + \
+			versionString + \
+			connectionString + \
+			streamingString
+
+		return loginRequest
