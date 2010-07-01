2289b7a154cbefe691d63f84ef023ef1fe35b4ee
palmerc <palmerc@9c8e43f7-b2fe-0310-8927-e999a62a3b53>
2010-07-01 10:39:22 +0000

    Communicator is now strictly send and receive. No longer making any assumptions about being line oriented.

--- development/iphone/mTrader/trunk/Classes/Communicator.h
+++ development/iphone/mTrader/trunk/Classes/Communicator.h
@@ -1,52 +1,52 @@
 //
 //  Communicator.h
-//  simpleNetworking - This class should only define a simple method for
+//  Simple Asynchronous Networking - This class should only define a simple method for
 //    connecting to an arbitrary socket. It shouldn't assume anything.
 //
 //  Created by Cameron Lowell Palmer on 17.12.09.
-//  Copyright 2009 Infront AS. All rights reserved.
+//  Copyright 2010 Infront AS. All rights reserved.
 //
 
+@protocol CommunicatorStatusDelegate;
 @protocol CommunicatorDataDelegate;
 
-@interface Communicator : NSObject {
+@interface Communicator : NSObject /*<NSStreamDelegate>*/ {
 @private
-	id <CommunicatorDataDelegate> delegate;
+	id <CommunicatorStatusDelegate> _statusDelegate;
+	id <CommunicatorDataDelegate> _dataDelegate;
 	
 	NSString *_host;
-	NSInteger _port;
+	NSUInteger _port;
 	
 	NSInputStream *_inputStream;
 	NSOutputStream *_outputStream;
-	NSMutableArray *_lineBuffer;
-	NSMutableData *_mutableDataBuffer;
 	
+	NSMutableData *_inboundBuffer;
+	NSMutableData *_outboundBuffer;
+		
 	NSUInteger _bytesReceived;
 	NSUInteger _bytesSent;
 }
 
-@property (nonatomic, assign) id <CommunicatorDataDelegate> delegate;
-@property (nonatomic, retain) NSString *host;
-@property NSInteger port;
-@property (nonatomic, retain) NSInputStream *inputStream;
-@property (nonatomic, retain) NSOutputStream *outputStream;
-@property (nonatomic, retain) NSMutableArray *lineBuffer;
-@property (nonatomic, retain) NSMutableData *mutableDataBuffer;
-- (void)readAvailableBytes:(NSInputStream *)aStream;
-- (void)dataReceived:(NSData *)dataBlock;
+@property (nonatomic, assign) id <CommunicatorStatusDelegate> statusDelegate;
+@property (nonatomic, assign) id <CommunicatorDataDelegate> dataDelegate;
+
+@property (readonly) NSString *host;
+@property (readonly) NSUInteger port;
 @property (readonly) NSUInteger bytesReceived;
 @property (readonly) NSUInteger bytesSent;
 
-- (id)initWithSocket:(NSString *)host onPort:(NSInteger)port;
-- (void)startConnection;
+- (void)startConnectionWithSocket:(NSString *)host onPort:(NSUInteger)port;
 - (void)stopConnection;
-- (void)writeString:(NSString *)string;
-- (NSData *)readLine;
+
+- (void)sendData:(NSData *)data;
+@end
+
+@protocol CommunicatorStatusDelegate <NSObject>
+- (void)connect;
+- (void)disconnect;
 @end
 
 @protocol CommunicatorDataDelegate <NSObject>
-- (void)dataReceived;
-- (void)connected;
-- (void)disconnected;
+- (void)receivedData:(NSData *)data;
 @end
-
