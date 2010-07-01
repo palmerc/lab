634019727c9418f24388ad409ceb32aac9649be6
palmerc <palmerc@9c8e43f7-b2fe-0310-8927-e999a62a3b53>
2010-07-01 12:35:51 +0000

    LineOrientedCommunication handles incoming data line discipline without worrying about the data encoding mechanism. It only cares about CRLF

---
+++ development/iphone/mTrader/trunk/Classes/LineOrientedCommunication.m
@@ -0,0 +1,119 @@
+//
+//  LineOrientedCommunication.m
+//  mTrader
+//
+//  Created by Cameron Lowell Palmer on 01.07.10.
+//  Copyright 2010 Infront AS. All rights reserved.
+//
+
+#define DEBUG_INCOMING 0
+#define DEBUG_OUTGOING 0
+#define DEBUG_LEFTOVERS 0
+
+#import "LineOrientedCommunication.h"
+
+@interface LineOrientedCommunication ()
+- (void)processDataBuffer;
+@end
+
+
+@implementation LineOrientedCommunication
+@synthesize dataDelegate = _dataDelegate;
+
+#pragma mark -
+#pragma mark Initialization
+
+- (id)init {
+	self = [super init];
+	if (self != nil) {
+		_dataBuffer = nil;
+	}
+	return self;
+}
+
+
+#pragma mark -
+#pragma mark CommunicatorDataDelegate methods
+
+- (void)receivedData:(NSData *)data {
+	if (_dataBuffer == nil) {
+		_dataBuffer = [[NSMutableData alloc] init];
+	}
+
+	[_dataBuffer appendData:data];
+	
+	if ([_dataBuffer length] > 0) {
+		[self processDataBuffer];	
+	}
+}
+
+
+#pragma mark -
+#pragma mark Data to Lines Methods
+
+- (void)processDataBuffer {
+	const char* characters = [_dataBuffer mutableBytes];
+	
+	NSRange lineRange;
+	lineRange.location = 0;
+	lineRange.length = 0;
+	
+	NSRange dataBufferRange;
+	dataBufferRange.location = 0;
+	dataBufferRange.length = 0;
+	
+	char current = ' ';
+	char previous = ' ';
+	// Find all the lines ending with CRLF
+	for (int i = 0; i < [_dataBuffer length]; i++) {
+		current = characters[i];
+		if (previous == '\r' && current == '\n') {
+			lineRange.length = (i + 1) - lineRange.location;
+			NSData *oneLine = [_dataBuffer subdataWithRange:lineRange];
+			
+			if (self.dataDelegate && [self.dataDelegate respondsToSelector:@selector(receivedDataLine:)]) {
+				[self.dataDelegate receivedDataLine:oneLine];
+			}
+			
+#if DEBUG_INCOMING
+			NSString *lineString = [[NSString alloc] initWithData:oneLine encoding:NSISOLatin1StringEncoding];
+			NSLog(@"LineOrientedCommunication");
+			NSLog(@"Raw: %@", oneLine);
+			NSLog(@"Text: %@", lineString);
+			[lineString release];
+#endif	
+			
+			// Advance to the next character
+			lineRange.location = i + 1;
+			dataBufferRange.location = i + 1;
+		}
+		
+		previous = current;
+	}
+	
+	// Stuff the leftovers back into the buffer at the beginning
+	dataBufferRange.length = [_dataBuffer length] - lineRange.location;
+	
+	NSData *leftovers = [_dataBuffer subdataWithRange:dataBufferRange];
+	
+#if DEBUG_LEFTOVERS
+	NSString *lineString = [[NSString alloc] initWithData:leftovers encoding:NSISOLatin1StringEncoding];
+	NSLog(@"LineOrientedCommunication");
+	NSLog(@"Raw: %@", leftovers);
+	NSLog(@"Text: %@", lineString);
+	[lineString release];
+#endif
+	
+	[_dataBuffer release];
+	_dataBuffer = nil;
+	
+	_dataBuffer = [NSMutableData dataWithBytes:[leftovers bytes] length:[leftovers length]];
+}
+
+- (void)dealloc {
+	[_dataBuffer release];
+	
+	[super dealloc];
+}
+
+@end
