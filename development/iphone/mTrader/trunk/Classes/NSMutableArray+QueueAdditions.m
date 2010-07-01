eed9ada509d749c984b0f2de0bd4a2c5be8db889
palmerc <palmerc@9c8e43f7-b2fe-0310-8927-e999a62a3b53>
2010-07-01 13:36:05 +0000

    Made a formatting change to the QueueAdditions category.

--- development/iphone/mTrader/trunk/Classes/NSMutableArray+QueueAdditions.m
+++ development/iphone/mTrader/trunk/Classes/NSMutableArray+QueueAdditions.m
@@ -11,11 +11,11 @@
 
 @implementation NSMutableArray (QueueAdditions)
 
--(void) enQueue:(id)anObject {
+- (void)enQueue:(id)anObject {
 	[self addObject:anObject];
 }
 
--(id) deQueue {
+- (id)deQueue {
 	id headObject = [self objectAtIndex:0];
 	if (headObject != nil) {
 		[[headObject retain] autorelease];
